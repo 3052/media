7f08cd35f43e9032e35adf4d05906fcbc79f4465
10317 <10317@proton.me>
2025-03-05 16:36:23 -0600

    +   legacy_id  EpisodeId

--- itv/itv_test.go
+++ itv/itv_test.go
@@ -9,25 +9,25 @@ import (
 var tests = []struct {
    content_id string
    key_id     string
-   legacy_id  LegacyId
+   legacy_id  EpisodeId
    url        string
 }{
    {
       content_id: "MTAtNTUwMy0wMDAxLTAwMV8yMg==",
       key_id:     "FUl4yiBqSRC1imOJbh17og==",
-      legacy_id:  LegacyId{"10", "5503", "0001"},
+      legacy_id:  EpisodeId{"10", "5503", "0001"},
       url:        "itv.com/watch/gone-girl/10a5503a0001",
    },
    {
       content_id: "MTAtMzkxNS0wMDAyLTAwMV8zNA==",
       key_id:     "zCXIAYrkT9+eG6gbjNG1Qw==",
-      legacy_id:  LegacyId{"10", "3915", "0002"},
+      legacy_id:  EpisodeId{"10", "3915", "0002"},
       url:        "itv.com/watch/community/10a3915/10a3915a0002",
    },
    {
       content_id: "MTAtMzkxOC0wMDAxLTAwMV8zNA==",
       key_id:     "znjzKgOaRBqJMBDGiUDN8g==",
-      legacy_id:  LegacyId{"10", "3918", "0001"},
+      legacy_id:  EpisodeId{"10", "3918", "0001"},
       url:        "itv.com/watch/joan/10a3918/10a3918a0001",
    },
 }
