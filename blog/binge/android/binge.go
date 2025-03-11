31399877b28f0a6a9d0e28b33ea0d22e21737f8c
10317 <10317@proton.me>
2025-03-10 21:43:51 -0500

    deleted:    blog/binge/web/2.go

--- blog/binge/android/binge.go
+++ blog/binge/android/binge.go
@@ -8,39 +8,15 @@ import (
    "net/http"
 )
 
-// new refresh token is not returned, so we can keep old
-func (t *token) refresh() error {
-   data, err := json.Marshal(map[string]string{
-      "grant_type": "refresh_token",
-      "scope": "openid offline_access drm:low email",
-      "client_id": "QQdtPlVtx1h9BkO09BDM2OrFi5vTPCty",
-      "refresh_token": t.RefreshToken,
-   })
-   if err != nil {
-      return err
-   }
-   resp, err := http.Post(
-      "https://auth.streamotion.com.au/oauth/token", "application/json",
-      bytes.NewReader(data),
-   )
-   if err != nil {
-      return err
-   }
-   defer resp.Body.Close()
-   return json.NewDecoder(resp.Body).Decode(t)
-}
+// android?
+//const client_id = "QQdtPlVtx1h9BkO09BDM2OrFi5vTPCty"
 
-type token struct {
-   AccessToken string `json:"access_token"`
-   IdToken string `json:"id_token"`
-   RefreshToken string `json:"refresh_token"`
-}
-
-type Byte[T any] []byte
+// web
+const client_id = "pM87TUXKQvSSu93ydRjDTqBgdYeCbdhZ"
 
 func new_token(username, password string) (Byte[token], error) {
    data, err := json.Marshal( map[string]string{
-      "client_id": "QQdtPlVtx1h9BkO09BDM2OrFi5vTPCty",
+      "client_id": client_id,
       "grant_type": "http://auth0.com/oauth/grant-type/password-realm",
       "password": password,
       "realm": "prod-martian-database",
@@ -62,6 +38,38 @@ func new_token(username, password string) (Byte[token], error) {
    }
    return io.ReadAll(resp.Body)
 }
+
+// new refresh token is not returned, so we can keep old
+func (t *token) refresh() error {
+   data, err := json.Marshal(map[string]string{
+      "client_id": client_id,
+      "grant_type": "refresh_token",
+      "refresh_token": t.RefreshToken,
+   })
+   if err != nil {
+      return err
+   }
+   resp, err := http.Post(
+      "https://auth.streamotion.com.au/oauth/token", "application/json",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      return errors.New(resp.Status)
+   }
+   return json.NewDecoder(resp.Body).Decode(t)
+}
+
+type token struct {
+   AccessToken string `json:"access_token"`
+   IdToken string `json:"id_token"`
+   RefreshToken string `json:"refresh_token"`
+}
+
+type Byte[T any] []byte
 
 func (t *token) unmarshal(data Byte[token]) error {
    return json.Unmarshal(data, t)
--- blog/binge/android/binge_test.go
+++ blog/binge/android/binge_test.go
@@ -1,31 +1,51 @@
 package binge
 
 import (
+   "fmt"
    "os"
    "os/exec"
    "strings"
    "testing"
-   "time"
 )
 
-func Test(t *testing.T) {
+func TestWrite(t *testing.T) {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
    data, err := exec.Command("password", "binge.com.au").Output()
    if err != nil {
       t.Fatal(err)
    }
    username, password, _ := strings.Cut(string(data), ":")
+   data, err = new_token(username, password)
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = os.WriteFile(home + "/media/binge/token", data, os.ModePerm)
+   if err != nil {
+      t.Fatal(err)
+   }
+}
+
+func TestRead(t *testing.T) {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   data, err := os.ReadFile(home + "/media/binge/token")
+   if err != nil {
+      t.Fatal(err)
+   }
    var token1 token
-   err = token1.New(username, password)
+   err = token1.unmarshal(data)
    if err != nil {
       t.Fatal(err)
    }
-   time.Sleep(time.Second)
-   resp, err := token1.refresh()
+   fmt.Printf("%+v\n", token1)
+   err = token1.refresh()
    if err != nil {
       t.Fatal(err)
    }
-   err = resp.Write(os.Stdout)
-   if err != nil {
-      t.Fatal(err)
-   }
+   fmt.Printf("%+v\n", token1)
 }
