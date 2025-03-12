5836490fa1fb373fab7c7cb3159a823fee410289
10317 <10317@proton.me>
2025-03-11 19:45:04 -0500

    deleted:    blog/binge/license.go

--- binge/binge.go
+++ binge/binge.go
@@ -8,6 +8,86 @@ import (
    "net/http"
 )
 
+func (t token_service) widevine(stream1 *stream, data []byte) ([]byte, error) {
+   req, err := http.NewRequest(
+      "POST", stream1.LicenseAcquisitionUrl.ComWidevineAlpha,
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("authorization", "Bearer " + t.AccessToken)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   return io.ReadAll(resp.Body)
+}
+
+// Akamai needs residential proxy (or Nord) and CloudFront/Fastly work with
+// just Mullvad
+func (p play) dash() (*stream, bool) {
+   for _, stream1 := range p.Streams {
+      if stream1.StreamingFormat == "dash" {
+         return &stream1, true
+      }
+   }
+   return nil, false
+}
+
+type stream struct {
+   LicenseAcquisitionUrl struct {
+      ComWidevineAlpha string `json:"com.widevine.alpha"`
+   }
+   Provider string
+   StreamingFormat string
+   Manifest string // MPD
+}
+
+type play struct {
+   Streams []stream
+}
+
+func (t token_service) play() (*play, error) {
+   data, err := json.Marshal(map[string]any{
+      "assetId": "7738",
+      "application": map[string]string{
+         "name": "binge",
+      },
+      "device": map[string]string{
+         "id": "50e785be-4c7f-4781-87e4-a3b4c75a3634",
+      },
+      "player": map[string]string{
+         "name": "VideoFS",
+      },
+   })
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://play.binge.com.au/api/v3/play", bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header = http.Header{
+      "content-type": {"application/json"},
+      "authorization": {"Bearer " + t.AccessToken},
+   }
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   play1 := &play{}
+   err = json.NewDecoder(resp.Body).Decode(play1)
+   if err != nil {
+      return nil, err
+   }
+   return play1, nil
+}
+
 type token_service struct {
    AccessToken string `json:"access_token"`
 }
--- binge/binge_test.go
+++ binge/binge_test.go
@@ -1,6 +1,7 @@
 package binge
 
 import (
+   "41.neocities.org/widevine"
    "fmt"
    "os"
    "os/exec"
@@ -8,6 +9,60 @@ import (
    "testing"
 )
 
+func TestLicense(t *testing.T) {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   data, err := os.ReadFile(home + "/media/binge/auth")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var auth1 auth
+   err = auth1.unmarshal(data)
+   if err != nil {
+      t.Fatal(err)
+   }
+   token, err := auth1.token()
+   if err != nil {
+      t.Fatal(err)
+   }
+   play1, err := token.play()
+   if err != nil {
+      t.Fatal(err)
+   }
+   stream1, ok := play1.dash()
+   if !ok {
+      t.Fatal(".dash()")
+   }
+   private_key, err := os.ReadFile(home + "/media/private_key.pem")
+   if err != nil {
+      t.Fatal(err)
+   }
+   client_id, err := os.ReadFile(home + "/media/client_id.bin")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var pssh widevine.Pssh
+   pssh.KeyIds = [][]byte{
+      []byte(key_id),
+   }
+   var cdm widevine.Cdm
+   err = cdm.New(private_key, client_id, pssh.Marshal())
+   if err != nil {
+      t.Fatal(err)
+   }
+   data, err = cdm.RequestBody()
+   if err != nil {
+      t.Fatal(err)
+   }
+   _, err = token.widevine(stream1, data)
+   if err != nil {
+      t.Fatal(err)
+   }
+}
+
+const key_id = "\x1e\v\xdc\xfd\x069Nٝ\x9f\xe6#\xb0<\xa6\xd2"
 func TestToken(t *testing.T) {
    home, err := os.UserHomeDir()
    if err != nil {
