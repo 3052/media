4ca2968741840355395094c24f9c01105d054a8e
10317 <10317@proton.me>
2025-03-11 21:41:26 -0500

    +   data, err := os.ReadFile(f.media + "/binge/Auth")

--- binge/binge.go
+++ binge/binge.go
@@ -9,112 +9,30 @@ import (
    "strconv"
 )
 
-func (t TokenService) Play(asset_id int) (*Play, error) {
-   data, err := json.Marshal(map[string]any{
-      "application": map[string]string{
-         "name": "binge",
-      },
-      "assetId": strconv.Itoa(asset_id),
-      "device": map[string]string{
-         "id": "!",
-      },
-      "player": map[string]string{
-         "name": "VideoFS",
-      },
-   })
-   if err != nil {
-      return nil, err
-   }
+func (t TokenService) Widevine(stream1 *Stream, data []byte) ([]byte, error) {
    req, err := http.NewRequest(
-      "POST", "https://play.binge.com.au/api/v3/play", bytes.NewReader(data),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header = http.Header{
-      "content-type":  {"application/json"},
-      "authorization": {"Bearer " + t.AccessToken},
-   }
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
-      return nil, errors.New(resp.Status)
-   }
-   play1 := &Play{}
-   err = json.NewDecoder(resp.Body).Decode(play1)
-   if err != nil {
-      return nil, err
-   }
-   return play1, nil
-}
-
-// web
-const client_id = "pM87TUXKQvSSu93ydRjDTqBgdYeCbdhZ"
-func (a *Auth) Token() (*TokenService, error) {
-   data, err := json.Marshal(map[string]string{"client_id": client_id})
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://tokenservice.streamotion.com.au/oauth/token",
+      "POST", stream1.LicenseAcquisitionUrl.ComWidevineAlpha,
       bytes.NewReader(data),
    )
    if err != nil {
       return nil, err
    }
-   req.Header = http.Header{
-      "content-type":  {"application/json"},
-      "authorization": {"Bearer " + a.AccessToken},
-   }
+   req.Header.Set("authorization", "Bearer "+t.AccessToken)
    resp, err := http.DefaultClient.Do(req)
    if err != nil {
       return nil, err
    }
    defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
-      return nil, errors.New(resp.Status)
-   }
-   token := &TokenService{}
-   err = json.NewDecoder(resp.Body).Decode(token)
-   if err != nil {
-      return nil, err
-   }
-   return token, nil
+   return io.ReadAll(resp.Body)
 }
 
-// new refresh token is not returned, so we can keep old
-func (a *Auth) refresh() error {
-   data, err := json.Marshal(map[string]string{
-      "client_id":     client_id,
-      "grant_type":    "refresh_token",
-      "refresh_token": a.RefreshToken,
-   })
-   if err != nil {
-      return err
+type Stream struct {
+   LicenseAcquisitionUrl struct {
+      ComWidevineAlpha string `json:"com.widevine.alpha"`
    }
-   resp, err := http.Post(
-      "https://auth.streamotion.com.au/oauth/token", "application/json",
-      bytes.NewReader(data),
-   )
-   if err != nil {
-      return err
-   }
-   defer resp.Body.Close()
-   err = json.NewDecoder(resp.Body).Decode(a)
-   if err != nil {
-      return err
-   }
-   if a.ErrorDescription != "" {
-      return errors.New(a.ErrorDescription)
-   }
-   return nil
-}
-
-func (a *Auth) Unmarshal(data Byte[Auth]) error {
-   return json.Unmarshal(data, a)
+   Manifest        string // MPD
+   Provider        string
+   StreamingFormat string
 }
 
 func NewAuth(username, password string) (Byte[Auth], error) {
@@ -157,6 +75,119 @@ type Play struct {
    Streams []Stream
 }
 
+type TokenService struct {
+   AccessToken string `json:"access_token"`
+}
+
+// web
+const client_id = "pM87TUXKQvSSu93ydRjDTqBgdYeCbdhZ"
+
+// new refresh token is not returned, so we can keep old
+func (a *Auth) refresh() error {
+   data, err := json.Marshal(map[string]string{
+      "client_id":     client_id,
+      "grant_type":    "refresh_token",
+      "refresh_token": a.RefreshToken,
+   })
+   if err != nil {
+      return err
+   }
+   resp, err := http.Post(
+      "https://auth.streamotion.com.au/oauth/token", "application/json",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   err = json.NewDecoder(resp.Body).Decode(a)
+   if err != nil {
+      return err
+   }
+   if a.ErrorDescription != "" {
+      return errors.New(a.ErrorDescription)
+   }
+   return nil
+}
+
+func (a *Auth) Unmarshal(data Byte[Auth]) error {
+   return json.Unmarshal(data, a)
+}
+
+func (a *Auth) Token() (*TokenService, error) {
+   data, err := json.Marshal(map[string]string{"client_id": client_id})
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://tokenservice.streamotion.com.au/oauth/token",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header = http.Header{
+      "content-type":  {"application/json"},
+      "authorization": {"Bearer " + a.AccessToken},
+   }
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      return nil, errors.New(resp.Status)
+   }
+   token := &TokenService{}
+   err = json.NewDecoder(resp.Body).Decode(token)
+   if err != nil {
+      return nil, err
+   }
+   return token, nil
+}
+
+func (t TokenService) Play(asset_id int) (*Play, error) {
+   data, err := json.Marshal(map[string]any{
+      "application": map[string]string{
+         "name": "binge",
+      },
+      "assetId": strconv.Itoa(asset_id),
+      "device": map[string]string{
+         "id": "!",
+      },
+      "player": map[string]string{
+         "name": "VideoFS",
+      },
+   })
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://play.binge.com.au/api/v3/play", bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header = http.Header{
+      "content-type":  {"application/json"},
+      "authorization": {"Bearer " + t.AccessToken},
+   }
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      return nil, errors.New(resp.Status)
+   }
+   play1 := &Play{}
+   err = json.NewDecoder(resp.Body).Decode(play1)
+   if err != nil {
+      return nil, err
+   }
+   return play1, nil
+}
+
 // Akamai needs residential proxy (or Nord) and CloudFront/Fastly work with
 // just Mullvad
 func (p Play) Dash() (*Stream, bool) {
@@ -166,34 +197,4 @@ func (p Play) Dash() (*Stream, bool) {
       }
    }
    return nil, false
-}
-
-type Stream struct {
-   LicenseAcquisitionUrl struct {
-      ComWidevineAlpha string `json:"com.widevine.alpha"`
-   }
-   Manifest        string // MPD
-   Provider        string
-   StreamingFormat string
-}
-
-func (t TokenService) Widevine(stream1 *Stream, data []byte) ([]byte, error) {
-   req, err := http.NewRequest(
-      "POST", stream1.LicenseAcquisitionUrl.ComWidevineAlpha,
-      bytes.NewReader(data),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("authorization", "Bearer "+t.AccessToken)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   return io.ReadAll(resp.Body)
-}
-
-type TokenService struct {
-   AccessToken string `json:"access_token"`
 }
--- internal/binge/binge.go
+++ internal/binge/binge.go
@@ -3,6 +3,7 @@ package main
 import (
    "41.neocities.org/media/binge"
    "41.neocities.org/media/internal"
+   "errors"
    "flag"
    "log"
    "net/http"
@@ -10,62 +11,39 @@ import (
    "path/filepath"
 )
 
-type flags struct {
-   e              internal.License
-   email          string
-   entity         binge.Entity
-   media          string
-   password       string
-   dash string
-}
-
 func (f *flags) download() error {
-   if f.dash != "" {
-      data, err := os.ReadFile(f.media + "/binge/Playlist")
-      if err != nil {
-         return err
-      }
-      var play binge.Playlist
-      err = play.Unmarshal(data)
-      if err != nil {
-         return err
-      }
-      f.e.Widevine = func(data []byte) ([]byte, error) {
-         return play.Widevine(data)
-      }
-      return f.e.Download(f.media + "/Mpd", f.dash)
-   }
-   data, err := os.ReadFile(f.media + "/binge/Authenticate")
+   data, err := os.ReadFile(f.media + "/binge/Auth")
    if err != nil {
       return err
    }
-   var auth binge.Authenticate
+   var auth binge.Auth
    err = auth.Unmarshal(data)
    if err != nil {
       return err
    }
-   deep, err := auth.DeepLink(f.entity)
+   token, err := auth.Token()
    if err != nil {
       return err
    }
-   data, err = auth.Playlist(deep)
+   play, err := token.Play(f.binge)
    if err != nil {
       return err
    }
-   err = f.write_file("/binge/Playlist", data)
+   stream, ok := play.Dash()
+   if !ok {
+      return errors.New(".Dash()")
+   }
+   if f.dash != "" {
+      f.e.Widevine = func(data []byte) ([]byte, error) {
+         return token.Widevine(stream, data)
+      }
+      return f.e.Download(f.media+"/Mpd", f.dash)
+   }
+   resp, err := http.Get(stream.Manifest)
    if err != nil {
       return err
    }
-   var play binge.Playlist
-   err = play.Unmarshal(data)
-   if err != nil {
-      return err
-   }
-   resp, err := http.Get(play.StreamUrl)
-   if err != nil {
-      return err
-   }
-   return internal.Mpd(f.media + "/Mpd", resp)
+   return internal.Mpd(f.media+"/Mpd", resp)
 }
 
 func (f *flags) New() error {
@@ -80,18 +58,23 @@ func (f *flags) New() error {
    return nil
 }
 
+func (f *flags) write_file(name string, data []byte) error {
+   log.Println("WriteFile", f.media+name)
+   return os.WriteFile(f.media+name, data, os.ModePerm)
+}
+
 func main() {
    var f flags
    err := f.New()
    if err != nil {
       panic(err)
    }
-   flag.Var(&f.entity, "a", "address")
    flag.StringVar(&f.e.ClientId, "c", f.e.ClientId, "client ID")
    flag.StringVar(&f.email, "e", "", "email")
    flag.StringVar(&f.dash, "i", "", "dash ID")
    flag.StringVar(&f.e.PrivateKey, "k", f.e.PrivateKey, "private key")
    flag.StringVar(&f.password, "p", "", "password")
+   flag.IntVar(&f.binge, "b", 0, "binge ID")
    flag.Parse()
    switch {
    case f.password != "":
@@ -99,7 +82,7 @@ func main() {
       if err != nil {
          panic(err)
       }
-   case f.entity[0] != "":
+   case f.binge >= 1:
       err := f.download()
       if err != nil {
          panic(err)
@@ -109,15 +92,19 @@ func main() {
    }
 }
 
+type flags struct {
+   binge    int
+   dash     string
+   e        internal.License
+   email    string
+   media    string
+   password string
+}
+
 func (f *flags) authenticate() error {
-   data, err := binge.NewAuthenticate(f.email, f.password)
+   data, err := binge.NewAuth(f.email, f.password)
    if err != nil {
       return err
    }
-   return f.write_file("/binge/Authenticate", data)
-}
-
-func (f *flags) write_file(name string, data []byte) error {
-   log.Println("WriteFile", f.media + name)
-   return os.WriteFile(f.media + name, data, os.ModePerm)
+   return f.write_file("/binge/Auth", data)
 }
