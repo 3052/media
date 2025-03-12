23a4de87bbcda302cde45fd04ef47b2d1f2adec4
10317 <10317@proton.me>
2025-03-11 20:06:34 -0500

    deleted:    binge/play.json

--- binge/binge.go
+++ binge/binge.go
@@ -8,7 +8,137 @@ import (
    "net/http"
 )
 
-func (t token_service) widevine(stream1 *stream, data []byte) ([]byte, error) {
+// android?
+//const client_id = "QQdtPlVtx1h9BkO09BDM2OrFi5vTPCty"
+
+// web
+const client_id = "pM87TUXKQvSSu93ydRjDTqBgdYeCbdhZ"
+
+func (a *Auth) Token() (*TokenService, error) {
+   data, err := json.Marshal(map[string]string{"client_id": client_id})
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://tokenservice.streamotion.com.au/oauth/token",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header = http.Header{
+      "content-type": {"application/json"},
+      "authorization": {"Bearer " + a.AccessToken},
+   }
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      return nil, errors.New(resp.Status)
+   }
+   token := &TokenService{}
+   err = json.NewDecoder(resp.Body).Decode(token)
+   if err != nil {
+      return nil, err
+   }
+   return token, nil
+}
+
+// new refresh token is not returned, so we can keep old
+func (a *Auth) refresh() error {
+   data, err := json.Marshal(map[string]string{
+      "client_id": client_id,
+      "grant_type": "refresh_token",
+      "refresh_token": a.RefreshToken,
+   })
+   if err != nil {
+      return err
+   }
+   resp, err := http.Post(
+      "https://auth.streamotion.com.au/oauth/token", "application/json",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   err = json.NewDecoder(resp.Body).Decode(a)
+   if err != nil {
+      return err
+   }
+   if a.ErrorDescription != "" {
+      return errors.New(a.ErrorDescription)
+   }
+   return nil
+}
+
+func (a *Auth) Unmarshal(data Byte[Auth]) error {
+   return json.Unmarshal(data, a)
+}
+
+func NewAuth(username, password string) (Byte[Auth], error) {
+   data, err := json.Marshal( map[string]string{
+      "client_id": client_id,
+      "grant_type": "http://auth0.com/oauth/grant-type/password-realm",
+      "password": password,
+      "realm": "prod-martian-database",
+      "username": username,
+      // need this otherwise service request fails
+      "audience": "streamotion.com.au",
+   })
+   if err != nil {
+      return nil, err
+   }
+   resp, err := http.Post(
+      "https://auth.streamotion.com.au/oauth/token", "application/json",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      return nil, errors.New(resp.Status)
+   }
+   return io.ReadAll(resp.Body)
+}
+
+type Auth struct {
+   AccessToken string `json:"access_token"`
+   ErrorDescription string `json:"error_description"`
+   IdToken string `json:"id_token"`
+   RefreshToken string `json:"refresh_token"`
+}
+
+type Byte[T any] []byte
+
+type Play struct {
+   Streams []Stream
+}
+
+// Akamai needs residential proxy (or Nord) and CloudFront/Fastly work with
+// just Mullvad
+func (p Play) Dash() (*Stream, bool) {
+   for _, stream1 := range p.Streams {
+      if stream1.StreamingFormat == "dash" {
+         return &stream1, true
+      }
+   }
+   return nil, false
+}
+
+type Stream struct {
+   LicenseAcquisitionUrl struct {
+      ComWidevineAlpha string `json:"com.widevine.alpha"`
+   }
+   Manifest string // MPD
+   Provider string
+   StreamingFormat string
+}
+
+func (t TokenService) Widevine(stream1 *Stream, data []byte) ([]byte, error) {
    req, err := http.NewRequest(
       "POST", stream1.LicenseAcquisitionUrl.ComWidevineAlpha,
       bytes.NewReader(data),
@@ -25,31 +155,7 @@ func (t token_service) widevine(stream1 *stream, data []byte) ([]byte, error) {
    return io.ReadAll(resp.Body)
 }
 
-// Akamai needs residential proxy (or Nord) and CloudFront/Fastly work with
-// just Mullvad
-func (p play) dash() (*stream, bool) {
-   for _, stream1 := range p.Streams {
-      if stream1.StreamingFormat == "dash" {
-         return &stream1, true
-      }
-   }
-   return nil, false
-}
-
-type stream struct {
-   LicenseAcquisitionUrl struct {
-      ComWidevineAlpha string `json:"com.widevine.alpha"`
-   }
-   Provider string
-   StreamingFormat string
-   Manifest string // MPD
-}
-
-type play struct {
-   Streams []stream
-}
-
-func (t token_service) play() (*play, error) {
+func (t TokenService) Play() (*Play, error) {
    data, err := json.Marshal(map[string]any{
       "assetId": "7738",
       "application": map[string]string{
@@ -80,7 +186,7 @@ func (t token_service) play() (*play, error) {
       return nil, err
    }
    defer resp.Body.Close()
-   play1 := &play{}
+   play1 := &Play{}
    err = json.NewDecoder(resp.Body).Decode(play1)
    if err != nil {
       return nil, err
@@ -88,112 +194,6 @@ func (t token_service) play() (*play, error) {
    return play1, nil
 }
 
-type token_service struct {
+type TokenService struct {
    AccessToken string `json:"access_token"`
-}
-
-func (a *auth) token() (*token_service, error) {
-   data, err := json.Marshal(map[string]string{"client_id": client_id})
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://tokenservice.streamotion.com.au/oauth/token",
-      bytes.NewReader(data),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header = http.Header{
-      "content-type": {"application/json"},
-      "authorization": {"Bearer " + a.AccessToken},
-   }
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
-      return nil, errors.New(resp.Status)
-   }
-   token := &token_service{}
-   err = json.NewDecoder(resp.Body).Decode(token)
-   if err != nil {
-      return nil, err
-   }
-   return token, nil
-}
-
-// android?
-//const client_id = "QQdtPlVtx1h9BkO09BDM2OrFi5vTPCty"
-
-// web
-const client_id = "pM87TUXKQvSSu93ydRjDTqBgdYeCbdhZ"
-
-// new refresh token is not returned, so we can keep old
-func (a *auth) refresh() error {
-   data, err := json.Marshal(map[string]string{
-      "client_id": client_id,
-      "grant_type": "refresh_token",
-      "refresh_token": a.RefreshToken,
-   })
-   if err != nil {
-      return err
-   }
-   resp, err := http.Post(
-      "https://auth.streamotion.com.au/oauth/token", "application/json",
-      bytes.NewReader(data),
-   )
-   if err != nil {
-      return err
-   }
-   defer resp.Body.Close()
-   err = json.NewDecoder(resp.Body).Decode(a)
-   if err != nil {
-      return err
-   }
-   if a.ErrorDescription != "" {
-      return errors.New(a.ErrorDescription)
-   }
-   return nil
-}
-
-type Byte[T any] []byte
-
-func (a *auth) unmarshal(data Byte[auth]) error {
-   return json.Unmarshal(data, a)
-}
-
-func new_auth(username, password string) (Byte[auth], error) {
-   data, err := json.Marshal( map[string]string{
-      "client_id": client_id,
-      "grant_type": "http://auth0.com/oauth/grant-type/password-realm",
-      "password": password,
-      "realm": "prod-martian-database",
-      "username": username,
-      // need this otherwise service request fails
-      "audience": "streamotion.com.au",
-   })
-   if err != nil {
-      return nil, err
-   }
-   resp, err := http.Post(
-      "https://auth.streamotion.com.au/oauth/token", "application/json",
-      bytes.NewReader(data),
-   )
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
-      return nil, errors.New(resp.Status)
-   }
-   return io.ReadAll(resp.Body)
-}
-
-type auth struct {
-   AccessToken string `json:"access_token"`
-   ErrorDescription string `json:"error_description"`
-   IdToken string `json:"id_token"`
-   RefreshToken string `json:"refresh_token"`
 }
