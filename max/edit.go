0abc52adee667d1a9575f7d15424206bb80e776e
367 <367@tuta.io>
2025-03-25 16:52:22 -0500

    +func (n Login) movie(id string) (*videos, error) {

--- max/edit.go
+++ max/edit.go
@@ -7,25 +7,52 @@ import (
    "iter"
    "net/http"
    "net/url"
-   "slices"
 )
 
-func (n Login) seasons() iter.Seq2[*videos, error] {
-   return func(yield func(*videos, error) bool) {
-      number := 1
-      for {
-         season1, err := n.videos(number)
-         if !yield(season1, err) {
-            break
-         }
-         for _, filter := range season1.Data.Attributes.Component.Filters {
-            if number >= len(filter.Options) {
-               return
-            }
-         }
-         number++
-      }
+func (n Login) movie(id string) (*videos, error) {
+   req, _ := http.NewRequest("", prd_api, nil)
+   req.URL.Path = "/cms/routes/movie/" + id
+   req.URL.RawQuery = url.Values{
+      "include":          {"default"},
+      "page[items.size]": {"1"},
+   }.Encode()
+   req.Header.Set("authorization", "Bearer "+n.Data.Attributes.Token)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
    }
+   defer resp.Body.Close()
+   var movie videos
+   err = json.NewDecoder(resp.Body).Decode(&movie)
+   if err != nil {
+      return nil, err
+   }
+   if len(movie.Errors) >= 1 {
+      return nil, errors.New(movie.Errors[0].Detail)
+   }
+   return &movie, nil
+}
+
+func (n Login) season(show string, number int) (*videos, error) {
+   req, _ := http.NewRequest("", prd_api, nil)
+   req.URL.Path = "/cms/collections/generic-show-page-rail-episodes-tabbed-content"
+   req.Header.Set("authorization", "Bearer "+n.Data.Attributes.Token)
+   req.URL.RawQuery = url.Values{
+      "include":          {"default"},
+      "pf[show.id]":      {show},
+      "pf[seasonNumber]": {fmt.Sprint(number)},
+   }.Encode()
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   season1 := &videos{}
+   err = json.NewDecoder(resp.Body).Decode(season1)
+   if err != nil {
+      return nil, err
+   }
+   return season1, nil
 }
 
 type video struct {
@@ -54,65 +81,10 @@ func (v *video) String() string {
 }
 
 type videos struct {
-   Data struct {
-      Attributes struct {
-         Component struct {
-            Filters []struct {
-               Options []struct{}
-            }
-         }
-      }
-   }
    Errors []struct {
       Detail string
    }
    Included []video
-}
-
-func (n Login) videos(number int) (*videos, error) {
-   req, _ := http.NewRequest("", prd_api, nil)
-   req.URL.Path = "/cms/collections/generic-show-page-rail-episodes-tabbed-content"
-   req.URL.RawQuery = url.Values{
-      "include":          {"default"},
-      "pf[show.id]":      {"14f9834d-bc23-41a8-ab61-5c8abdbea505"},
-      "pf[seasonNumber]": {fmt.Sprint(number)},
-   }.Encode()
-   req.Header.Set("authorization", "Bearer "+n.Data.Attributes.Token)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   season1 := &videos{}
-   err = json.NewDecoder(resp.Body).Decode(season1)
-   if err != nil {
-      return nil, err
-   }
-   return season1, nil
-}
-
-func (n Login) movie(route string) (*videos, error) {
-   req, _ := http.NewRequest("", prd_api, nil)
-   req.URL.RawQuery = url.Values{
-      "include":          {"default"},
-      "page[items.size]": {"1"},
-   }.Encode()
-   req.Header.Set("authorization", "Bearer "+n.Data.Attributes.Token)
-   req.URL.Path = "/cms/routes" + route
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var movie videos
-   err = json.NewDecoder(resp.Body).Decode(&movie)
-   if err != nil {
-      return nil, err
-   }
-   if len(movie.Errors) >= 1 {
-      return nil, errors.New(movie.Errors[0].Detail)
-   }
-   return &movie, nil
 }
 
 func (v *videos) seq(video_type string) iter.Seq[video] {
@@ -135,10 +107,4 @@ func (v *videos) movie() iter.Seq[video] {
 
 func (v *videos) episode() iter.Seq[video] {
    return v.seq("EPISODE")
-}
-
-func (s *videos) sorted() []video {
-   return slices.SortedFunc(s.episode(), func(a, b video) int {
-      return a.Attributes.EpisodeNumber - b.Attributes.EpisodeNumber
-   })
 }
--- max/edit_test.go
+++ max/edit_test.go
@@ -9,6 +9,34 @@ import (
    "time"
 )
 
+func (transport) RoundTrip(req *http.Request) (*http.Response, error) {
+   log.Println(req.Method, req.URL)
+   return http.DefaultTransport.RoundTrip(req)
+}
+
+type transport struct{}
+
+func TestMovie(t *testing.T) {
+   home, err := os.UserHomeDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   data, err := os.ReadFile(home + "/media/max/Login")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var login1 Login
+   err = login1.Unmarshal(data)
+   if err != nil {
+      t.Fatal(err)
+   }
+   movie, err := login1.movie("12199308-9afb-460b-9d79-9d54b5d2514c")
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Println(movie.movie())
+}
+
 func TestShow(t *testing.T) {
    http.DefaultClient.Transport = transport{}
    log.SetFlags(log.Ltime)
@@ -25,46 +53,17 @@ func TestShow(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-   var line bool
-   for season1, err := range login1.seasons() {
-      if err != nil {
-         t.Fatal(err)
+   season, err := login1.season("14f9834d-bc23-41a8-ab61-5c8abdbea505", 1)
+   if err != nil {
+      t.Fatal(err)
+   }
+   season1 := slices.SortedFunc(season.episode(), func(a, b video) int {
+      return a.Attributes.EpisodeNumber - b.Attributes.EpisodeNumber
+   })
+   for i, episode := range season1 {
+      if i >= 1 {
+         fmt.Println()
       }
-      for _, episode := range season1.sorted() {
-         if line {
-            fmt.Println()
-         } else {
-            line = true
-         }
-         fmt.Println(&episode)
-      }
-      time.Sleep(99*time.Millisecond)
+      fmt.Println(&episode)
    }
-}
-
-func (transport) RoundTrip(req *http.Request) (*http.Response, error) {
-   log.Println(req.Method, req.URL)
-   return http.DefaultTransport.RoundTrip(req)
-}
-
-type transport struct{}
-func TestMovie(t *testing.T) {
-   home, err := os.UserHomeDir()
-   if err != nil {
-      t.Fatal(err)
-   }
-   data, err := os.ReadFile(home + "/media/max/Login")
-   if err != nil {
-      t.Fatal(err)
-   }
-   var login1 Login
-   err = login1.Unmarshal(data)
-   if err != nil {
-      t.Fatal(err)
-   }
-   movie, err := login1.movie("/movie/12199308-9afb-460b-9d79-9d54b5d2514c")
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Println(movie.movie())
 }
