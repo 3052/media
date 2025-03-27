774ebc1cce7c3c4ef77f9dbc4367c336f1a2a466
367 <367@tuta.io>
2025-03-27 16:24:37 -0500

    +func (r *refresh) refresh() (Byte[refresh], error) {

--- molotov/molotov.go
+++ molotov/molotov.go
@@ -7,37 +7,7 @@ import (
    "net/http"
 )
 
-func (r *refresh) unmarshal(data Byte[refresh]) error {
-   return json.Unmarshal(data, r)
-}
-
-type Byte[T any] []byte
-
-type refresh struct {
-   AccessToken string `json:"access_token"`
-   RefreshToken string `json:"refresh_token"`
-}
-
-// authorization server issues a new refresh token, in which case the
-// client MUST discard the old refresh token and replace it with the new
-// refresh token
-func (n login) refresh() (Byte[refresh], error) {
-   req, _ := http.NewRequest("", "https://fapi.molotov.tv", nil)
-   req.URL.Path = "/v3/auth/refresh/" + n.RefreshToken
-   req.Header.Set("x-molotov-agent", molotov_agent)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   return io.ReadAll(resp.Body)
-}
-
 const molotov_agent = `{ "app_build": 4, "app_id": "browser_app" }`
-
-type login struct {
-   RefreshToken string `json:"refresh_token"`
-}
 
 func (n *login) New(email, password string) error {
    data, err := json.Marshal(map[string]string{
@@ -62,4 +32,34 @@ func (n *login) New(email, password string) error {
    }
    defer resp.Body.Close()
    return json.NewDecoder(resp.Body).Decode(n)
+}
+
+// authorization server issues a new refresh token, in which case the
+// client MUST discard the old refresh token and replace it with the new
+// refresh token
+func (r *refresh) refresh() (Byte[refresh], error) {
+   req, _ := http.NewRequest("", "https://fapi.molotov.tv", nil)
+   req.URL.Path = "/v3/auth/refresh/" + r.RefreshToken
+   req.Header.Set("x-molotov-agent", molotov_agent)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   return io.ReadAll(resp.Body)
+}
+
+type Byte[T any] []byte
+
+type refresh struct {
+   AccessToken string `json:"access_token"`
+   RefreshToken string `json:"refresh_token"`
+}
+
+type login struct {
+   Auth refresh
+}
+
+func (r *refresh) unmarshal(data Byte[refresh]) error {
+   return json.Unmarshal(data, r)
 }
