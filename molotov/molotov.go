f4002fb01fa352508f3f12eef1987cbae9abc7a0
367 <367@tuta.io>
2025-03-27 20:56:16 -0500

    +         "ref": "v1.10.3",

--- molotov/molotov.go
+++ molotov/molotov.go
@@ -10,90 +10,7 @@ import (
    "strings"
 )
 
-type View struct {
-   Program struct {
-      Actions struct {
-         Play struct {
-            Url string
-         }
-      }
-   }
-}
-
-func (r *Refresh) Asset(view1 *View) (Byte[Asset], error) {
-   req, err := http.NewRequest("", view1.Program.Actions.Play.Url, nil)
-   if err != nil {
-      return nil, err
-   }
-   query := req.URL.Query()
-   query.Set("access_token", r.AccessToken)
-   req.URL.RawQuery = query.Encode()
-   req.Header = http.Header{
-      "x-forwarded-for": {"138.199.15.158"},
-      "x-molotov-agent": {molotov_agent},
-   }
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   return io.ReadAll(resp.Body)
-}
-
 const molotov_agent = `{ "app_build": 4, "app_id": "browser_app" }`
-
-func (n *Login) New(email, password string) error {
-   data, err := json.Marshal(map[string]string{
-      "grant_type": "password",
-      "email": email,
-      "password": password,
-   })
-   if err != nil {
-      return err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://fapi.molotov.tv/v3.1/auth/login",
-      bytes.NewReader(data),
-   )
-   if err != nil {
-      return err
-   }
-   req.Header.Set("x-molotov-agent", molotov_agent)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return err
-   }
-   defer resp.Body.Close()
-   return json.NewDecoder(resp.Body).Decode(n)
-}
-
-type Login struct {
-   Auth Refresh
-}
-
-// authorization server issues a new refresh token, in which case the
-// client MUST discard the old refresh token and replace it with the new
-// refresh token
-func (r *Refresh) Refresh() (Byte[Refresh], error) {
-   req, _ := http.NewRequest("", "https://fapi.molotov.tv", nil)
-   req.URL.Path = "/v3/auth/refresh/" + r.RefreshToken
-   req.Header.Set("x-molotov-agent", molotov_agent)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   return io.ReadAll(resp.Body)
-}
-
-func (r *Refresh) Unmarshal(data Byte[Refresh]) error {
-   return json.Unmarshal(data, r)
-}
-
-type Refresh struct {
-   AccessToken string `json:"access_token"`
-   RefreshToken string `json:"refresh_token"`
-}
 
 // https://www.molotov.tv/fr_fr/p/15082-531/la-vie-aquatique
 type Address struct {
@@ -112,42 +29,6 @@ func (a *Address) String() string {
       b = strconv.AppendInt(b, a.Channel, 10)
    }
    return string(b)
-}
-
-type Asset struct {
-   Stream struct {
-      Url string // MPD
-   }
-   UpDrm struct {
-      License struct {
-         HttpHeaders map[string]string `json:"http_headers"`
-      }
-   } `json:"up_drm"`
-}
-
-func (r *Refresh) View(web *Address) (*View, error) {
-   req, _ := http.NewRequest("", "https://fapi.molotov.tv", nil)
-   req.URL.Path = func() string {
-      b := []byte("/v2/channels/")
-      b = strconv.AppendInt(b, web.Channel, 10)
-      b = append(b, "/programs/"...)
-      b = strconv.AppendInt(b, web.Program, 10)
-      b = append(b, "/view"...)
-      return string(b)
-   }()
-   req.URL.RawQuery = "access_token=" + r.AccessToken
-   req.Header.Set("x-molotov-agent", molotov_agent)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   view1 := &View{}
-   err = json.NewDecoder(resp.Body).Decode(view1)
-   if err != nil {
-      return nil, err
-   }
-   return view1, nil
 }
 
 func (a *Address) Set(data string) error {
@@ -174,7 +55,16 @@ func (a *Address) Set(data string) error {
    return nil
 }
 
-type Byte[T any] []byte
+type Asset struct {
+   Stream struct {
+      Url string // MPD
+   }
+   UpDrm struct {
+      License struct {
+         HttpHeaders map[string]string `json:"http_headers"`
+      }
+   } `json:"up_drm"`
+}
 
 func (a *Asset) Widevine(data []byte) ([]byte, error) {
    req, err := http.NewRequest(
@@ -208,4 +98,114 @@ func (a *Asset) FhdReady() string {
 
 func (a *Asset) Unmarshal(data Byte[Asset]) error {
    return json.Unmarshal(data, a)
+}
+
+type Byte[T any] []byte
+
+func (n *Login) New(email, password string) error {
+   data, err := json.Marshal(map[string]string{
+      "grant_type": "password",
+      "email": email,
+      "password": password,
+   })
+   if err != nil {
+      return err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://fapi.molotov.tv/v3.1/auth/login",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return err
+   }
+   req.Header.Set("x-molotov-agent", molotov_agent)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   return json.NewDecoder(resp.Body).Decode(n)
+}
+
+type Login struct {
+   Auth Refresh
+}
+
+func (r *Refresh) Asset(view1 *View) (Byte[Asset], error) {
+   req, err := http.NewRequest("", view1.Program.Actions.Play.Url, nil)
+   if err != nil {
+      return nil, err
+   }
+   query := req.URL.Query()
+   query.Set("access_token", r.AccessToken)
+   req.URL.RawQuery = query.Encode()
+   req.Header = http.Header{
+      "x-forwarded-for": {"138.199.15.158"},
+      "x-molotov-agent": {molotov_agent},
+   }
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   return io.ReadAll(resp.Body)
+}
+
+func (r *Refresh) View(web *Address) (*View, error) {
+   req, _ := http.NewRequest("", "https://fapi.molotov.tv", nil)
+   req.URL.Path = func() string {
+      b := []byte("/v2/channels/")
+      b = strconv.AppendInt(b, web.Channel, 10)
+      b = append(b, "/programs/"...)
+      b = strconv.AppendInt(b, web.Program, 10)
+      b = append(b, "/view"...)
+      return string(b)
+   }()
+   req.URL.RawQuery = "access_token=" + r.AccessToken
+   req.Header.Set("x-molotov-agent", molotov_agent)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   view1 := &View{}
+   err = json.NewDecoder(resp.Body).Decode(view1)
+   if err != nil {
+      return nil, err
+   }
+   return view1, nil
+}
+
+// authorization server issues a new refresh token, in which case the
+// client MUST discard the old refresh token and replace it with the new
+// refresh token
+func (r *Refresh) Refresh() (Byte[Refresh], error) {
+   req, _ := http.NewRequest("", "https://fapi.molotov.tv", nil)
+   req.URL.Path = "/v3/auth/refresh/" + r.RefreshToken
+   req.Header.Set("x-molotov-agent", molotov_agent)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   return io.ReadAll(resp.Body)
+}
+
+func (r *Refresh) Unmarshal(data Byte[Refresh]) error {
+   return json.Unmarshal(data, r)
+}
+
+type Refresh struct {
+   AccessToken string `json:"access_token"`
+   RefreshToken string `json:"refresh_token"`
+}
+
+type View struct {
+   Program struct {
+      Actions struct {
+         Play struct {
+            Url string
+         }
+      }
+   }
 }
--- molotov/molotov_test.go
+++ molotov/molotov_test.go
@@ -1,11 +1,7 @@
 package molotov
 
 import (
-   "41.neocities.org/widevine"
-   "fmt"
    "os"
-   "os/exec"
-   "strings"
    "testing"
 )
 
@@ -18,67 +14,17 @@ func TestWidevine(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-   var token refresh
-   err = token.unmarshal(data)
+   var token Refresh
+   err = token.Unmarshal(data)
    if err != nil {
       t.Fatal(err)
    }
-   view1, err := token.view()
+   var web Address
+   err = web.Set("molotov.tv/fr_fr/p/15082-531")
    if err != nil {
       t.Fatal(err)
    }
-   assets1, err := token.assets(view1)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%q\n", assets1.fhd_ready())
-   private_key, err := os.ReadFile(home + "/media/private_key.pem")
-   if err != nil {
-      t.Fatal(err)
-   }
-   client_id, err := os.ReadFile(home + "/media/client_id.bin")
-   if err != nil {
-      t.Fatal(err)
-   }
-   var pssh widevine.Pssh
-   pssh.KeyIds = [][]byte{
-      []byte("\xc3\x1c\xd0+m\x17\x01\xee\xa1\xedp7\xa8~\xd8J"),
-   }
-   var cdm widevine.Cdm
-   err = cdm.New(private_key, client_id, pssh.Marshal())
-   if err != nil {
-      t.Fatal(err)
-   }
-   data, err = cdm.RequestBody()
-   if err != nil {
-      t.Fatal(err)
-   }
-   _, err = assets1.widevine(data)
-   if err != nil {
-      t.Fatal(err)
-   }
-}
-
-func TestRefresh(t *testing.T) {
-   data, err := exec.Command("password", "molotov.tv").Output()
-   if err != nil {
-      t.Fatal(err)
-   }
-   email, password, _ := strings.Cut(string(data), ":")
-   var login1 login
-   err = login1.New(email, password)
-   if err != nil {
-      t.Fatal(err)
-   }
-   data, err = login1.Auth.refresh()
-   if err != nil {
-      t.Fatal(err)
-   }
-   home, err := os.UserHomeDir()
-   if err != nil {
-      t.Fatal(err)
-   }
-   err = os.WriteFile(home + "/media/molotov/refresh", data, os.ModePerm)
+   _, err = token.View(&web)
    if err != nil {
       t.Fatal(err)
    }
