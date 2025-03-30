e264c82ecd18c3f346cc4c2807116f78aa0024cb
367 <367@tuta.io>
2025-03-30 18:20:32 -0500

    deleted:    max/edit.go

--- max/max.go
+++ max/max.go
@@ -5,12 +5,16 @@ import (
    "encoding/json"
    "errors"
    "io"
+   "iter"
    "net/http"
+   "net/url"
+   "path"
+   "strconv"
    "strings"
 )
 
 func (n *Login) Playback(edit_id string) (Byte[Playback], error) {
-   value := map[string]any{
+   data, err := json.Marshal(map[string]any{
       "editId": edit_id,
       "consumptionType":      "streaming",
       "appBundle":            "",         // required
@@ -42,8 +46,7 @@ func (n *Login) Playback(edit_id string) (Byte[Playback], error) {
             }, // required
          }, // required
       }, // required
-   }
-   data, err := json.MarshalIndent(value, "", " ")
+   })
    if err != nil {
       return nil, err
    }
@@ -66,25 +69,11 @@ func (n *Login) Playback(edit_id string) (Byte[Playback], error) {
       return nil, err
    }
    defer resp.Body.Close()
+   if resp.StatusCode == 504 {
+      // bail since no response body
+      return nil, errors.New(resp.Status)
+   }
    return io.ReadAll(resp.Body)
-}
-
-type Playback struct {
-   Drm struct {
-      Schemes struct {
-         Widevine struct {
-            LicenseUrl string
-         }
-      }
-   }
-   Errors []struct {
-      Detail string
-   }
-   Fallback struct {
-      Manifest struct {
-         Url Url // MPD
-      }
-   }
 }
 
 func (p *Playback) Widevine(data []byte) ([]byte, error) {
@@ -100,17 +89,6 @@ func (p *Playback) Widevine(data []byte) ([]byte, error) {
       return nil, errors.New(resp.Status)
    }
    return io.ReadAll(resp.Body)
-}
-
-func (p *Playback) Unmarshal(data Byte[Playback]) error {
-   err := json.Unmarshal(data, p)
-   if err != nil {
-      return err
-   }
-   if len(p.Errors) >= 1 {
-      return errors.New(p.Errors[0].Detail)
-   }
-   return nil
 }
 
 // you must
@@ -230,3 +208,171 @@ type Login struct {
 func (n *Login) Unmarshal(data Byte[Login]) error {
    return json.Unmarshal(data, n)
 }
+func (p *Playback) Unmarshal(data Byte[Playback]) error {
+   err := json.Unmarshal(data, p)
+   if err != nil {
+      return err
+   }
+   if len(p.Errors) >= 1 {
+      return errors.New(p.Errors[0].Detail)
+   }
+   return nil
+}
+
+type Playback struct {
+   Drm struct {
+      Schemes struct {
+         Widevine struct {
+            LicenseUrl string
+         }
+      }
+   }
+   Errors []struct {
+      Detail string
+   }
+   Fallback struct {
+      Manifest struct {
+         Url Url // MPD
+      }
+   }
+}
+
+type Videos struct {
+   Errors []struct {
+      Detail string // show was filtered by validator
+      Message string // Token is missing or not valid
+   }
+   Included []Video
+}
+
+func (n Login) Season(id ShowId, number int) (*Videos, error) {
+   req, _ := http.NewRequest("", prd_api, nil)
+   req.URL.Path = "/cms/collections/generic-show-page-rail-episodes-tabbed-content"
+   req.Header.Set("authorization", "Bearer "+n.Data.Attributes.Token)
+   req.URL.RawQuery = url.Values{
+      "include":          {"default"},
+      "pf[seasonNumber]": {strconv.Itoa(number)},
+      "pf[show.id]":      {string(id)},
+   }.Encode()
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   season := &Videos{}
+   err = json.NewDecoder(resp.Body).Decode(season)
+   if err != nil {
+      return nil, err
+   }
+   return season, nil
+}
+
+func (s ShowId) String() string {
+   return string(s)
+}
+
+// max.com/movies/12199308-9afb-460b-9d79-9d54b5d2514c
+// max.com/movies/heretic/12199308-9afb-460b-9d79-9d54b5d2514c
+// max.com/shows/14f9834d-bc23-41a8-ab61-5c8abdbea505
+// max.com/shows/white-lotus/14f9834d-bc23-41a8-ab61-5c8abdbea505
+func (s *ShowId) Set(data string) error {
+   switch {
+   case strings.Contains(data, "/movies/"):
+   case strings.Contains(data, "/shows/"):
+   default:
+      return errors.New("/movies/ or /shows/ not found")
+   }
+   *s = ShowId(path.Base(data))
+   return nil
+}
+
+type ShowId string
+
+type Video struct {
+   Attributes *struct {
+      SeasonNumber  int
+      EpisodeNumber int
+      Name          string
+      VideoType     string
+   }
+   Relationships *struct {
+      Edit *struct {
+         Data struct {
+            Id string
+         }
+      }
+   }
+}
+
+func (v *Video) String() string {
+   var b []byte
+   if v.Attributes.SeasonNumber >= 1 {
+      b = append(b, "season number = "...)
+      b = strconv.AppendInt(b, int64(v.Attributes.SeasonNumber), 10)
+   }
+   if v.Attributes.EpisodeNumber >= 1 {
+      b = append(b, "\nepisode number = "...)
+      b = strconv.AppendInt(b, int64(v.Attributes.EpisodeNumber), 10)
+   }
+   if b != nil {
+      b = append(b, '\n')
+   }
+   b = append(b, "name = "...)
+   b = append(b, v.Attributes.Name...)
+   b = append(b, "\nvideo type = "...)
+   b = append(b, v.Attributes.VideoType...)
+   b = append(b, "\nedit id = "...)
+   b = append(b, v.Relationships.Edit.Data.Id...)
+   return string(b)
+}
+
+func (v *Videos) Seq() iter.Seq[Video] {
+   return func(yield func(Video) bool) {
+      for _, video1 := range v.Included {
+         if video1.Attributes != nil {
+            switch video1.Attributes.VideoType {
+            case "EPISODE", "MOVIE":
+               if !yield(video1) {
+                  return
+               }
+            }
+         }
+      }
+   }
+}
+
+func (n Login) Movie(id ShowId) (*Videos, error) {
+   req, _ := http.NewRequest("", prd_api, nil)
+   req.URL.Path = "/cms/routes/movie/" + string(id)
+   req.URL.RawQuery = url.Values{
+      "include":          {"default"},
+      "page[items.size]": {"1"},
+   }.Encode()
+   req.Header.Set("authorization", "Bearer "+n.Data.Attributes.Token)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   var movie Videos
+   err = json.NewDecoder(resp.Body).Decode(&movie)
+   if err != nil {
+      return nil, err
+   }
+   if movie.Error() != "" {
+      return nil, &movie
+   }
+   return &movie, nil
+}
+
+func (v *Videos) Error() string {
+   if len(v.Errors) >= 1 {
+      err := v.Errors[0]
+      if err.Detail != "" {
+         return err.Detail
+      }
+      return err.Message
+   }
+   return ""
+}
+
