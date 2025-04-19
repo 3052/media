07c1c1f17f1590be3d651f591fb5ab2b2fded066
367 <367@tuta.io>
2025-04-19 14:35:15 -0500

    +func (f *flags) do_address() error {

--- internal/max/max.go
+++ internal/max/max.go
@@ -13,94 +13,6 @@ import (
    "slices"
 )
 
-func (f *flags) do_mpd() error {
-   if f.dash != "" {
-      data, err := os.ReadFile(f.media + "/max/Playback")
-      if err != nil {
-         return err
-      }
-      var play max.Playback
-      err = play.Unmarshal(data)
-      if err != nil {
-         return err
-      }
-      f.e.Widevine = func(data []byte) ([]byte, error) {
-         return play.Widevine(data)
-      }
-      return f.e.Download(f.media+"/Mpd", f.dash)
-   }
-   data, err := os.ReadFile(f.media + "/max/Login")
-   if err != nil {
-      return err
-   }
-   var login max.Login
-   err = login.Unmarshal(data)
-   if err != nil {
-      return err
-   }
-   data, err = login.Playback(f.edit)
-   if err != nil {
-      return err
-   }
-   var play max.Playback
-   err = play.Unmarshal(data)
-   if err != nil {
-      return err
-   }
-   err = write_file(f.media + "/max/Playback", data)
-   if err != nil {
-      return err
-   }
-   resp, err := http.Get(play.Mpd())
-   if err != nil {
-      return err
-   }
-   return internal.Mpd(f.media+"/Mpd", resp)
-}
-
-type flags struct {
-   dash     string
-   e        internal.License
-   edit     string
-   initiate bool
-   login    bool
-   media    string
-   proxy    bool
-   season   int
-   show_id  max.ShowId
-}
-
-func (f *flags) do_edit() error {
-   data, err := os.ReadFile(f.media + "/max/Login")
-   if err != nil {
-      return err
-   }
-   var login max.Login
-   err = login.Unmarshal(data)
-   if err != nil {
-      return err
-   }
-   var videos *max.Videos
-   if f.season >= 1 {
-      videos, err = login.Season(f.show_id, f.season)
-   } else {
-      videos, err = login.Movie(f.show_id)
-   }
-   if err != nil {
-      return err
-   }
-   sorted := slices.SortedFunc(videos.Seq(), func(a, b max.Video) int {
-      return a.Attributes.EpisodeNumber - b.Attributes.EpisodeNumber
-   })
-   for i, video := range sorted {
-      if i >= 1 {
-         fmt.Println()
-      }
-      fmt.Println(&video)
-   }
-   return nil
-}
-
 func (f *flags) do_initiate() error {
    var st max.St
    err := st.New()
@@ -159,16 +71,44 @@ func (f *flags) do_login() error {
    return write_file(f.media + "/max/Login", data)
 }
 
+func (f *flags) do_address() error {
+   data, err := os.ReadFile(f.media + "/max/Login")
+   if err != nil {
+      return err
+   }
+   var login max.Login
+   err = login.Unmarshal(data)
+   if err != nil {
+      return err
+   }
+   var videos *max.Videos
+   if f.season >= 1 {
+      videos, err = login.Season(f.show_id, f.season)
+   } else {
+      videos, err = login.Movie(f.show_id)
+   }
+   if err != nil {
+      return err
+   }
+   sorted := slices.SortedFunc(videos.Seq(), func(a, b max.Video) int {
+      return a.Attributes.EpisodeNumber - b.Attributes.EpisodeNumber
+   })
+   for i, video := range sorted {
+      if i >= 1 {
+         fmt.Println()
+      }
+      fmt.Println(&video)
+   }
+   return nil
+}
+
 func main() {
    var f flags
    err := f.New()
    if err != nil {
       panic(err)
    }
-   flag.Var(&f.show_id, "a", "address")
    flag.StringVar(&f.e.ClientId, "c", f.e.ClientId, "client ID")
-   flag.StringVar(&f.edit, "e", "", "edit")
-   flag.StringVar(&f.dash, "i", "", "DASH ID")
    flag.BoolVar(
       &f.initiate, "initiate", false, "/authentication/linkDevice/initiate",
    )
@@ -177,8 +117,11 @@ func main() {
       &f.login, "login", false, "/authentication/linkDevice/login",
    )
    flag.BoolVar(&f.proxy, "p", false, "proxy")
-   flag.IntVar(&f.season, "s", 0, "season")
    flag.IntVar(&internal.ThreadCount, "t", 1, "thread count")
+   flag.StringVar(&f.address, "a", "address")
+   flag.IntVar(&f.season, "s", 0, "season")
+   flag.StringVar(&f.edit, "e", "", "edit ID")
+   flag.StringVar(&f.dash, "i", "", "DASH ID")
    flag.Parse()
    if f.proxy {
       http.DefaultClient.Transport = &proxy.Transport{
@@ -196,13 +139,18 @@ func main() {
       if err != nil {
          panic(err)
       }
-   case f.show_id != "":
-      err := f.do_edit()
+   case f.address != "":
+      err := f.do_address()
       if err != nil {
          panic(err)
       }
    case f.edit != "":
-      err := f.do_mpd()
+      err := f.do_edit()
+      if err != nil {
+         panic(err)
+      }
+   case f.dash != "":
+      err := f.do_dash()
       if err != nil {
          panic(err)
       }
@@ -210,3 +158,111 @@ func main() {
       flag.Usage()
    }
 }
+
+type flags struct {
+   e        internal.License
+   initiate bool
+   login    bool
+   proxy    bool
+   media    string
+   
+   address string
+   season   int
+   
+   edit     string
+   
+   dash     string
+}
+
+///
+
+func (f *flags) do_edit() error {
+   if f.dash != "" {
+      data, err := os.ReadFile(f.media + "/max/Playback")
+      if err != nil {
+         return err
+      }
+      var play max.Playback
+      err = play.Unmarshal(data)
+      if err != nil {
+         return err
+      }
+      f.e.Widevine = func(data []byte) ([]byte, error) {
+         return play.Widevine(data)
+      }
+      return f.e.Download(f.media+"/Mpd", f.dash)
+   }
+   data, err := os.ReadFile(f.media + "/max/Login")
+   if err != nil {
+      return err
+   }
+   var login max.Login
+   err = login.Unmarshal(data)
+   if err != nil {
+      return err
+   }
+   data, err = login.Playback(f.edit)
+   if err != nil {
+      return err
+   }
+   var play max.Playback
+   err = play.Unmarshal(data)
+   if err != nil {
+      return err
+   }
+   err = write_file(f.media + "/max/Playback", data)
+   if err != nil {
+      return err
+   }
+   resp, err := http.Get(play.Mpd())
+   if err != nil {
+      return err
+   }
+   return internal.Mpd(f.media+"/Mpd", resp)
+}
+
+func (f *flags) do_dash() error {
+   if f.dash != "" {
+      data, err := os.ReadFile(f.media + "/max/Playback")
+      if err != nil {
+         return err
+      }
+      var play max.Playback
+      err = play.Unmarshal(data)
+      if err != nil {
+         return err
+      }
+      f.e.Widevine = func(data []byte) ([]byte, error) {
+         return play.Widevine(data)
+      }
+      return f.e.Download(f.media+"/Mpd", f.dash)
+   }
+   data, err := os.ReadFile(f.media + "/max/Login")
+   if err != nil {
+      return err
+   }
+   var login max.Login
+   err = login.Unmarshal(data)
+   if err != nil {
+      return err
+   }
+   data, err = login.Playback(f.edit)
+   if err != nil {
+      return err
+   }
+   var play max.Playback
+   err = play.Unmarshal(data)
+   if err != nil {
+      return err
+   }
+   err = write_file(f.media + "/max/Playback", data)
+   if err != nil {
+      return err
+   }
+   resp, err := http.Get(play.Mpd())
+   if err != nil {
+      return err
+   }
+   return internal.Mpd(f.media+"/Mpd", resp)
+}
+
