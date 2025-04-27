567afc23c3096e7e035776059d5aed238e026731
367 <367@tuta.io>
2025-04-27 15:47:45 -0500

    +func season_episodes(id int64) (*child, error) {

--- blog/amc/amc_test.go
+++ blog/amc/amc_test.go
@@ -1,23 +1,30 @@
 package amc
 
 import (
-   "os"
+   "fmt"
    "testing"
 )
 
+var show = struct{
+   id int64
+   url string
+}{
+   id: 1010578,
+   url: "amcplus.com/shows/orphan-black--1010578",
+}
+
 func Test(t *testing.T) {
-   resp, err := series_detail()
+   series, err := series_detail(show.id)
    if err != nil {
       t.Fatal(err)
    }
-   defer resp.Body.Close()
-   file, err := os.Create("amc.json")
-   if err != nil {
-      t.Fatal(err)
-   }
-   defer file.Close()
-   _, err = file.ReadFrom(resp.Body)
-   if err != nil {
-      t.Fatal(err)
+   var line bool
+   for season := range series.seasons() {
+      if line {
+         fmt.Println()
+      } else {
+         line = true
+      }
+      fmt.Println(season)
    }
 }
