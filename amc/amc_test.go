4fed0c96822512ff607ba1d1f340ae50de710509
367 <367@tuta.io>
2025-04-27 22:52:44 -0500

    deleted:    internal/amc/0.go

--- amc/amc_test.go
+++ amc/amc_test.go
@@ -5,51 +5,12 @@ import (
    "testing"
 )
 
-/*
-flag.StringVar(&series, "series", "", "series ID")
-flag.StringVar(&season, "s", "", "season ID")
-flag.StringVar(&episode, "e", "", "episode or movie ID")
-flag.StringVar(&dash, "d", "", "DASH ID")
-*/
-
-func TestSeasons(t *testing.T) {
-   series, err := series_detail(show.id)
-   if err != nil {
-      t.Fatal(err)
-   }
-   var line bool
-   for season := range series.seasons() {
-      if line {
-         fmt.Println()
-      } else {
-         line = true
-      }
-      fmt.Println(&season.Properties.Metadata)
-   }
-}
-
-func TestEpisodes(t *testing.T) {
-   season, err := season_episodes(season.id)
-   if err != nil {
-      t.Fatal(err)
-   }
-   var line bool
-   for episode := range season.episodes() {
-      if line {
-         fmt.Println()
-      } else {
-         line = true
-      }
-      fmt.Println(&episode.Properties.Metadata)
-   }
-}
-
 type test struct {
    id int64
    url string
 }
 
-var show = test{
+var series = test{
    id:  1010578,
    url: "amcplus.com/shows/orphan-black--1010578",
 }
--- internal/amc/amc.go
+++ internal/amc/amc.go
@@ -5,11 +5,59 @@ import (
    "41.neocities.org/media/internal"
    "errors"
    "flag"
+   "fmt"
    "log"
    "net/http"
    "os"
    "path/filepath"
 )
+
+func (f *flags) do_season() error {
+   season, err := amc.SeasonEpisodes(f.season)
+   if err != nil {
+      return err
+   }
+   var line bool
+   for episode := range season.Episodes() {
+      if line {
+         fmt.Println()
+      } else {
+         line = true
+      }
+      fmt.Println(&episode.Properties.Metadata)
+   }
+   return nil
+}
+
+func (f *flags) do_series() error {
+   series, err := amc.SeriesDetail(f.series)
+   if err != nil {
+      return err
+   }
+   var line bool
+   for season := range series.Seasons() {
+      if line {
+         fmt.Println()
+      } else {
+         line = true
+      }
+      fmt.Println(&season.Properties.Metadata)
+   }
+   return nil
+}
+
+func (f *flags) do_email() error {
+   var auth amc.Auth
+   err := auth.Unauth()
+   if err != nil {
+      return err
+   }
+   data, err := auth.Login(f.email, f.password)
+   if err != nil {
+      return err
+   }
+   return write_file(f.media+"/amc/Auth", data)
+}
 
 func main() {
    var f flags
@@ -22,21 +70,32 @@ func main() {
    flag.StringVar(&f.e.PrivateKey, "key", f.e.PrivateKey, "private key")
    flag.StringVar(&f.password, "password", "", "password")
    
-   flag.Int64Var(&f.episode, "e", 0, "episode or movie ID")
-   flag.Int64Var(&f.season, "s", 0, "season ID")
    flag.Int64Var(&f.series, "series", 0, "series ID")
-   
+   flag.Int64Var(&f.season, "s", 0, "season ID")
+   flag.Int64Var(&f.episode, "e", 0, "episode or movie ID")
+   flag.StringVar(&f.dash, "d", "", "DASH ID")
    
    flag.Parse()
+   
    if f.email != "" {
       if f.password != "" {
-         err := f.do_email_password()
+         err := f.do_email()
          if err != nil {
             panic(err)
          }
       }
-   } else if f.amc >= 1 {
-      err := f.do_amc()
+   } else if f.series >= 1 {
+      err := f.do_series()
+      if err != nil {
+         panic(err)
+      }
+   } else if f.season >= 1 {
+      err := f.do_season()
+      if err != nil {
+         panic(err)
+      }
+   } else if f.episode >= 1 {
+      err := f.do_episode()
       if err != nil {
          panic(err)
       }
@@ -50,7 +109,35 @@ func main() {
    }
 }
 
-func (f *flags) do_amc() error {
+type flags struct {
+   e        internal.License
+   email    string
+   password string
+   media    string
+   
+   series  int64
+   season  int64
+   episode int64
+   dash    string
+}
+
+func (f *flags) New() error {
+   var err error
+   f.media, err = os.UserHomeDir()
+   if err != nil {
+      return err
+   }
+   f.media = filepath.ToSlash(f.media) + "/media"
+   f.e.ClientId = f.media + "/client_id.bin"
+   f.e.PrivateKey = f.media + "/private_key.pem"
+   return nil
+}
+
+func write_file(name string, data []byte) error {
+   log.Println("WriteFile", name)
+   return os.WriteFile(name, data, os.ModePerm)
+}
+func (f *flags) do_episode() error {
    data, err := os.ReadFile(f.media + "/amc/Auth")
    if err != nil {
       return err
@@ -72,7 +159,7 @@ func (f *flags) do_amc() error {
    if err != nil {
       return err
    }
-   data, err = auth.Playback(f.amc)
+   data, err = auth.Playback(f.episode)
    if err != nil {
       return err
    }
