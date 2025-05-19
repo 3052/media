8e6b95aa54736bf455a82cadac0b8d212d9eabe8
367 <367@tuta.io>
2025-05-18 22:56:26 -0500

    +func (f *flags) New() error {

--- internal/amc/amc.go
+++ internal/amc/amc.go
@@ -12,7 +12,24 @@ import (
    "path/filepath"
 )
 
-func (f *flags) do_episode() error {
+func (f *flags) New() error {
+   var err error
+   f.media, err = os.UserHomeDir()
+   if err != nil {
+      return err
+   }
+   f.media = filepath.ToSlash(f.media) + "/media"
+   f.e.ClientId = f.media + "/client_id.bin"
+   f.e.PrivateKey = f.media + "/private_key.pem"
+   return nil
+}
+
+func write_file(name string, data []byte) error {
+   log.Println("WriteFile", name)
+   return os.WriteFile(name, data, os.ModePerm)
+}
+
+func (f *flags) do_refresh() error {
    data, err := os.ReadFile(f.media + "/amc/Auth")
    if err != nil {
       return err
@@ -22,57 +39,24 @@ func (f *flags) do_episode() error {
    if err != nil {
       return err
    }
-   data, err = auth.Playback(f.episode)
+   data, err = auth.Refresh()
    if err != nil {
       return err
    }
-   err = write_file(f.media+"/amc/Playback", data)
-   if err != nil {
-      return err
-   }
-   var play amc.Playback
-   err = play.Unmarshal(data)
-   if err != nil {
-      return err
-   }
-   source, ok := play.Dash()
-   if !ok {
-      return errors.New(".Dash()")
-   }
-   resp, err := http.Get(source.Src)
-   if err != nil {
-      return err
-   }
-   return net.Mpd(f.media+"/Mpd", resp)
+   return write_file(f.media+"/amc/Auth", data)
 }
 
-func (f *flags) do_dash() error {
-   data, err := os.ReadFile(f.media + "/amc/Playback")
+func (f *flags) do_email() error {
+   var auth amc.Auth
+   err := auth.Unauth()
    if err != nil {
       return err
    }
-   var play amc.Playback
-   err = play.Unmarshal(data)
+   data, err := auth.Login(f.email, f.password)
    if err != nil {
       return err
    }
-   source, _ := play.Dash()
-   f.e.Widevine = func(data []byte) ([]byte, error) {
-      return play.Widevine(source, data)
-   }
-   return f.e.Download(f.media+"/Mpd", f.dash)
-}
-
-type flags struct {
-   e        net.License
-   media    string
-   email    string
-   password string
-   refresh  bool
-   series   int64
-   season   int64
-   episode  int64
-   dash     string
+   return write_file(f.media+"/amc/Auth", data)
 }
 
 func (f *flags) do_season() error {
@@ -127,7 +111,71 @@ func (f *flags) do_series() error {
    return nil
 }
 
-func (f *flags) do_refresh() error {
+func main() {
+   var f flags
+   err := f.New()
+   if err != nil {
+      panic(err)
+   }
+   flag.StringVar(&f.e.ClientId, "client", f.e.ClientId, "client ID")
+   flag.StringVar(&f.e.PrivateKey, "key", f.e.PrivateKey, "private key")
+   //////////////////////////////////////////////////////////////////////////////
+   flag.StringVar(&f.email, "email", "", "email")
+   flag.StringVar(&f.password, "password", "", "password")
+   //////////////////////////////////////////////////////////////////////////////
+   flag.BoolVar(&f.refresh, "r", false, "refresh")
+   //////////////////////////////////////////////////////////////////////////////
+   flag.Int64Var(&f.series, "series", 0, "series ID")
+   //////////////////////////////////////////////////////////////////////////////
+   flag.Int64Var(&f.season, "s", 0, "season ID")
+   //////////////////////////////////////////////////////////////////////////////
+   flag.Int64Var(&f.episode, "e", 0, "episode or movie ID")
+   flag.Float64Var(&f.tolerance, "t", 0.2, "tolerance")
+   flag.Func("b", "bandwidth", func(data string) error {
+      return f.bandwidth.Set(data)
+   })
+   flag.Parse()
+   if f.email != "" {
+      if f.password != "" {
+         err = f.do_email()
+      }
+   } else if f.refresh {
+      err = f.do_refresh()
+   } else if f.series >= 1 {
+      err = f.do_series()
+   } else if f.season >= 1 {
+      err = f.do_season()
+   } else if f.episode >= 1 {
+      err = f.do_episode()
+   } else {
+      flag.Usage()
+   }
+   if err != nil {
+      panic(err)
+   }
+}
+
+type flags struct {
+   e        net.License
+   media    string
+   /////////////////////////
+   email    string
+   password string
+   /////////////////////////
+   refresh  bool
+   /////////////////////////
+   series   int64
+   /////////////////////////
+   season   int64
+   /////////////////////////
+   episode  int64
+   tolerance float64
+   bandwidth net.Bandwidth
+}
+
+///
+
+func (f *flags) do_episode() error {
    data, err := os.ReadFile(f.media + "/amc/Auth")
    if err != nil {
       return err
@@ -137,92 +185,44 @@ func (f *flags) do_refresh() error {
    if err != nil {
       return err
    }
-   data, err = auth.Refresh()
+   data, err = auth.Playback(f.episode)
    if err != nil {
       return err
    }
-   return write_file(f.media+"/amc/Auth", data)
-}
-
-func (f *flags) do_email() error {
-   var auth amc.Auth
-   err := auth.Unauth()
+   err = write_file(f.media+"/amc/Playback", data)
    if err != nil {
       return err
    }
-   data, err := auth.Login(f.email, f.password)
+   var play amc.Playback
+   err = play.Unmarshal(data)
    if err != nil {
       return err
    }
-   return write_file(f.media+"/amc/Auth", data)
-}
-
-func main() {
-   var f flags
-   err := f.New()
-   if err != nil {
-      panic(err)
+   source, ok := play.Dash()
+   if !ok {
+      return errors.New(".Dash()")
    }
-   flag.StringVar(&f.e.ClientId, "client", f.e.ClientId, "client ID")
-   flag.StringVar(&f.dash, "d", "", "DASH ID")
-   flag.Int64Var(&f.episode, "e", 0, "episode or movie ID")
-   flag.StringVar(&f.email, "email", "", "email")
-   flag.StringVar(&f.e.PrivateKey, "key", f.e.PrivateKey, "private key")
-   flag.StringVar(&f.password, "password", "", "password")
-   flag.BoolVar(&f.refresh, "r", false, "refresh")
-   flag.Int64Var(&f.season, "s", 0, "season ID")
-   flag.Int64Var(&f.series, "series", 0, "series ID")
-   flag.Parse()
-   if f.email != "" {
-      if f.password != "" {
-         err := f.do_email()
-         if err != nil {
-            panic(err)
-         }
-      }
-   } else if f.refresh {
-      err := f.do_refresh()
-      if err != nil {
-         panic(err)
-      }
-   } else if f.series >= 1 {
-      err := f.do_series()
-      if err != nil {
-         panic(err)
-      }
-   } else if f.season >= 1 {
-      err := f.do_season()
-      if err != nil {
-         panic(err)
-      }
-   } else if f.episode >= 1 {
-      err := f.do_episode()
-      if err != nil {
-         panic(err)
-      }
-   } else if f.dash != "" {
-      err := f.do_dash()
-      if err != nil {
-         panic(err)
-      }
-   } else {
-      flag.Usage()
-   }
-}
-
-func (f *flags) New() error {
-   var err error
-   f.media, err = os.UserHomeDir()
+   resp, err := http.Get(source.Src)
    if err != nil {
       return err
    }
-   f.media = filepath.ToSlash(f.media) + "/media"
-   f.e.ClientId = f.media + "/client_id.bin"
-   f.e.PrivateKey = f.media + "/private_key.pem"
-   return nil
+   return net.Mpd(f.media+"/Mpd", resp)
 }
 
-func write_file(name string, data []byte) error {
-   log.Println("WriteFile", name)
-   return os.WriteFile(name, data, os.ModePerm)
+func (f *flags) do_dash() error {
+   data, err := os.ReadFile(f.media + "/amc/Playback")
+   if err != nil {
+      return err
+   }
+   var play amc.Playback
+   err = play.Unmarshal(data)
+   if err != nil {
+      return err
+   }
+   source, _ := play.Dash()
+   f.e.Widevine = func(data []byte) ([]byte, error) {
+      return play.Widevine(source, data)
+   }
+   return f.e.Download(f.media+"/Mpd", f.dash)
 }
+
