0ad19a17f234a148a88ad2a6610d482848c68fa6
367 <367@tuta.io>
2025-05-20 08:56:13 -0500

    +   flag.IntVar(&net.Threads, "threads", 1, "threads")

--- internal/tubi/tubi.go
+++ internal/tubi/tubi.go
@@ -15,14 +15,6 @@ func write_file(name string, data []byte) error {
    return os.WriteFile(name, data, os.ModePerm)
 }
 
-type flags struct {
-   e     net.License
-   media string
-
-   tubi int
-   dash string
-}
-
 func main() {
    var f flags
    err := f.New()
@@ -30,59 +22,28 @@ func main() {
       panic(err)
    }
    flag.StringVar(&f.e.ClientId, "c", f.e.ClientId, "client ID")
-   flag.StringVar(&f.dash, "d", "", "DASH ID")
    flag.StringVar(&f.e.PrivateKey, "p", f.e.PrivateKey, "private key")
+   flag.IntVar(&net.Threads, "threads", 1, "threads")
+   ///////////////////////////////////////////////////////////////
    flag.IntVar(&f.tubi, "t", 0, "Tubi ID")
-   flag.IntVar(&net.ThreadCount, "thread", 1, "thread count")
+   flag.Var(&f.bitrate, "b", "bitrate")
    flag.Parse()
-   switch {
-   case f.tubi >= 1:
+   if f.tubi >= 1 {
       err = f.do_tubi()
-   case f.dash != "":
-      err = f.do_dash()
-   default:
+      if err != nil {
+         panic(err)
+      }
+   } else {
       flag.Usage()
    }
-   if err != nil {
-      panic(err)
-   }
 }
 
-func (f *flags) do_tubi() error {
-   data, err := tubi.NewContent(f.tubi)
-   if err != nil {
-      return err
-   }
-   var content tubi.Content
-   err = content.Unmarshal(data)
-   if err != nil {
-      return err
-   }
-   err = write_file(f.media+"/tubi/Content", data)
-   if err != nil {
-      return err
-   }
-   resp, err := http.Get(content.VideoResources[0].Manifest.Url)
-   if err != nil {
-      return err
-   }
-   return net.Mpd(f.media+"/Mpd", resp)
-}
-
-func (f *flags) do_dash() error {
-   data, err := os.ReadFile(f.media + "/tubi/Content")
-   if err != nil {
-      return err
-   }
-   var content tubi.Content
-   err = content.Unmarshal(data)
-   if err != nil {
-      return err
-   }
-   f.e.Widevine = func(data []byte) ([]byte, error) {
-      return content.VideoResources[0].Widevine(data)
-   }
-   return f.e.Download(f.media+"/Mpd", f.dash)
+type flags struct {
+   e     net.License
+   media string
+   ////////////////////////
+   tubi int
+   bitrate net.Bitrate
 }
 
 func (f *flags) New() error {
@@ -95,4 +56,24 @@ func (f *flags) New() error {
    f.e.ClientId = f.media + "/client_id.bin"
    f.e.PrivateKey = f.media + "/private_key.pem"
    return nil
+}
+
+func (f *flags) do_tubi() error {
+   data, err := tubi.NewContent(f.tubi)
+   if err != nil {
+      return err
+   }
+   var content tubi.Content
+   err = content.Unmarshal(data)
+   if err != nil {
+      return err
+   }
+   resp, err := http.Get(content.VideoResources[0].Manifest.Url)
+   if err != nil {
+      return err
+   }
+   f.e.Widevine = func(data []byte) ([]byte, error) {
+      return content.VideoResources[0].Widevine(data)
+   }
+   return f.e.Bitrate(resp, &f.bitrate)
 }
