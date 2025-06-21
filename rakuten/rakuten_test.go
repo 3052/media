d3417b63680eb8b8fa7ee024a97648a0086604d1
367 <367@tuta.io>
2025-06-21 00:01:50 -0500

    +   data, err := exec.Command("password", "-i", "nordvpn.com").Output()

--- rakuten/rakuten_test.go
+++ rakuten/rakuten_test.go
@@ -2,13 +2,29 @@ package rakuten
 
 import (
    "fmt"
+   "net/http"
+   "net/url"
+   "os/exec"
+   "strings"
    "testing"
 )
 
 func Test(t *testing.T) {
+   data, err := exec.Command("password", "-i", "nordvpn.com").Output()
+   if err != nil {
+      t.Fatal(err)
+   }
+   username, password, _ := strings.Cut(string(data), ":")
+   http.DefaultTransport = &http.Transport{
+      Proxy: http.ProxyURL(&url.URL{
+         Scheme: "https",
+         User: url.UserPassword(username, password),
+         Host: "cz103.nordvpn.com:89",
+      }),
+   }
    test := web_tests[0]
    var web Address
-   err := web.Set(test.url)
+   err = web.Set(test.url)
    if err != nil {
       t.Fatal(err)
    }
