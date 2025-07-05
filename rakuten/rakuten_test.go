4eaca164c2e2467843447c3978f7ced10c350ed8
367 <367@tuta.io>
2025-07-05 00:15:00 -0500

    deleted:    internal/rakuten/main.go

--- rakuten/rakuten_test.go
+++ rakuten/rakuten_test.go
@@ -9,6 +9,32 @@ import (
    "testing"
 )
 
+var web_tests = []struct{
+   language string
+   url      string
+}{
+   {
+      language: "ENG",
+      url:      "//rakuten.tv/uk?content_type=tv_shows&tv_show_id=clink",
+   },
+   {
+      language: "ENG",
+      url:      "//rakuten.tv/fr?content_type=movies&content_id=infidele",
+   },
+   {
+      language: "ENG",
+      url:      "//rakuten.tv/pl?content_type=movies&content_id=ad-astra",
+   },
+   {
+      language: "ENG",
+      url:      "//rakuten.tv/se?content_type=movies&content_id=i-heart-huckabees",
+   },
+   {
+      language: "SPA",
+      url:      "//rakuten.tv/cz?content_type=movies&content_id=transvulcania-the-people-s-run",
+   },
+}
+
 func Test(t *testing.T) {
    data, err := exec.Command("password", "-i", "nordvpn.com").Output()
    if err != nil {
@@ -43,32 +69,4 @@ func Test(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-}
-
-var web_tests = []web_test{
-   {
-      language: "SPA",
-      url:      "//rakuten.tv/cz?content_type=movies&content_id=transvulcania-the-people-s-run",
-   },
-   {
-      language: "ENG",
-      url:      "//rakuten.tv/fr?content_type=movies&content_id=infidele",
-   },
-   {
-      language: "ENG",
-      url:      "//rakuten.tv/pl?content_type=movies&content_id=ad-astra",
-   },
-   {
-      language: "ENG",
-      url:      "//rakuten.tv/se?content_type=movies&content_id=i-heart-huckabees",
-   },
-   {
-      language: "ENG",
-      url:      "//rakuten.tv/uk?content_type=tv_shows&tv_show_id=clink",
-   },
-}
-
-type web_test struct {
-   language string
-   url      string
 }
