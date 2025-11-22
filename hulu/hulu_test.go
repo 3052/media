aebfad6c1d16b2913fde2a9d503fcc46a2585a76
27 <27@riseup.net>
2025-11-22 16:21:31 -0600

    +      url:     "hulu.com/watch/f70dfd4d-dbfb-46b8-abb3-136c841bba11",

--- hulu/hulu_test.go
+++ hulu/hulu_test.go
@@ -5,6 +5,22 @@ import (
    "testing"
 )
 
+var tests = []struct {
+   content string
+   id      string
+   url     string
+}{
+   {
+      content: "film",
+      id:      "EAB::f70dfd4d-dbfb-46b8-abb3-136c841bba11::61556664::101167038",
+      url:     "hulu.com/watch/f70dfd4d-dbfb-46b8-abb3-136c841bba11",
+   },
+   {
+      content: "episode",
+      url:     "hulu.com/watch/023c49bf-6a99-4c67-851c-4c9e7609cc1d",
+   },
+}
+
 func TestPlayReady(t *testing.T) {
    cache, err := os.UserCacheDir()
    if err != nil {
@@ -34,20 +50,4 @@ func TestPlayReady(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-}
-
-var tests = []struct {
-   content string
-   id      string
-   url     string
-}{
-   {
-      content: "film",
-      id:      "EAB::f70dfd4d-dbfb-46b8-abb3-136c841bba11::61556664::101167038",
-      url:     "hulu.com/watch/f70dfd4d-dbfb-46b8-abb3-136c841bba11",
-   },
-   {
-      content: "episode",
-      url:     "hulu.com/watch/023c49bf-6a99-4c67-851c-4c9e7609cc1d",
-   },
 }
