f105e938a40c6afbb271444fe85d49cb91e0f60f
27 <27@riseup.net>
2025-12-06 15:56:07 -0600

    deleted:    internal/amc/main.go

--- internal/amc/amc.go
+++ internal/amc/amc.go
@@ -1,16 +1,90 @@
 package main
 
 import (
-   "encoding/json"
-   "errors"
-   "fmt"
-   "log"
-   "os"
-
    "41.neocities.org/media/amc"
    "41.neocities.org/net"
+   "encoding/json"
+   "errors"
+   "flag"
+   "fmt"
+   "log"
+   "net/http"
+   "os"
+   "path"
+   "path/filepath"
 )
 
+func main() {
+   log.SetFlags(log.Ltime)
+   net.Transport(func(req *http.Request) string {
+      if path.Ext(req.URL.Path) == ".m4f" {
+         return ""
+      }
+      return "LP"
+   })
+   var program runner
+   err := program.run()
+   if err != nil {
+      log.Fatal(err)
+   }
+}
+
+func (r *runner) run() error {
+   var err error
+   r.cache, err = os.UserCacheDir()
+   if err != nil {
+      return err
+   }
+   r.cache = filepath.ToSlash(r.cache)
+   r.config.ClientId = r.cache + "/L3/client_id.bin"
+   r.config.PrivateKey = r.cache + "/L3/private_key.pem"
+
+   flag.StringVar(&r.email, "E", "", "email")
+   flag.StringVar(&r.password, "P", "", "password")
+   flag.Int64Var(&r.series, "S", 0, "series ID")
+   flag.StringVar(&r.config.ClientId, "c", r.config.ClientId, "client ID")
+   flag.StringVar(&r.dash, "d", "", "DASH ID")
+   flag.Int64Var(&r.episode, "e", 0, "episode or movie ID")
+   flag.StringVar(&r.config.PrivateKey, "p", r.config.PrivateKey, "private key")
+   flag.BoolVar(&r.refresh, "r", false, "refresh")
+   flag.Int64Var(&r.season, "s", 0, "season ID")
+   flag.Parse()
+
+   if r.email != "" {
+      if r.password != "" {
+         return r.do_auth()
+      }
+   }
+   if r.refresh {
+      return r.do_refresh()
+   }
+   if r.series >= 1 {
+      return r.do_series()
+   }
+   if r.season >= 1 {
+      return r.do_season()
+   }
+   if r.episode >= 1 {
+      return r.do_episode()
+   }
+   if r.dash != "" {
+      return r.do_dash()
+   }
+   flag.Usage()
+   return nil
+}
+
+type runner struct {
+   config   net.Config
+   dash     string
+   email    string
+   episode  int64
+   password string
+   refresh  bool
+   season   int64
+   series   int64
+   cache string
+}
 func (r *runner) read() (*amc.Cache, error) {
    data, err := os.ReadFile(r.cache + "/amc/Cache")
    if err != nil {
--- internal/paramount/paramount.go
+++ internal/paramount/paramount.go
@@ -12,58 +12,8 @@ import (
    "path/filepath"
 )
 
-// INTL does NOT allow anonymous key request, so if you are INTL you
-// will need to use US VPN until someone codes the INTL login
-func (f *flag_set) do_dash() error {
-   data, err := os.ReadFile(f.cache + "/paramount/Cache")
-   if err != nil {
-      return err
-   }
-   var cache paramount.Cache
-   err = json.Unmarshal(data, &cache)
-   if err != nil {
-      return err
-   }
-   at, err := paramount.ComCbsApp.At()
-   if err != nil {
-      return err
-   }
-   token, err := at.Token(f.paramount)
-   if err != nil {
-      return err
-   }
-   f.config.Send = func(data []byte) ([]byte, error) {
-      return token.Widevine(data)
-   }
-   return f.config.Download(cache.MpdBody, cache.Mpd, f.dash)
-}
-
-func (f *flag_set) secret() paramount.AppSecret {
-   if f.intl {
-      return paramount.ComCbsCa
-   }
-   return paramount.ComCbsApp
-}
-
-func (f *flag_set) New() error {
-   var err error
-   f.cache, err = os.UserCacheDir()
-   if err != nil {
-      return err
-   }
-   f.cache = filepath.ToSlash(f.cache)
-   f.config.ClientId = f.cache + "/L3/client_id.bin"
-   f.config.PrivateKey = f.cache + "/L3/private_key.pem"
-   flag.StringVar(&f.config.ClientId, "C", f.config.ClientId, "client ID")
-   flag.StringVar(&f.config.PrivateKey, "P", f.config.PrivateKey, "private key")
-   flag.BoolVar(&f.intl, "i", false, "intl")
-   flag.StringVar(&f.paramount, "p", "", "paramount ID")
-   flag.StringVar(&f.dash, "d", "", "DASH ID")
-   flag.Parse()
-   return nil
-}
-
 func main() {
+   log.SetFlags(log.Ltime)
    net.Transport(func(req *http.Request) string {
       switch path.Ext(req.URL.Path) {
       case ".m4s", ".mp4":
@@ -75,45 +25,62 @@ func main() {
       }
       return "LP"
    })
-   log.SetFlags(log.Ltime)
-   var set flag_set
-   err := set.New()
-   if err != nil {
-      log.Fatal(err)
-   }
-   switch {
-   case set.paramount != "":
-      err = set.do_paramount()
-   case set.dash != "":
-      err = set.do_dash()
-   default:
-      flag.Usage()
-   }
+   var program runner
+   err := program.run()
    if err != nil {
       log.Fatal(err)
    }
 }
 
-type flag_set struct {
+func (r *runner) run() error {
+   cache, err := os.UserCacheDir()
+   if err != nil {
+      return err
+   }
+   cache = filepath.ToSlash(cache)
+   r.config.ClientId = cache + "/L3/client_id.bin"
+   r.config.PrivateKey = cache + "/L3/private_key.pem"
+   r.cache = cache + "/paramount/Cache.json"
+   
+   flag.StringVar(&r.config.ClientId, "C", r.config.ClientId, "client ID")
+   flag.StringVar(&r.config.PrivateKey, "P", r.config.PrivateKey, "private key")
+   flag.StringVar(&r.dash, "d", "", "DASH ID")
+   flag.BoolVar(&r.intl, "i", false, "intl")
+   flag.StringVar(&r.paramount, "p", "", "paramount ID")
+   flag.Parse()
+   if r.paramount != "" {
+      return r.do_paramount()
+   }
+   if r.dash != "" {
+      return r.do_dash()
+   }
+   flag.Usage()
+   return nil
+}
+
+type runner struct {
    cache     string
    config    net.Config
-   intl      bool
    // 1
    paramount string
+   intl      bool
    // 2
    dash      string
 }
+
+///
 
 func write_file(name string, data []byte) error {
    log.Println("WriteFile", name)
    return os.WriteFile(name, data, os.ModePerm)
 }
-func (f *flag_set) do_paramount() error {
-   at, err := f.secret().At()
+
+func (r *runner) do_paramount() error {
+   at, err := r.secret().At()
    if err != nil {
       return err
    }
-   item, err := at.Item(f.paramount)
+   item, err := at.Item(r.paramount)
    if err != nil {
       return err
    }
@@ -126,9 +93,42 @@ func (f *flag_set) do_paramount() error {
    if err != nil {
       return err
    }
-   err = write_file(f.cache + "/paramount/Cache", data)
+   err = write_file(r.cache + "/paramount/Cache", data)
    if err != nil {
       return err
    }
    return net.Representations(cache.MpdBody, cache.Mpd)
+}
+
+// INTL does NOT allow anonymous key request, so if you are INTL you
+// will need to use US VPN until someone codes the INTL login
+func (r *runner) do_dash() error {
+   data, err := os.ReadFile(r.cache + "/paramount/Cache")
+   if err != nil {
+      return err
+   }
+   var cache paramount.Cache
+   err = json.Unmarshal(data, &cache)
+   if err != nil {
+      return err
+   }
+   at, err := paramount.ComCbsApp.At()
+   if err != nil {
+      return err
+   }
+   token, err := at.Token(r.paramount)
+   if err != nil {
+      return err
+   }
+   r.config.Send = func(data []byte) ([]byte, error) {
+      return token.Widevine(data)
+   }
+   return r.config.Download(cache.MpdBody, cache.Mpd, r.dash)
+}
+
+func (r *runner) secret() paramount.AppSecret {
+   if r.intl {
+      return paramount.ComCbsCa
+   }
+   return paramount.ComCbsApp
 }
