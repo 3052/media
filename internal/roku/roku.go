2ebbe77aefd119b2dd7ecdc8f542b1c9e844c470
27 <27@riseup.net>
2025-12-06 14:32:31 -0600

    +   flag.BoolVar(&r.get_user, "g", false, "get user")

--- internal/roku/roku.go
+++ internal/roku/roku.go
@@ -37,15 +37,15 @@ func (r *runner) run() error {
    flag.StringVar(&r.config.PrivateKey, "P", r.config.PrivateKey, "private key")
    flag.BoolVar(&r.connection, "c", false, "connection")
    flag.StringVar(&r.dash, "d", "", "DASH ID")
-   flag.BoolVar(&r.get_credentials, "g", false, "get credentials")
+   flag.BoolVar(&r.get_user, "g", false, "get user")
    flag.StringVar(&r.roku, "r", "", "Roku ID")
-   flag.BoolVar(&r.set_credentials, "s", false, "set credentials")
+   flag.BoolVar(&r.set_user, "s", false, "set user")
    flag.Parse()
    if r.connection {
       return r.do_connection()
    }
-   if r.set_credentials {
-      return r.do_credentials()
+   if r.set_user {
+      return r.do_user()
    }
    if r.roku != "" {
       return r.do_roku()
@@ -82,10 +82,10 @@ func (r *runner) read() (*roku.Cache, error) {
 func (r *runner) do_connection() error {
    var (
       cache roku.Cache
-      credentials *roku.Credentials
+      user *roku.user
       err error
    )
-   cache.Connection, err = credentials.NewConnection()
+   cache.Connection, err = user.NewConnection()
    if err != nil {
       return err
    }
@@ -97,88 +97,61 @@ func (r *runner) do_connection() error {
    return r.write(&cache)
 }
 
+func (r *runner) do_user() error {
+   cache, err := r.read()
+   if err != nil {
+      return err
+   }
+   cache.User, err = cache.Connection.GetUser(cache.LinkCode)
+   if err != nil {
+      return err
+   }
+   return r.write(cache)
+}
+
 type runner struct {
    cache       string
    config      net.Config
    // 1
    connection  bool
    // 2
-   set_credentials bool
+   set_user bool
    // 3
    roku        string
-   get_credentials  bool
+   get_user  bool
    // 4
    dash string
 }
 
-///
-
-func (r *runner) do_credentials() error {
-   data, err := os.ReadFile(r.cache + "/roku/AccountToken")
-   if err != nil {
-      return err
-   }
-   var token roku.AccountToken
-   err = token.Unmarshal(data)
-   if err != nil {
-      return err
-   }
-   data, err = os.ReadFile(r.cache + "/roku/Activation")
-   if err != nil {
-      return err
-   }
-   var activation roku.Activation
-   err = activation.Unmarshal(data)
-   if err != nil {
-      return err
-   }
-   data, err = token.Code(&activation)
-   if err != nil {
-      return err
-   }
-   return write_file(r.cache+"/roku/Code", data)
-}
-
-func write_file(name string, data []byte) error {
-   log.Println("WriteFile", name)
-   return os.WriteFile(name, data, os.ModePerm)
-}
-
 func (r *runner) do_roku() error {
-   var code *roku.Code
-   if r.get_credentials {
-      data, err := os.ReadFile(r.cache + "/roku/Code")
-      if err != nil {
-         return err
-      }
-      code = &roku.Code{}
-      err = code.Unmarshal(data)
-      if err != nil {
-         return err
-      }
-   }
-   data, err := code.AccountToken()
-   if err != nil {
-      return err
-   }
-   var token roku.AccountToken
-   err = token.Unmarshal(data)
-   if err != nil {
-      return err
-   }
-   data1, err := token.Playback(r.roku)
-   if err != nil {
-      return err
-   }
-   var play roku.Playback
-   err = play.Unmarshal(data1)
-   if err != nil {
-      return err
-   }
-   resp, err := http.Get(play.Url)
-   if err != nil {
-      return err
-   }
+    cache := &roku.Cache{} 
+    if r.get_user {
+        var err error
+        cache, err = r.read()
+        if err != nil {
+            return err
+        }
+    }
+    connection, err := cache.User.NewConnection()
+    if err != nil {
+        return err
+    }
+    playback, err := connection.Playback(r.roku)
+    if err != nil {
+        return err
+    }
+    err = playback.Mpd(cache)
+    if err != nil {
+        return err
+    }
+    err = r.write(cache)
+    if err != nil {
+        return err
+    }
+    return net.Representations(cache.MpdBody, cache.Mpd)
+}
+
+func (r *runner) do_dash() error {
    r.config.Send = func(data []byte) ([]byte, error) {
       return play.Widevine(data)
    }
--- roku/roku.go
+++ roku/roku.go
@@ -6,104 +6,30 @@ import (
    "errors"
    "io"
    "net/http"
+   "net/url"
    "strings"
 )
 
 type Cache struct {
-   Mpd     *url.URL
-   MpdBody []byte
    Connection *Connection
-   LinkCode *LinkCode
+   LinkCode   *LinkCode
+   Mpd        *url.URL
+   MpdBody    []byte
+   User       *User
 }
 
-// RequestLinkCode requests a new activation code from the server.
-func (c *Connection) RequestLinkCode() (*LinkCode, error) {
-   payload, err := json.Marshal(map[string]string{"platform": "googletv"})
+func (p *Playback) Mpd(storage *Cache) error {
+   resp, err := http.Get(p.Url)
    if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://googletv.web.roku.com/api/v1/account/activation",
-      bytes.NewReader(payload),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("content-type", "application/json")
-   req.Header.Set("user-agent", userAgent)
-   req.Header.Set("x-roku-content-token", c.AuthToken)
-
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
+      return err
    }
    defer resp.Body.Close()
-
-   link := &LinkCode{}
-   err = json.NewDecoder(resp.Body).Decode(link)
+   storage.MpdBody, err = io.ReadAll(resp.Body)
    if err != nil {
-      return nil, err
+      return err
    }
-   return link, nil
-}
-// NewConnection initializes a session. Credentials can be nil.
-func (c *Credentials) NewConnection() (*Connection, error) {
-   req, _ := http.NewRequest("", "https://googletv.web.roku.com", nil)
-   req.URL.Path = "/api/v1/account/token"
-   req.Header.Set("user-agent", userAgent)
-   if c != nil {
-      req.Header.Set("x-roku-content-token", c.Token)
-   }
-
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-
-   connector := &Connection{}
-   err = json.NewDecoder(resp.Body).Decode(connector)
-   if err != nil {
-      return nil, err
-   }
-   return connector, nil
-}
-
-// GetCredentials exchanges the LinkCode for permanent Credentials.
-func (c *Connection) GetCredentials(link *LinkCode) (*Credentials, error) {
-   req, _ := http.NewRequest("", "https://googletv.web.roku.com", nil)
-   req.URL.Path = "/api/v1/account/activation/" + link.Code
-   req.Header.Set("user-agent", userAgent)
-   req.Header.Set("x-roku-content-token", c.AuthToken)
-
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-
-   account := &Credentials{}
-   err = json.NewDecoder(resp.Body).Decode(account)
-   if err != nil {
-      return nil, err
-   }
-   return account, nil
-}
-const userAgent = "trc-googletv; production; 0"
-
-// Connection represents the active session with the Roku API.
-type Connection struct {
-   AuthToken string
-}
-
-// LinkCode represents the activation code displayed to the user.
-type LinkCode struct {
-   Code string
-}
-
-// Credentials represents the persistent saved account token.
-type Credentials struct {
-   Token string
+   storage.Mpd = resp.Request.URL
+   return nil
 }
 
 // Playback represents the media playback metadata.
@@ -116,6 +42,43 @@ type Playback struct {
    Url string // MPD
 }
 
+// GetUser exchanges the LinkCode for a permanent User token.
+func (c *Connection) GetUser(link *LinkCode) (*User, error) {
+   req, _ := http.NewRequest("", "https://googletv.web.roku.com", nil)
+   req.URL.Path = "/api/v1/account/activation/" + link.Code
+   req.Header.Set("user-agent", userAgent)
+   req.Header.Set("x-roku-content-token", c.AuthToken)
+
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+
+   currentUser := &User{}
+   err = json.NewDecoder(resp.Body).Decode(currentUser)
+   if err != nil {
+      return nil, err
+   }
+   return currentUser, nil
+}
+
+const userAgent = "trc-googletv; production; 0"
+
+// Connection represents the active session with the Roku API.
+type Connection struct {
+   AuthToken string
+}
+
+// LinkCode represents the activation code displayed to the user.
+type LinkCode struct {
+   Code string
+}
+
+// User represents the persistent saved account token.
+type User struct {
+   Token string
+}
 // Playback fetches the DASH manifest and DRM information.
 func (c *Connection) Playback(rokuId string) (*Playback, error) {
    payload, err := json.Marshal(map[string]string{
@@ -155,19 +118,7 @@ func (c *Connection) Playback(rokuId string) (*Playback, error) {
    return playResult, nil
 }
 
-// String generates the user instructions.
-func (l *LinkCode) String() string {
-   var output strings.Builder
-   output.WriteString("1 Visit the URL\n")
-   output.WriteString("  therokuchannel.com/link\n")
-   output.WriteString("\n")
-   output.WriteString("2 Enter the activation code\n")
-   output.WriteString("  ")
-   output.WriteString(l.Code)
-   return output.String()
-}
-
-// Widevine fetches the license key.
+// Widevine fetches the license key
 func (p *Playback) Widevine(payload []byte) ([]byte, error) {
    resp, err := http.Post(
       p.Drm.Widevine.LicenseServer, "application/x-protobuf",
@@ -180,3 +131,68 @@ func (p *Playback) Widevine(payload []byte) ([]byte, error) {
    return io.ReadAll(resp.Body)
 }
 
+// String generates the user instructions
+func (l *LinkCode) String() string {
+   var output strings.Builder
+   output.WriteString("1 Visit the URL\n")
+   output.WriteString("  therokuchannel.com/link\n")
+   output.WriteString("\n")
+   output.WriteString("2 Enter the activation code\n")
+   output.WriteString("  ")
+   output.WriteString(l.Code)
+   return output.String()
+}
+
+// RequestLinkCode requests a new activation code from the server
+func (c *Connection) RequestLinkCode() (*LinkCode, error) {
+   payload, err := json.Marshal(map[string]string{"platform": "googletv"})
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://googletv.web.roku.com/api/v1/account/activation",
+      bytes.NewReader(payload),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("content-type", "application/json")
+   req.Header.Set("user-agent", userAgent)
+   req.Header.Set("x-roku-content-token", c.AuthToken)
+
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+
+   link := &LinkCode{}
+   err = json.NewDecoder(resp.Body).Decode(link)
+   if err != nil {
+      return nil, err
+   }
+   return link, nil
+}
+
+// NewConnection initializes a session. User can be nil
+func (u *User) NewConnection() (*Connection, error) {
+   req, _ := http.NewRequest("", "https://googletv.web.roku.com", nil)
+   req.URL.Path = "/api/v1/account/token"
+   req.Header.Set("user-agent", userAgent)
+   if u != nil {
+      req.Header.Set("x-roku-content-token", u.Token)
+   }
+
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+
+   connector := &Connection{}
+   err = json.NewDecoder(resp.Body).Decode(connector)
+   if err != nil {
+      return nil, err
+   }
+   return connector, nil
+}
