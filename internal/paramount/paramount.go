5d8dd5738c1519c1ac983aae9771b9945ee4b4e5
27 <27@riseup.net>
2025-12-06 23:06:08 -0600

    +func (c *command) do_paramount() error {

--- internal/paramount/paramount.go
+++ internal/paramount/paramount.go
@@ -7,47 +7,86 @@ import (
    "flag"
    "log"
    "net/http"
+   "net/url"
    "os"
    "path"
    "path/filepath"
 )
 
-type Cache struct {
-   Mpd      *url.URL
-   MpdBody  []byte
-}
-
-// INTL does NOT allow anonymous key request, so if you are INTL you
-// will need to use US VPN until someone codes the INTL login
-func (r *runner) do_dash() error {
-   data, err := os.ReadFile(r.cache)
+func (c *command) do_paramount() error {
+   at, err := paramount.GetAt(c.app_secret())
    if err != nil {
       return err
    }
-   var cache paramount.Cache
+   item, err := paramount.FetchItem(at, c.paramount)
+   if err != nil {
+      return err
+   }
+   var cache user_cache
+   cache.Mpd.Url, cache.Mpd.Body, err = item.Mpd()
+   if err != nil {
+      return err
+   }
+   data, err := json.Marshal(cache)
+   if err != nil {
+      return err
+   }
+   log.Println("WriteFile", c.name)
+   err = os.WriteFile(c.name, data, os.ModePerm)
+   if err != nil {
+      return err
+   }
+   return net.Representations(cache.Mpd.Url, cache.Mpd.Body)
+}
+
+func (c *command) do_dash() error {
+   data, err := os.ReadFile(c.name)
+   if err != nil {
+      return err
+   }
+   var cache user_cache
    err = json.Unmarshal(data, &cache)
    if err != nil {
       return err
    }
-   at, err := paramount.ComCbsApp.At()
+   // INTL does NOT allow anonymous key request, so if you are INTL you
+   // will need to use US VPN until someone codes the INTL login
+   at, err := paramount.GetAt(paramount.ComCbsApp.AppSecret)
    if err != nil {
       return err
    }
-   token, err := at.Token(r.paramount)
+   var token paramount.SessionToken
+   err = token.Fetch(at, c.paramount)
    if err != nil {
       return err
    }
-   r.config.Send = func(data []byte) ([]byte, error) {
+   c.config.Send = func(data []byte) ([]byte, error) {
       return token.Widevine(data)
    }
-   return r.config.Download(cache.MpdBody, cache.Mpd, r.dash)
+   return c.config.Download(cache.Mpd.Url, cache.Mpd.Body, c.dash)
+}
+type user_cache struct {
+   Mpd struct {
+      Body  []byte
+      Url *url.URL
+   }
 }
 
-func (r *runner) secret() paramount.AppSecret {
-   if r.intl {
-      return paramount.ComCbsCa
+type command struct {
+   name     string
+   config    net.Config
+   // 1
+   paramount string
+   intl      bool
+   // 2
+   dash      string
+}
+
+func (c *command) app_secret() string {
+   if c.intl {
+      return paramount.ComCbsCa.AppSecret
    }
-   return paramount.ComCbsApp
+   return paramount.ComCbsApp.AppSecret
 }
 
 func main() {
@@ -63,71 +102,34 @@ func main() {
       }
       return "LP"
    })
-   var program runner
-   err := program.run()
+   err := new(command).run()
    if err != nil {
       log.Fatal(err)
    }
 }
 
-func (r *runner) run() error {
+func (c *command) run() error {
    cache, err := os.UserCacheDir()
    if err != nil {
       return err
    }
    cache = filepath.ToSlash(cache)
-   r.config.ClientId = cache + "/L3/client_id.bin"
-   r.config.PrivateKey = cache + "/L3/private_key.pem"
-   r.cache = cache + "/paramount/Cache.json"
+   c.config.ClientId = cache + "/L3/client_id.bin"
+   c.config.PrivateKey = cache + "/L3/private_key.pem"
+   c.name = cache + "/paramount/user_cache.json"
    
-   flag.StringVar(&r.config.ClientId, "C", r.config.ClientId, "client ID")
-   flag.StringVar(&r.config.PrivateKey, "P", r.config.PrivateKey, "private key")
-   flag.StringVar(&r.dash, "d", "", "DASH ID")
-   flag.BoolVar(&r.intl, "i", false, "intl")
-   flag.StringVar(&r.paramount, "p", "", "paramount ID")
+   flag.StringVar(&c.config.ClientId, "C", c.config.ClientId, "client ID")
+   flag.StringVar(&c.config.PrivateKey, "P", c.config.PrivateKey, "private key")
+   flag.StringVar(&c.dash, "d", "", "DASH ID")
+   flag.BoolVar(&c.intl, "i", false, "intl")
+   flag.StringVar(&c.paramount, "p", "", "paramount ID")
    flag.Parse()
-   if r.paramount != "" {
-      return r.do_paramount()
+   if c.paramount != "" {
+      return c.do_paramount()
    }
-   if r.dash != "" {
-      return r.do_dash()
+   if c.dash != "" {
+      return c.do_dash()
    }
    flag.Usage()
    return nil
-}
-
-type runner struct {
-   cache     string
-   config    net.Config
-   // 1
-   paramount string
-   intl      bool
-   // 2
-   dash      string
-}
-
-func (r *runner) do_paramount() error {
-   at, err := r.secret().At()
-   if err != nil {
-      return err
-   }
-   item, err := at.Item(r.paramount)
-   if err != nil {
-      return err
-   }
-   var cache paramount.Cache
-   err = item.Mpd(&cache)
-   if err != nil {
-      return err
-   }
-   data, err := json.Marshal(cache)
-   if err != nil {
-      return err
-   }
-   log.Println("WriteFile", r.cache)
-   err = os.WriteFile(r.cache, data, os.ModePerm)
-   if err != nil {
-      return err
-   }
-   return net.Representations(cache.MpdBody, cache.Mpd)
 }
--- paramount/paramount.go
+++ paramount/paramount.go
@@ -17,6 +17,94 @@ import (
    "strings"
 )
 
+func (s *SessionToken) Fetch(at, contentId string) error {
+   req, _ := http.NewRequest("", "https://www.paramountplus.com", nil)
+   req.URL.Path = func() string {
+      var data strings.Builder
+      data.WriteString("/apps-api/v3.1/androidphone/irdeto-control")
+      data.WriteString("/anonymous-session-token.json")
+      return data.String()
+   }()
+   req.URL.RawQuery = url.Values{
+      "at":        {at},
+      "contentId": {contentId},
+   }.Encode()
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return err
+   }
+   if resp.StatusCode != http.StatusOK {
+      var data strings.Builder
+      err = resp.Write(&data)
+      if err != nil {
+         return err
+      }
+      return errors.New(data.String())
+   }
+   defer resp.Body.Close()
+   return json.NewDecoder(resp.Body).Decode(s)
+}
+
+func GetAt(appSecret string) (string, error) {
+   // 1. Decode hex secret key
+   key, err := hex.DecodeString(secret_key)
+   if err != nil {
+      return "", err
+   }
+   // 2. Create aes cipher with key
+   block, err := aes.NewCipher(key)
+   if err != nil {
+      return "", err
+   }
+   // 3 & 4. Create payload: "|" + appSecret
+   data := []byte("|" + appSecret)
+   // 5. Apply PKCS7 Padding (Separate Function)
+   data = pkcs7Pad(data, aes.BlockSize)
+   // Prepare Empty IV (16 bytes of zeros)
+   iv := make([]byte, aes.BlockSize)
+   // 6. CBC encrypt with empty IV
+   // We encrypt 'data' in place
+   cipher.NewCBCEncrypter(block, iv).CryptBlocks(data, data)
+   // 8. Create Header for block size (uint16)
+   sizeHeader := make([]byte, 2)
+   binary.BigEndian.PutUint16(sizeHeader, uint16(aes.BlockSize))
+   // 7 & 8. Combine [Size] + [IV] + [Encrypted Data]
+   finalBuffer := slices.Concat(sizeHeader, iv, data)
+   // 9. Return result base64 encoded
+   return base64.StdEncoding.EncodeToString(finalBuffer), nil
+}
+
+const secret_key = "302a6a0d70a7e9b967f91d39fef3e387816e3095925ae4537bce96063311f9c5"
+
+// pkcs7Pad appends padding to the data according to the PKCS #7 standard.
+func pkcs7Pad(data []byte, blockSize int) []byte {
+   // Calculate the number of padding bytes needed.
+   // If data is already a multiple of blockSize, this results in a full block of padding.
+   paddingLen := blockSize - (len(data) % blockSize)
+   // Create a padding byte (the value is the length of the padding)
+   padByte := byte(paddingLen)
+   // Append the padding byte 'paddingLen' times
+   for i := 0; i < paddingLen; i++ {
+      data = append(data, padByte)
+   }
+   return data
+}
+
+type Provider struct {
+   AppSecret string
+   Version   string
+}
+
+var ComCbsApp = Provider{
+   AppSecret: "9fc14cb03691c342",
+   Version:   "16.0.0",
+}
+
+var ComCbsCa = Provider{
+   AppSecret: "6c68178445de8138",
+   Version:   "16.0.0",
+}
+
 func (i *Item) Mpd() (*url.URL, []byte, error) {
    req, _ := http.NewRequest("", "https://link.theplatform.com", nil)
    req.URL.Path = func() string {
@@ -43,6 +131,7 @@ func (i *Item) Mpd() (*url.URL, []byte, error) {
    }
    return resp.Request.URL, data, nil
 }
+
 func FetchItem(at, contentId string) (*Item, error) {
    req, _ := http.NewRequest("", "https://www.paramountplus.com", nil)
    req.URL.Path = func() string {
@@ -148,92 +237,4 @@ func (s *SessionToken) Widevine(data []byte) ([]byte, error) {
       return nil, errors.New(string(data))
    }
    return data, nil
-}
-
-func (s *SessionToken) Fetch(at, contentId string) error {
-   req, _ := http.NewRequest("", "https://www.paramountplus.com", nil)
-   req.URL.Path = func() string {
-      var data strings.Builder
-      data.WriteString("/apps-api/v3.1/androidphone/irdeto-control")
-      data.WriteString("/anonymous-session-token.json")
-      return data.String()
-   }()
-   req.URL.RawQuery = url.Values{
-      "at":        {at},
-      "contentId": {contentId},
-   }.Encode()
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return err
-   }
-   if resp.StatusCode != http.StatusOK {
-      var data strings.Builder
-      err = resp.Write(&data)
-      if err != nil {
-         return err
-      }
-      return errors.New(data.String())
-   }
-   defer resp.Body.Close()
-   return json.NewDecoder(resp.Body).Decode(s)
-}
-
-func GetAt(appSecret string) (string, error) {
-   // 1. Decode hex secret key
-   key, err := hex.DecodeString(secret_key)
-   if err != nil {
-      return "", err
-   }
-   // 2. Create aes cipher with key
-   block, err := aes.NewCipher(key)
-   if err != nil {
-      return "", err
-   }
-   // 3 & 4. Create payload: "|" + appSecret
-   data := []byte("|" + appSecret)
-   // 5. Apply PKCS7 Padding (Separate Function)
-   data = pkcs7Pad(data, aes.BlockSize)
-   // Prepare Empty IV (16 bytes of zeros)
-   iv := make([]byte, aes.BlockSize)
-   // 6. CBC encrypt with empty IV
-   // We encrypt 'data' in place
-   cipher.NewCBCEncrypter(block, iv).CryptBlocks(data, data)
-   // 8. Create Header for block size (uint16)
-   sizeHeader := make([]byte, 2)
-   binary.BigEndian.PutUint16(sizeHeader, uint16(aes.BlockSize))
-   // 7 & 8. Combine [Size] + [IV] + [Encrypted Data]
-   finalBuffer := slices.Concat(sizeHeader, iv, data)
-   // 9. Return result base64 encoded
-   return base64.StdEncoding.EncodeToString(finalBuffer), nil
-}
-
-type Provider struct {
-   AppSecret string
-   Version   string
-}
-
-var ComCbsApp = Provider{
-   AppSecret: "9fc14cb03691c342",
-   Version:   "16.0.0",
-}
-
-var ComCbsCa = Provider{
-   AppSecret: "6c68178445de8138",
-   Version:   "16.0.0",
-}
-
-const secret_key = "302a6a0d70a7e9b967f91d39fef3e387816e3095925ae4537bce96063311f9c5"
-
-// pkcs7Pad appends padding to the data according to the PKCS #7 standard.
-func pkcs7Pad(data []byte, blockSize int) []byte {
-   // Calculate the number of padding bytes needed.
-   // If data is already a multiple of blockSize, this results in a full block of padding.
-   paddingLen := blockSize - (len(data) % blockSize)
-   // Create a padding byte (the value is the length of the padding)
-   padByte := byte(paddingLen)
-   // Append the padding byte 'paddingLen' times
-   for i := 0; i < paddingLen; i++ {
-      data = append(data, padByte)
-   }
-   return data
 }
