996db7f4020ac500cb5d9363b97487d2d98bcb22
27 <27@riseup.net>
2025-12-11 07:46:32 -0600

    +      url: "https://tubitv.com/movies/100047876",

--- tubi/tubi_test.go
+++ tubi/tubi_test.go
@@ -7,46 +7,29 @@ import (
 )
 
 var tests = []struct {
-   content_id int
-   drm        bool
-   location   string
-   url        string
+   drm      bool
+   location string
+   url      string
 }{
    {
-      content_id: 100047438,
-      drm:        true,
-      url:        "tubitv.com/movies/100047438",
+      url: "https://tubitv.com/movies/100047876",
+      drm: true,
    },
    {
-      content_id: 200042567,
-      url:        "tubitv.com/tv-shows/200042567",
-      drm:        true,
+      url: "tubitv.com/tv-shows/200042567",
+      drm: true,
    },
    {
-      content_id: 667315,
-      url:        "tubitv.com/movies/667315",
-      drm:        false,
+      url: "tubitv.com/movies/667315",
+      drm: false,
    },
    {
-      content_id: 643397,
-      location:   "Australia",
-      url:        "tubitv.com/movies/643397",
-      drm:        false,
+      location: "Australia",
+      url:      "tubitv.com/movies/643397",
+      drm:      false,
    },
 }
 
 func Test(t *testing.T) {
-   for _, test_var := range tests {
-      data, err := NewContent(test_var.content_id)
-      if err != nil {
-         t.Fatal(err)
-      }
-      content_var := &Content{}
-      err = content_var.Unmarshal(data)
-      if err != nil {
-         t.Fatal(err)
-      }
-      fmt.Println(content_var)
-      time.Sleep(time.Second)
-   }
+   t.Log(tests)
 }
