f2efc93566ebd2e87d0c9d7b8abc2a2d5d84ac17
27 <27@riseup.net>
2025-12-19 19:32:26 -0600

    +   flag.IntVar(&c.config.Threads, "t", 2, "threads")

--- examples/paramount/paramount.go
+++ examples/paramount/paramount.go
@@ -12,6 +12,65 @@ import (
    "path/filepath"
 )
 
+func (c *command) run() error {
+   cache, err := os.UserCacheDir()
+   if err != nil {
+      return err
+   }
+   cache = filepath.ToSlash(cache)
+   c.config.ClientId = cache + "/L3/client_id.bin"
+   c.config.PrivateKey = cache + "/L3/private_key.pem"
+   c.name = cache + "/paramount/userCache.xml"
+
+   flag.StringVar(&c.config.ClientId, "C", c.config.ClientId, "client ID")
+   flag.StringVar(&c.config.PrivateKey, "P", c.config.PrivateKey, "private key")
+   flag.StringVar(&c.dash, "d", "", "DASH ID")
+   flag.BoolVar(&c.intl, "i", false, "intl")
+   flag.StringVar(&c.paramount, "p", "", "paramount ID")
+   flag.IntVar(&c.config.Threads, "t", 2, "threads")
+   flag.Parse()
+
+   if c.paramount != "" {
+      return c.do_paramount()
+   }
+   if c.dash != "" {
+      return c.do_dash()
+   }
+   flag.Usage()
+   return nil
+}
+
+func (c *command) app_secret() string {
+   if c.intl {
+      return paramount.ComCbsCa.AppSecret
+   }
+   return paramount.ComCbsApp.AppSecret
+}
+
+func (c *command) do_paramount() error {
+   at, err := paramount.GetAt(c.app_secret())
+   if err != nil {
+      return err
+   }
+   item, err := paramount.FetchItem(at, c.paramount)
+   if err != nil {
+      return err
+   }
+   cache, err := item.Mpd()
+   if err != nil {
+      return err
+   }
+   data, err := xml.Marshal(cache)
+   if err != nil {
+      return err
+   }
+   log.Println("WriteFile", c.name)
+   err = os.WriteFile(c.name, data, os.ModePerm)
+   if err != nil {
+      return err
+   }
+   return maya.Representations(cache.Url, cache.Body)
+}
 func (c *command) do_dash() error {
    data, err := os.ReadFile(c.name)
    if err != nil {
@@ -64,63 +123,4 @@ func main() {
    if err != nil {
       log.Fatal(err)
    }
-}
-
-func (c *command) run() error {
-   cache, err := os.UserCacheDir()
-   if err != nil {
-      return err
-   }
-   cache = filepath.ToSlash(cache)
-   c.config.ClientId = cache + "/L3/client_id.bin"
-   c.config.PrivateKey = cache + "/L3/private_key.pem"
-   c.name = cache + "/paramount/userCache.xml"
-
-   flag.StringVar(&c.config.ClientId, "C", c.config.ClientId, "client ID")
-   flag.StringVar(&c.config.PrivateKey, "P", c.config.PrivateKey, "private key")
-   flag.StringVar(&c.dash, "d", "", "DASH ID")
-   flag.BoolVar(&c.intl, "i", false, "intl")
-   flag.StringVar(&c.paramount, "p", "", "paramount ID")
-   flag.Parse()
-
-   if c.paramount != "" {
-      return c.do_paramount()
-   }
-   if c.dash != "" {
-      return c.do_dash()
-   }
-   flag.Usage()
-   return nil
-}
-
-func (c *command) app_secret() string {
-   if c.intl {
-      return paramount.ComCbsCa.AppSecret
-   }
-   return paramount.ComCbsApp.AppSecret
-}
-
-func (c *command) do_paramount() error {
-   at, err := paramount.GetAt(c.app_secret())
-   if err != nil {
-      return err
-   }
-   item, err := paramount.FetchItem(at, c.paramount)
-   if err != nil {
-      return err
-   }
-   cache, err := item.Mpd()
-   if err != nil {
-      return err
-   }
-   data, err := xml.Marshal(cache)
-   if err != nil {
-      return err
-   }
-   log.Println("WriteFile", c.name)
-   err = os.WriteFile(c.name, data, os.ModePerm)
-   if err != nil {
-      return err
-   }
-   return maya.Representations(cache.Url, cache.Body)
 }
