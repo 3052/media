d619793d3f5a05c299cefd90e6c237070f7fea61
27 <27@riseup.net>
2025-12-25 08:34:04 -0600

    new file:   research/disney/login.go

---
+++ research/disney/login_test.go
@@ -0,0 +1,34 @@
+package disney
+
+import (
+   "os"
+   "os/exec"
+   "testing"
+)
+
+func output(name string, arg ...string) (string, error) {
+   data, err := exec.Command(name, arg...).Output()
+   if err != nil {
+      return "", err
+   }
+   return string(data), nil
+}
+
+func TestLogin(t *testing.T) {
+   email, err := output("credential", "-h=disneyplus.com", "-k=user")
+   if err != nil {
+      t.Fatal(err)
+   }
+   password, err := output("credential", "-h=disneyplus.com")
+   if err != nil {
+      t.Fatal(err)
+   }
+   resp, err := login(email, password)
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = resp.Write(os.Stdout)
+   if err != nil {
+      t.Fatal(err)
+   }
+}
