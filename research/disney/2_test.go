9e3299c947026a4f1776b3b972cb749f8ed2bd3d
27 <27@riseup.net>
2025-12-24 23:52:00 -0600

    deleted:    research/disney/1_test.go

--- research/disney/2_test.go
+++ research/disney/2_test.go
@@ -7,6 +7,14 @@ import (
    "testing"
 )
 
+var test = struct{
+   entity string
+   url string
+}{
+   entity: "7df81cf5-6be5-4e05-9ff6-da33baf0b94d",
+   url: "https://disneyplus.com/play/7df81cf5-6be5-4e05-9ff6-da33baf0b94d",
+}
+
 func TestPlayback(t *testing.T) {
    cache, err := os.UserCacheDir()
    if err != nil {
@@ -21,7 +29,15 @@ func TestPlayback(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-   play, err := token.playback()
+   explore, err := token.explore(test.entity)
+   if err != nil {
+      t.Fatal(err)
+   }
+   resource_id, ok := explore.restart()
+   if !ok {
+      t.Fatal(".restart()")
+   }
+   play, err := token.playback(resource_id)
    if err != nil {
       t.Fatal(err)
    }
--- research/disney/readme.md
+++ research/disney/readme.md
@@ -1,7 +1,5 @@
 # disney
 
-https://disneyplus.com/play/7df81cf5-6be5-4e05-9ff6-da33baf0b94d
-
 ## android
 
 - https://apkmirror.com/apk/disney/disney
