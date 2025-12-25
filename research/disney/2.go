feb157dc4e2e69886d9fb045b27e9238f615ea44
27 <27@riseup.net>
2025-12-24 22:28:02 -0600

    new file:   research/disney/3.go

--- research/disney/2.go
+++ research/disney/2.go
@@ -1,30 +1,90 @@
 package disney
 
 import (
-   "errors"
+   "encoding/base64"
+   "encoding/json"
+   "fmt"
    "io"
    "net/http"
    "net/url"
-   "bytes"
+   "strings"
 )
 
-func (r refresh_token) obtain_license(data []byte) ([]byte, error) {
+const playback_id = `
+{
+   "mediaId": "aa401a2b-b7f4-4c11-bf61-a3b06f9c974d"
+}
+`
+
+func (r refresh_token) playback() (*playback, error) {
    var req http.Request
    req.Header = http.Header{}
    req.Method = "POST"
    req.URL = &url.URL{}
    req.URL.Host = "disney.playback.edge.bamgrid.com"
-   req.URL.Path = "/widevine/v1/obtain-license"
+   req.URL.Path = "/v7/playback/ctr-regular"
    req.URL.Scheme = "https"
-   req.Body = io.NopCloser(bytes.NewReader(data))
-   req.Header.Set("Authorization", r.Extensions.Sdk.Token.AccessToken)
+   req.Header.Set("Content-Type", "application/json")
+   req.Header.Set(
+      "Authorization", "Bearer " + r.Extensions.Sdk.Token.AccessToken,
+   )
+   req.Header.Set("X-Dss-Feature-Filtering", "true")
+   req.Header.Set("X-Application-Version", "")
+   req.Header.Set("X-Bamsdk-Client-Id", "")
+   req.Header.Set("X-Bamsdk-Platform", "")
+   req.Header.Set("X-Bamsdk-Version", "")
+   data := base64.StdEncoding.EncodeToString([]byte(playback_id))
+   data = fmt.Sprintf(`
+   {
+     "playback": {
+       "attributes": {
+         "assetInsertionStrategies": {
+           "point": "SGAI",
+           "range": "SGAI"
+         }
+       }
+     },
+     "playbackId": %q
+   }
+   `, data)
+   req.Body = io.NopCloser(strings.NewReader(data))
    resp, err := http.DefaultClient.Do(&req)
    if err != nil {
       return nil, err
    }
    defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
-      return nil, errors.New(resp.Status)
+   var result playback
+   err = json.NewDecoder(resp.Body).Decode(&result)
+   if err != nil {
+      return nil, err
    }
-   return io.ReadAll(resp.Body)
+   if len(result.Errors) >= 1 {
+      return nil, &result.Errors[0]
+   }
+   return &result, nil
+}
+
+func (e *Error) Error() string {
+   var data strings.Builder
+   data.WriteString("code = ")
+   data.WriteString(e.Code)
+   data.WriteString("\ndescription = ")
+   data.WriteString(e.Description)
+   return data.String()
+}
+
+type Error struct {
+   Code string
+   Description string
+}
+
+type playback struct {
+   Errors []Error
+   Stream struct {
+      Sources []struct {
+         Complete struct {
+            Url string
+         }
+      }
+   }
 }
---
+++ research/disney/3.go
@@ -0,0 +1,30 @@
+package disney
+
+import (
+   "errors"
+   "io"
+   "net/http"
+   "net/url"
+   "bytes"
+)
+
+func (r refresh_token) obtain_license(data []byte) ([]byte, error) {
+   var req http.Request
+   req.Header = http.Header{}
+   req.Method = "POST"
+   req.URL = &url.URL{}
+   req.URL.Host = "disney.playback.edge.bamgrid.com"
+   req.URL.Path = "/widevine/v1/obtain-license"
+   req.URL.Scheme = "https"
+   req.Body = io.NopCloser(bytes.NewReader(data))
+   req.Header.Set("Authorization", r.Extensions.Sdk.Token.AccessToken)
+   resp, err := http.DefaultClient.Do(&req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      return nil, errors.New(resp.Status)
+   }
+   return io.ReadAll(resp.Body)
+}
