e3148c95cbd2025c5d27b58712c0dc7dab6863cf
27 <27@riseup.net>
2025-12-25 10:10:36 -0600

    deleted:    research/2025/12/25/register_device.json

--- research/2025/12/25/register_device.go
+++ research/2025/12/25/register_device.go
@@ -6,103 +6,60 @@ import (
    "net/http"
 )
 
-const client_api_key = "ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84"
-
-func register_device() (*http.Response, error) {
+func (r *register_device) fetch() error {
    data, err := json.Marshal(map[string]any{
-      "operationName": "registerDevice",
       "query": query_register_device,
       "variables": map[string]any{
          "input": map[string]any{
-            "deviceFamily": "browser",
-            "applicationRuntime": "firefox",
-            "deviceProfile": "windows",
-            "deviceLanguage": "en",
-            "devicePlatformId": "browser",
+            "deviceProfile": "!",
+            "deviceFamily": "!",
+            "applicationRuntime": "!",
             "attributes": map[string]string{
-               "brand": "web",
-               "browserName": "firefox",
-               "browserVersion": "128.0",
-               "manufacturer": "n/a",
-               "operatingSystem": "windows",
-               "operatingSystemVersion": "10.0",
+               "operatingSystem": "",
+               "operatingSystemVersion": "",
             },
          },
       },
    })
    if err != nil {
-      return nil, err
+      return err
    }
    req, err := http.NewRequest(
       "POST", "https://disney.api.edge.bamgrid.com/graph/v1/device/graphql",
       bytes.NewReader(data),
    )
    if err != nil {
-      return nil, err
+      return err
    }
    req.Header.Add("Authorization", "Bearer " + client_api_key)
-   return http.DefaultClient.Do(req)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   return json.NewDecoder(resp.Body).Decode(r)
 }
+
+type register_device struct {
+   Extensions struct {
+      Sdk struct {
+         Token struct {
+            AccessToken string
+         }
+      }
+   }
+}
+
+const client_api_key = "ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84"
 
 const query_register_device = `
 mutation registerDevice($input: RegisterDeviceInput!) {
-      registerDevice(registerDevice: $input) {
-        grant {
-          grantType
-          assertion
-        },
-        token {
-          accessToken
-          accessTokenType
-          expiresIn
-          refreshToken
-          tokenType
-        },
-        session: activeSession {
-          sessionId
-          partnerName
-          device {
-            id
-            category
-            platform
-          }
-          profile {
-            id
-          }
-          experiments {
-            featureId
-            variantId
-            version
-          }
-          portabilityLocation {
-            countryCode
-            type
-          }
-          homeLocation {
-            adsSupported
-            countryCode
-          }
-          household {
-            householdScore
-          }
-          preferredMaturityRating {
-            impliedMaturityRating
-            ratingSystem
-          }
-          identity {
-            id
-          }
-          location {
-            adsSupported
-            type
-            countryCode
-            dma
-            asn
-            regionName
-            connectionType
-            zipCode
-          }
-        }
+   registerDevice(registerDevice: $input) {
+      token {
+         accessToken
+         refreshToken
       }
-    }
+   }
+}
 `
+
