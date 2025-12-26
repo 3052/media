c877a1c943151266a2ed51f454f9c97f93bcf3f7
27 <27@riseup.net>
2025-12-26 06:19:29 -0600

    +func output(name string, arg ...string) (string, error) {

--- rakuten/rakuten_test.go
+++ rakuten/rakuten_test.go
@@ -8,6 +8,59 @@ import (
    "testing"
 )
 
+func output(name string, arg ...string) (string, error) {
+   data, err := exec.Command(name, arg...).Output()
+   if err != nil {
+      return "", err
+   }
+   return string(data), nil
+}
+
+func TestPlayReady(t *testing.T) {
+   const (
+      address  = "https://rakuten.tv/cz?content_type=movies&content_id=transvulcania-the-people-s-run"
+      language = "SPA"
+   )
+   user, err := output("credential", "-h=api.nordvpn.com", "-k=user")
+   if err != nil {
+      t.Fatal(err)
+   }
+   password, err := output("credential", "-h", "api.nordvpn.com")
+   if err != nil {
+      t.Fatal(err)
+   }
+   http.DefaultTransport = &http.Transport{
+      Proxy: http.ProxyURL(&url.URL{
+         Scheme: "https",
+         User:   url.UserPassword(user, password),
+         Host:   "cz103.nordvpn.com:89",
+      }),
+   }
+   var movie_var Movie
+   err = movie_var.ParseURL(address)
+   if err != nil {
+      t.Fatal(err)
+   }
+   stream, err := movie_var.RequestStream(language, Player.PlayReady, Quality.HD)
+   if err != nil {
+      t.Fatal(err)
+   }
+   cache, err := os.UserCacheDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = os.WriteFile(
+      cache+"/rakuten/PlayReady",
+      []byte(stream.StreamInfos[0].LicenseUrl), os.ModePerm,
+   )
+   if err != nil {
+      t.Fatal(err)
+   }
+}
+
+func TestLog(t *testing.T) {
+   t.Log(address_tests, classification_tests)
+}
 var classification_tests = []string{
    "https://rakuten.tv/cz?content_type=movies&content_id=transvulcania-the-people-s-run",
    "https://rakuten.tv/dk/movies/a-time-to-kill",
@@ -45,54 +98,4 @@ var address_tests = []struct {
       format: "?tv_show_id=",
       url:    "https://rakuten.tv/uk?content_type=tv_shows&tv_show_id=clink",
    },
-}
-
-func TestPlayReady(t *testing.T) {
-   const (
-      address  = "https://rakuten.tv/cz?content_type=movies&content_id=transvulcania-the-people-s-run"
-      language = "SPA"
-   )
-   user, err := exec.Command(
-      "credential", "-h", "api.nordvpn.com", "-k", "user",
-   ).Output()
-   if err != nil {
-      t.Fatal(err)
-   }
-   password, err := exec.Command(
-      "credential", "-h", "api.nordvpn.com",
-   ).Output()
-   if err != nil {
-      t.Fatal(err)
-   }
-   http.DefaultTransport = &http.Transport{
-      Proxy: http.ProxyURL(&url.URL{
-         Scheme: "https",
-         User:   url.UserPassword(string(user), string(password)),
-         Host:   "cz103.nordvpn.com:89",
-      }),
-   }
-   var movie_var Movie
-   err = movie_var.ParseURL(address)
-   if err != nil {
-      t.Fatal(err)
-   }
-   stream, err := movie_var.RequestStream(language, Player.PlayReady, Quality.HD)
-   if err != nil {
-      t.Fatal(err)
-   }
-   cache, err := os.UserCacheDir()
-   if err != nil {
-      t.Fatal(err)
-   }
-   err = os.WriteFile(
-      cache+"/rakuten/PlayReady",
-      []byte(stream.StreamInfos[0].LicenseUrl), os.ModePerm,
-   )
-   if err != nil {
-      t.Fatal(err)
-   }
-}
-
-func TestLog(t *testing.T) {
-   t.Log(address_tests, classification_tests)
 }
