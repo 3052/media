25c6bbbdc158dd263061a43240f07ceddd3b70bb
27 <27@riseup.net>
2025-12-28 09:10:32 -0600

    +   if len(result.Data.Errors) >= 1 {

--- disney/playback_test.go
+++ disney/playback_test.go
@@ -2,11 +2,31 @@ package disney
 
 import (
    "41.neocities.org/drm/widevine"
+   "encoding/base64"
    "encoding/xml"
-   "fmt"
    "os"
    "testing"
+   "time"
 )
+
+var tests = []struct {
+   entity string
+   key_ids []string
+   url    string
+}{
+   {
+      entity: "7df81cf5-6be5-4e05-9ff6-da33baf0b94d",
+      key_ids: []string{
+         "vDaf4HL8QKG42hzb6/0AaQ==", // L3
+         "GzlnsDJgQ82Du9PjayM4IQ==", // L1
+      },
+      url: "https://disneyplus.com/play/7df81cf5-6be5-4e05-9ff6-da33baf0b94d",
+   },
+   {
+      entity: "d0d0796c-a144-42fa-a730-4cbd1014ef1f",
+      url: "https://disneyplus.com/browse/entity-d0d0796c-a144-42fa-a730-4cbd1014ef1f",
+   },
+}
 
 func TestPlayback(t *testing.T) {
    cache, err := os.UserCacheDir()
@@ -22,28 +42,24 @@ func TestPlayback(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-   explore_item, err := account_with.explore(test.entity)
-   if err != nil {
-      t.Fatal(err)
-   }
-   resource_id, ok := explore_item.restart()
-   if !ok {
-      t.Fatal(".restart()")
-   }
-   play, err := account_with.playback(resource_id)
-   if err != nil {
-      t.Fatal(err)
-   }
-   for i, source := range play.Stream.Sources {
+   for i, test := range tests {
       if i >= 1 {
-         fmt.Println()
+         time.Sleep(time.Second)
       }
-      fmt.Printf("%+v\n", source)
+      explore_item, err := account_with.explore(test.entity)
+      if err != nil {
+         t.Fatal(err)
+      }
+      resource_id, ok := explore_item.restart()
+      if !ok {
+         t.Fatal(".restart()")
+      }
+      play, err := account_with.playback(resource_id)
+      if err != nil {
+         t.Fatal(err)
+      }
+      t.Logf("%+v", play.Stream.Sources[0])
    }
-}
-
-var key_id = []byte{
-   188, 54, 159, 224, 114, 252, 64, 161, 184, 218, 28, 219, 235, 253, 0, 105,
 }
 
 func TestWidevine(t *testing.T) {
@@ -60,6 +76,10 @@ func TestWidevine(t *testing.T) {
       t.Fatal(err)
    }
    private_key, err := widevine.ParsePrivateKey(pem_bytes)
+   if err != nil {
+      t.Fatal(err)
+   }
+   key_id, err := base64.StdEncoding.DecodeString(tests[0].key_ids[0])
    if err != nil {
       t.Fatal(err)
    }
@@ -86,12 +106,4 @@ func TestWidevine(t *testing.T) {
    if err != nil {
       t.Fatal(err)
    }
-}
-
-var test = struct {
-   entity string
-   url    string
-}{
-   entity: "7df81cf5-6be5-4e05-9ff6-da33baf0b94d",
-   url:    "https://disneyplus.com/play/7df81cf5-6be5-4e05-9ff6-da33baf0b94d",
 }
