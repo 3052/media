f386868d0d3ab3bca14eba3c6b66eedddad7bebb
27 <27@riseup.net>
2025-12-30 19:56:08 -0600

    +func (p *Page) GetFormattedString() (string, error) {

--- examples/disney/disney.go
+++ examples/disney/disney.go
@@ -4,14 +4,45 @@ import (
    "41.neocities.org/maya"
    "41.neocities.org/media/disney"
    "encoding/xml"
-   "errors"
    "flag"
+   "fmt"
    "log"
    "net/http"
    "os"
    "path"
    "path/filepath"
 )
+
+func (c *command) do_media_id() error {
+   cache, err := read(c.name)
+   if err != nil {
+      return err
+   }
+   playback, err := cache.Account.Playback(c.media_id)
+   if err != nil {
+      return err
+   }
+   cache.Hls, err = playback.Hls()
+   if err != nil {
+      return err
+   }
+   err = write(c.name, cache)
+   if err != nil {
+      return err
+   }
+   return maya.ListHls(cache.Hls.Body, cache.Hls.Url)
+}
+
+func (c *command) do_hls() error {
+   cache, err := read(c.name)
+   if err != nil {
+      return err
+   }
+   c.job.Send = func(data []byte) ([]byte, error) {
+      return cache.Account.Widevine(data)
+   }
+   return c.job.DownloadHls(cache.Hls.Body, cache.Hls.Url, c.hls)
+}
 
 func main() {
    log.SetFlags(log.Ltime)
@@ -37,22 +68,31 @@ func (c *command) run() error {
    c.name = cache + "/disney/userCache.xml"
    c.job.ClientID = cache + "/L3/client_id.bin"
    c.job.PrivateKey = cache + "/L3/private_key.pem"
-
    flag.StringVar(&c.job.ClientID, "C", c.job.ClientID, "client ID")
    flag.StringVar(&c.job.PrivateKey, "P", c.job.PrivateKey, "private key")
-   flag.StringVar(&c.address, "a", "", "address")
+   // 1
    flag.StringVar(&c.email, "e", "", "email")
-   flag.StringVar(&c.hls, "h", "", "HLS ID")
    flag.StringVar(&c.password, "p", "", "password")
+   // 2
+   flag.StringVar(&c.address, "a", "", "address")
+   // 3
+   flag.StringVar(&c.media_id, "m", "", "media ID")
+   // 4
+   flag.StringVar(&c.hls, "h", "", "HLS ID")
    flag.Parse()
-
+   // 1
    if c.email != "" {
       if c.password != "" {
          return c.do_email_password()
       }
    }
+   // 2
    if c.address != "" {
       return c.do_address()
+   }
+   // 3
+   if c.media_id != "" {
+      return c.do_media_id()
    }
    if c.hls != "" {
       return c.do_hls()
@@ -101,20 +141,24 @@ func (c *command) do_email_password() error {
    return write(c.name, &cache)
 }
 
-func (c *command) do_hls() error {
-   cache, err := read(c.name)
-   if err != nil {
-      return err
-   }
-   c.job.Send = func(data []byte) ([]byte, error) {
-      return cache.Account.Widevine(data)
-   }
-   return c.job.DownloadHls(cache.Hls.Body, cache.Hls.Url, c.hls)
-}
-
 type user_cache struct {
    Account *disney.Account
    Hls     *disney.Hls
+}
+
+type command struct {
+   job      maya.WidevineJob
+   name     string
+   // 1
+   email    string
+   password string
+   
+   // 2
+   address  string
+   // 3
+   media_id string
+   // 4
+   hls      string
 }
 
 func (c *command) do_address() error {
@@ -130,30 +174,10 @@ func (c *command) do_address() error {
    if err != nil {
       return err
    }
-   playback_id, ok := explore.PlaybackId()
-   if !ok {
-      return errors.New(".PlaybackId()")
-   }
-   playback, err := cache.Account.Playback(playback_id)
+   page, err := explore.Data.Page.GetFormattedString()
    if err != nil {
       return err
    }
-   cache.Hls, err = playback.Hls()
-   if err != nil {
-      return err
-   }
-   err = write(c.name, cache)
-   if err != nil {
-      return err
-   }
-   return maya.ListHls(cache.Hls.Body, cache.Hls.Url)
-}
-
-type command struct {
-   address  string
-   hls      string
-   email    string
-   job      maya.WidevineJob
-   name     string
-   password string
+   fmt.Println(page)
+   return nil
 }
