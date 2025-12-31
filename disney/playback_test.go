6621f383eda8867e211f996735bec7878a872540
27 <27@riseup.net>
2025-12-30 18:01:40 -0600

    new file:   research/disney/readme.md

--- disney/playback_test.go
+++ disney/playback_test.go
@@ -7,16 +7,31 @@ func Test(t *testing.T) {
 }
 
 var tests = []struct {
+   entity   string
    location string
-   playback_id string
-   url      string
+   url      []string
 }{
    {
+      entity:   "episode",
       location: "US",
-      url:      "https://disneyplus.com/play/7df81cf5-6be5-4e05-9ff6-da33baf0b94d",
+      url: []string{
+         "https://disneyplus.com/browse/entity-21e70fbf-6a51-41b3-88e9-f111830b046c",
+         "https://disneyplus.com/play/d32df5dd-4487-4e4c-9649-20f4bb472923",
+      },
    },
    {
+      entity:   "movie",
+      location: "US",
+      url: []string{
+         "https://disneyplus.com/browse/entity-7df81cf5-6be5-4e05-9ff6-da33baf0b94d",
+         "https://disneyplus.com/play/7df81cf5-6be5-4e05-9ff6-da33baf0b94d",
+      },
+   },
+   {
+      entity:   "movie",
       location: "KR", // MUST DO KR LOGIN FIRST
-      url:      "https://disneyplus.com/browse/entity-d0d0796c-a144-42fa-a730-4cbd1014ef1f",
+      url: []string{
+         "https://disneyplus.com/browse/entity-d0d0796c-a144-42fa-a730-4cbd1014ef1f",
+      },
    },
 }
