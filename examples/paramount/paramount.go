ef122bdcb7bc4132c8215277cea5fdc051596790
27 <27@riseup.net>
2026-01-20 06:43:41 -0600

    +   return c.job.DownloadDash(cache.Body, cache.Url, c.dash)

--- examples/paramount/paramount.go
+++ examples/paramount/paramount.go
@@ -12,32 +12,31 @@ import (
    "path/filepath"
 )
 
-func (c *command) run() error {
-   cache, err := os.UserCacheDir()
+func (c *command) do_dash() error {
+   data, err := os.ReadFile(c.name)
    if err != nil {
       return err
    }
-   cache = filepath.ToSlash(cache)
-   c.config.ClientId = cache + "/L3/client_id.bin"
-   c.config.PrivateKey = cache + "/L3/private_key.pem"
-   c.name = cache + "/paramount/userCache.xml"
-
-   flag.StringVar(&c.config.ClientId, "C", c.config.ClientId, "client ID")
-   flag.StringVar(&c.config.PrivateKey, "P", c.config.PrivateKey, "private key")
-   flag.StringVar(&c.dash, "d", "", "DASH ID")
-   flag.BoolVar(&c.intl, "i", false, "intl")
-   flag.StringVar(&c.paramount, "p", "", "paramount ID")
-   flag.IntVar(&c.config.Threads, "t", 3, "threads")
-   flag.Parse()
-
-   if c.paramount != "" {
-      return c.do_paramount()
+   var cache paramount.Mpd
+   err = xml.Unmarshal(data, &cache)
+   if err != nil {
+      return err
    }
-   if c.dash != "" {
-      return c.do_dash()
+   // INTL does NOT allow anonymous key request, so if you are INTL you
+   // will need to use US VPN until someone codes the INTL login
+   at, err := paramount.GetAt(paramount.ComCbsApp.AppSecret)
+   if err != nil {
+      return err
    }
-   flag.Usage()
-   return nil
+   var token paramount.SessionToken
+   err = token.Fetch(at, c.paramount)
+   if err != nil {
+      return err
+   }
+   c.job.Send = func(data []byte) ([]byte, error) {
+      return token.Widevine(data)
+   }
+   return c.job.DownloadDash(cache.Body, cache.Url, c.dash)
 }
 
 func (c *command) app_secret() string {
@@ -45,6 +44,54 @@ func (c *command) app_secret() string {
       return paramount.ComCbsCa.AppSecret
    }
    return paramount.ComCbsApp.AppSecret
+}
+
+func main() {
+   log.SetFlags(log.Ltime)
+   maya.Transport(func(req *http.Request) string {
+      switch path.Ext(req.URL.Path) {
+      case ".m4s", ".mp4":
+         return ""
+      }
+      switch path.Base(req.URL.Path) {
+      case "anonymous-session-token.json", "getlicense":
+         return "L"
+      }
+      return "LP"
+   })
+   err := new(command).run()
+   if err != nil {
+      log.Fatal(err)
+   }
+}
+
+func (c *command) run() error {
+   cache, err := os.UserCacheDir()
+   if err != nil {
+      return err
+   }
+   cache = filepath.ToSlash(cache)
+   c.job.ClientId = cache + "/L3/client_id.bin"
+   c.job.PrivateKey = cache + "/L3/private_key.pem"
+   c.name = cache + "/paramount/userCache.xml"
+   flag.StringVar(&c.job.ClientId, "C", c.job.ClientId, "client ID")
+   flag.StringVar(&c.job.PrivateKey, "P", c.job.PrivateKey, "private key")
+   // 1
+   flag.StringVar(&c.paramount, "p", "", "paramount ID")
+   flag.BoolVar(&c.intl, "i", false, "intl")
+   // 2
+   flag.StringVar(&c.dash, "d", "", "DASH ID")
+   flag.Parse()
+   // 1
+   if c.paramount != "" {
+      return c.do_paramount()
+   }
+   // 2
+   if c.dash != "" {
+      return c.do_dash()
+   }
+   flag.Usage()
+   return nil
 }
 
 func (c *command) do_paramount() error {
@@ -69,58 +116,15 @@ func (c *command) do_paramount() error {
    if err != nil {
       return err
    }
-   return maya.Representations(cache.Url, cache.Body)
-}
-func (c *command) do_dash() error {
-   data, err := os.ReadFile(c.name)
-   if err != nil {
-      return err
-   }
-   var cache paramount.Mpd
-   err = xml.Unmarshal(data, &cache)
-   if err != nil {
-      return err
-   }
-   // INTL does NOT allow anonymous key request, so if you are INTL you
-   // will need to use US VPN until someone codes the INTL login
-   at, err := paramount.GetAt(paramount.ComCbsApp.AppSecret)
-   if err != nil {
-      return err
-   }
-   var token paramount.SessionToken
-   err = token.Fetch(at, c.paramount)
-   if err != nil {
-      return err
-   }
-   c.config.Send = func(data []byte) ([]byte, error) {
-      return token.Widevine(data)
-   }
-   return c.config.Download(cache.Url, cache.Body, c.dash)
+   return maya.ListDash(cache.Body, cache.Url)
 }
 
 type command struct {
-   config    maya.Config
-   dash      string
-   intl      bool
+   job    maya.WidevineJob
    name      string
+   // 1
    paramount string
-}
-
-func main() {
-   log.SetFlags(log.Ltime)
-   maya.Transport(func(req *http.Request) string {
-      switch path.Ext(req.URL.Path) {
-      case ".m4s", ".mp4":
-         return ""
-      }
-      switch path.Base(req.URL.Path) {
-      case "anonymous-session-token.json", "getlicense":
-         return "L"
-      }
-      return "LP"
-   })
-   err := new(command).run()
-   if err != nil {
-      log.Fatal(err)
-   }
+   intl      bool
+   // 2
+   dash      string
 }
