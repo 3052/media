3775dfbbc0aeb2cfc9e9b8a7708dbe518ad0b552
27 <27@riseup.net>
2026-02-06 21:14:25 -0600

    deleted:    research/paramount/paramount.go

--- examples/paramount/paramount.go
+++ examples/paramount/paramount.go
@@ -12,6 +12,67 @@ import (
    "path/filepath"
 )
 
+func (c *command) run() error {
+   cache, err := os.UserCacheDir()
+   if err != nil {
+      return err
+   }
+   cache = filepath.ToSlash(cache)
+   c.name = cache + "/paramount/userCache.xml"
+   c.job.CertificateChain = cache + "/SL2000/CertificateChain"
+   c.job.EncryptSignKey = cache + "/SL2000/EncryptSignKey"
+   // 1
+   flag.StringVar(&c.username, "U", "", "username")
+   flag.StringVar(&c.password, "P", "", "password")
+   // 2
+   flag.StringVar(&c.paramount, "p", "", "paramount ID")
+   // 2, 3
+   flag.BoolVar(&c.intl, "i", false, "intl")
+   // 3
+   flag.StringVar(&c.dash, "d", "", "DASH ID")
+   flag.BoolVar(&c.cookie, "c", false, "cookie")
+   flag.StringVar(&c.job.CertificateChain, "C", c.job.CertificateChain, "certificate chain")
+   flag.StringVar(&c.job.EncryptSignKey, "E", c.job.EncryptSignKey, "encrypt sign key")
+   flag.Parse()
+   // 1
+   if c.username != "" {
+      if c.password != "" {
+         return c.do_username_password()
+      }
+   }
+   // 2
+   if c.paramount != "" {
+      return c.do_paramount()
+   }
+   // 3
+   if c.dash != "" {
+      return c.do_dash()
+   }
+   flag.Usage()
+   return nil
+}
+func (c *command) do_dash() error {
+   at, err := paramount.GetAt(c.app_secret())
+   if err != nil {
+      return err
+   }
+   cache, err := read(c.name)
+   if err != nil {
+      return err
+   }
+   if !c.cookie {
+      cache.Cookie = nil
+   }
+   token, err := paramount.PlayReady(at, cache.Item.ContentId, cache.Cookie)
+   if err != nil {
+      return err
+   }
+   c.job.Send = func(data []byte) ([]byte, error) {
+      return token.Send(data)
+   }
+   return c.job.DownloadDash(cache.Mpd.Body, cache.Mpd.Url, c.dash)
+}
+
 func (c *command) app_secret() string {
    if c.intl {
       return paramount.ComCbsCa.AppSecret
@@ -79,25 +140,6 @@ func read(name string) (*user_cache, error) {
    return cache, nil
 }
 
-func (c *command) do_dash() error {
-   cache, err := read(c.name)
-   if err != nil {
-      return err
-   }
-   at, err := paramount.GetAt(c.app_secret())
-   if err != nil {
-      return err
-   }
-   token, err := paramount.PlayReady(at, cache.Item.ContentId, cache.Cookie)
-   if err != nil {
-      return err
-   }
-   c.job.Send = func(data []byte) ([]byte, error) {
-      return token.Send(data)
-   }
-   return c.job.DownloadDash(cache.Mpd.Body, cache.Mpd.Url, c.dash)
-}
-
 func (c *command) do_paramount() error {
    at, err := paramount.GetAt(c.app_secret())
    if err != nil {
@@ -131,48 +173,8 @@ type command struct {
    // 2
    paramount string
    // 2, 3
-   intl      bool
+   intl bool
    // 3
    dash   string
    cookie bool
-}
-
-func (c *command) run() error {
-   cache, err := os.UserCacheDir()
-   if err != nil {
-      return err
-   }
-   cache = filepath.ToSlash(cache)
-   c.name = cache + "/paramount/userCache.xml"
-   c.job.CertificateChain = cache + "/SL2000/CertificateChain"
-   c.job.EncryptSignKey = cache + "/SL2000/EncryptSignKey"
-   // 1
-   flag.StringVar(&c.username, "U", "", "username")
-   flag.StringVar(&c.password, "P", "", "password")
-   // 2
-   flag.StringVar(&c.paramount, "p", "", "paramount ID")
-   // 2, 3
-   flag.BoolVar(&c.intl, "i", false, "intl")
-   // 3
-   flag.StringVar(&c.dash, "d", "", "DASH ID")
-   flag.BoolVar(&c.cookie, "g", false, "cookie")
-   flag.StringVar(&c.job.CertificateChain, "C", c.job.CertificateChain, "certificate chain")
-   flag.StringVar(&c.job.EncryptSignKey, "E", c.job.EncryptSignKey, "encrypt sign key")
-   flag.Parse()
-   // 1
-   if c.username != "" {
-      if c.password != "" {
-         return c.do_username_password()
-      }
-   }
-   // 2
-   if c.paramount != "" {
-      return c.do_paramount()
-   }
-   // 3
-   if c.dash != "" {
-      return c.do_dash()
-   }
-   flag.Usage()
-   return nil
 }
--- paramount/paramount.go
+++ paramount/paramount.go
@@ -17,7 +17,42 @@ import (
    "strings"
 )
 
+// 540p L3
+func Widevine(at, contentId string) (*SessionToken, error) {
+   var req http.Request
+   req.Header = http.Header{}
+   req.URL = &url.URL{
+      Scheme: "https",
+      Host: "www.paramountplus.com",
+      Path: "/apps-api/v3.1/androidphone/irdeto-control/anonymous-session-token.json",
+      RawQuery: url.Values{
+         "at":        {at},
+         "contentId": {contentId},
+      }.Encode(),
+   }
+   resp, err := http.DefaultClient.Do(&req)
+   if err != nil {
+      return nil, err
+   }
+   if resp.StatusCode != http.StatusOK {
+      var data strings.Builder
+      err = resp.Write(&data)
+      if err != nil {
+         return nil, err
+      }
+      return nil, errors.New(data.String())
+   }
+   defer resp.Body.Close()
+   var token SessionToken
+   err = json.NewDecoder(resp.Body).Decode(&token)
+   if err != nil {
+      return nil, err
+   }
+   return &token, nil
+}
+
 // 1080p SL2000
+// 1440p SL2000 + cookie
 func PlayReady(at, contentId string, cookie *http.Cookie) (*SessionToken, error) {
    var req http.Request
    req.Header = http.Header{}
@@ -100,40 +135,6 @@ var ComCbsApp = Provider{
 var ComCbsCa = Provider{
    AppSecret: "6c68178445de8138",
    Version:   "16.0.0",
-}
-
-// 540p L3
-func Widevine(at, contentId string) (*SessionToken, error) {
-   var req http.Request
-   req.Header = http.Header{}
-   req.URL = &url.URL{
-      Scheme: "https",
-      Host: "www.paramountplus.com",
-      Path: "/apps-api/v3.1/androidphone/irdeto-control/anonymous-session-token.json",
-      RawQuery: url.Values{
-         "at":        {at},
-         "contentId": {contentId},
-      }.Encode(),
-   }
-   resp, err := http.DefaultClient.Do(&req)
-   if err != nil {
-      return nil, err
-   }
-   if resp.StatusCode != http.StatusOK {
-      var data strings.Builder
-      err = resp.Write(&data)
-      if err != nil {
-         return nil, err
-      }
-      return nil, errors.New(data.String())
-   }
-   defer resp.Body.Close()
-   var token SessionToken
-   err = json.NewDecoder(resp.Body).Decode(&token)
-   if err != nil {
-      return nil, err
-   }
-   return &token, nil
 }
 
 func (s *SessionToken) Send(data []byte) ([]byte, error) {
