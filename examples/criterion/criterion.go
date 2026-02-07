a4bd08d9f941e4803b1bca290e5e0d6c690de807
27 <27@riseup.net>
2026-02-06 22:52:05 -0600

    new file:   research/paramount/paramount.go

--- examples/criterion/criterion.go
+++ examples/criterion/criterion.go
@@ -13,53 +13,15 @@ import (
    "path/filepath"
 )
 
-type user_cache struct {
-   MediaFile *criterion.MediaFile
-   Mpd       *criterion.Mpd
-   Token     *criterion.Token
-}
-
-func (c *command) do_address() error {
-   cache, err := read(c.name)
-   if err != nil {
-      return err
-   }
-   err = cache.Token.Refresh()
-   if err != nil {
-      return err
-   }
-   item, err := cache.Token.Item(path.Base(c.address))
-   if err != nil {
-      return err
-   }
-   files, err := cache.Token.Files(item)
-   if err != nil {
-      return err
-   }
-   var ok bool
-   cache.MediaFile, ok = files.Dash()
-   if !ok {
-      return errors.New(".Dash()")
-   }
-   cache.Mpd, err = cache.MediaFile.Mpd()
-   if err != nil {
-      return err
-   }
-   err = write(c.name, cache)
-   if err != nil {
-      return err
-   }
-   return maya.ListDash(cache.Mpd.Body, cache.Mpd.Url)
-}
 func (c *command) run() error {
    cache, err := os.UserCacheDir()
    if err != nil {
       return err
    }
    cache = filepath.ToSlash(cache)
+   c.name = cache + "/criterion/userCache.xml"
    c.job.ClientId = cache + "/L3/client_id.bin"
    c.job.PrivateKey = cache + "/L3/private_key.pem"
-   c.name = cache + "/criterion/userCache.xml"
    // 1
    flag.StringVar(&c.email, "e", "", "email")
    flag.StringVar(&c.password, "p", "", "password")
@@ -69,7 +31,6 @@ func (c *command) run() error {
    flag.StringVar(&c.job.ClientId, "C", c.job.ClientId, "client ID")
    flag.StringVar(&c.job.PrivateKey, "P", c.job.PrivateKey, "private key")
    flag.StringVar(&c.dash, "d", "", "DASH ID")
-   flag.IntVar(&c.job.Threads, "t", 2, "threads")
    flag.Parse()
    // 1
    if c.email != "" {
@@ -142,6 +103,7 @@ func (c *command) do_dash() error {
    }
    return c.job.DownloadDash(cache.Mpd.Body, cache.Mpd.Url, c.dash)
 }
+
 func main() {
    log.SetFlags(log.Ltime)
    maya.Transport(func(req *http.Request) string {
@@ -154,4 +116,42 @@ func main() {
    if err != nil {
       log.Fatal(err)
    }
+}
+type user_cache struct {
+   MediaFile *criterion.MediaFile
+   Mpd       *criterion.Mpd
+   Token     *criterion.Token
+}
+
+func (c *command) do_address() error {
+   cache, err := read(c.name)
+   if err != nil {
+      return err
+   }
+   err = cache.Token.Refresh()
+   if err != nil {
+      return err
+   }
+   item, err := cache.Token.Item(path.Base(c.address))
+   if err != nil {
+      return err
+   }
+   files, err := cache.Token.Files(item)
+   if err != nil {
+      return err
+   }
+   var ok bool
+   cache.MediaFile, ok = files.Dash()
+   if !ok {
+      return errors.New(".Dash()")
+   }
+   cache.Mpd, err = cache.MediaFile.Mpd()
+   if err != nil {
+      return err
+   }
+   err = write(c.name, cache)
+   if err != nil {
+      return err
+   }
+   return maya.ListDash(cache.Mpd.Body, cache.Mpd.Url)
 }
