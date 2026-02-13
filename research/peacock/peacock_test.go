6da27b577fb9ec0a7277f79d3614513650a74636
27 <27@riseup.net>
2026-02-12 21:05:19 -0600

    deleted:    research/peacock/signin.go

--- research/peacock/peacock_test.go
+++ research/peacock/peacock_test.go
@@ -1,12 +1,65 @@
 package peacock
 
 import (
-   "encoding/hex"
    "fmt"
    "os"
+   "os/exec"
    "testing"
 )
 
+func TestSignRead(t *testing.T) {
+   cache, err := os.UserCacheDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   data, err := os.ReadFile(cache + "/peacock/peacock.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var sign SignIn
+   sign.Unmarshal(data)
+   auth, err := sign.Auth()
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", auth)
+}
+
+func output(name string, arg ...string) (string, error) {
+   data, err := exec.Command(name, arg...).Output()
+   if err != nil {
+      return "", err
+   }
+   return string(data), nil
+}
+
+func TestSignWrite(t *testing.T) {
+   user, err := output("credential", "-h=peacocktv.com", "-k=user")
+   if err != nil {
+      t.Fatal(err)
+   }
+   password, err := output("credential", "-h=peacocktv.com")
+   if err != nil {
+      t.Fatal(err)
+   }
+   var sign SignIn
+   err = sign.New(user, password)
+   if err != nil {
+      t.Fatal(err)
+   }
+   data, err := sign.Marshal()
+   if err != nil {
+      t.Fatal(err)
+   }
+   cache, err := os.UserCacheDir()
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = os.WriteFile(cache + "/peacock/peacock.json", data, os.ModePerm)
+   if err != nil {
+      t.Fatal(err)
+   }
+}
 func TestVideo(t *testing.T) {
    home, err := os.UserHomeDir()
    if err != nil {
