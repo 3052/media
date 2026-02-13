17949fa0ceb447c3c60be7b6cab6b60c878bef08
27 <27@riseup.net>
2026-02-12 21:33:51 -0600

    +   resp, err := http.DefaultClient.Do(req)

--- research/peacock/peacock.go
+++ research/peacock/peacock.go
@@ -16,125 +16,6 @@ import (
    "time"
 )
 
-func (s *SignIn) New(user, password string) error {
-   if user == "" {
-      return errors.New("user")
-   }
-   body := url.Values{
-      "userIdentifier": {user},
-      "password": {password},
-   }.Encode()
-   req, err := http.NewRequest(
-      "POST", "https://rango.id.peacocktv.com/signin/service/international",
-      strings.NewReader(body),
-   )
-   if err != nil {
-      return err
-   }
-   req.Header = http.Header{
-      "Content-Type": {"application/x-www-form-urlencoded"},
-      "X-Skyott-Proposition": {"NBCUOTT"},
-      "X-Skyott-Provider": {"NBCU"},
-      "X-Skyott-Territory": {Territory},
-   }
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusCreated {
-      var b strings.Builder
-      res.Write(&b)
-      return errors.New(b.String())
-   }
-   for _, cookie := range res.Cookies() {
-      if cookie.Name == "idsession" {
-         s.cookie = cookie
-         return nil
-      }
-   }
-   return http.ErrNoCookie
-}
-
-func (s SignIn) Auth() (*AuthToken, error) {
-   var v struct {
-      Auth struct {
-         AuthScheme string `json:"authScheme"`
-         Proposition string `json:"proposition"`
-         Provider string `json:"provider"`
-         ProviderTerritory string `json:"providerTerritory"`
-      } `json:"auth"`
-      Device struct {
-         DrmDeviceId string `json:"drmDeviceId"`
-         ID string `json:"id"`
-         Platform string `json:"platform"`
-         Type string `json:"type"`
-      } `json:"device"`
-   }
-   v.Auth.AuthScheme = "MESSO"
-   v.Auth.Proposition = "NBCUOTT"
-   v.Auth.Provider = "NBCU"
-   v.Auth.ProviderTerritory = Territory
-   // if empty /drm/widevine/acquirelicense will fail with
-   // {
-   //    "errorCode": "OVP_00306",
-   //    "description": "Security failure"
-   // }
-   v.Device.DrmDeviceId = "UNKNOWN"
-   // if incorrect /video/playouts/vod will fail with
-   // {
-   //    "errorCode": "OVP_00311",
-   //    "description": "Unknown deviceId"
-   // }
-   // changing this too often will result in a four hour block
-   // {
-   //    "errorCode": "OVP_00014",
-   //    "description": "Maximum number of streaming devices exceeded"
-   // }
-   v.Device.ID = "PC"
-   v.Device.Platform = "ANDROIDTV"
-   v.Device.Type = "TV"
-   body, err := json.Marshal(v)
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://ovp.peacocktv.com/auth/tokens", bytes.NewReader(body),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.AddCookie(s.cookie)
-   req.Header.Set("content-type", "application/vnd.tokens.v1+json")
-   req.Header.Set("x-sky-signature", sign(req.Method, req.URL.Path, nil, body))
-   res, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
-      var b bytes.Buffer
-      res.Write(&b)
-      return nil, errors.New(b.String())
-   }
-   auth := new(AuthToken)
-   if err := json.NewDecoder(res.Body).Decode(auth); err != nil {
-      return nil, err
-   }
-   return auth, nil
-}
-
-func (s *SignIn) Unmarshal(b []byte) error {
-   return json.Unmarshal(b, &s.cookie)
-}
-
-type SignIn struct {
-   cookie *http.Cookie
-}
-
-func (s SignIn) Marshal() ([]byte, error) {
-   return json.Marshal(s.cookie)
-}
 func (v VideoPlayout) Akamai() (string, bool) {
    for _, endpoint := range v.Asset.Endpoints {
       if endpoint.CDN == "AKAMAI" {
@@ -215,18 +96,18 @@ func (a AuthToken) Video(content_id string) (*VideoPlayout, error) {
    req.Header.Set(
       "x-sky-signature", sign(req.Method, req.URL.Path, req.Header, body),
    )
-   res, err := http.DefaultClient.Do(req)
+   resp, err := http.DefaultClient.Do(req)
    if err != nil {
       return nil, err
    }
-   defer res.Body.Close()
-   if res.StatusCode != http.StatusOK {
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
       var b bytes.Buffer
-      res.Write(&b)
+      resp.Write(&b)
       return nil, errors.New(b.String())
    }
    video := new(VideoPlayout)
-   if err := json.NewDecoder(res.Body).Decode(video); err != nil {
+   if err := json.NewDecoder(resp.Body).Decode(video); err != nil {
       return nil, err
    }
    return video, nil
@@ -247,12 +128,12 @@ func (q *QueryNode) New(content_id string) error {
       "X-Skyott-Proposition": {"NBCUOTT"},
       "X-Skyott-Territory": {Territory},
    }
-   res, err := http.DefaultClient.Do(req)
+   resp, err := http.DefaultClient.Do(req)
    if err != nil {
       return err
    }
-   defer res.Body.Close()
-   return json.NewDecoder(res.Body).Decode(q)
+   defer resp.Body.Close()
+   return json.NewDecoder(resp.Body).Decode(q)
 }
 
 func sign(method, path string, head http.Header, body []byte) string {
@@ -332,6 +213,107 @@ type QueryNode struct {
    }
 }
 
-func (q QueryNode) Year() int {
-   return q.Attributes.Year
+func FetchIdSession(user, password string) (*http.Cookie, error) {
+   data := url.Values{
+      "userIdentifier": {user},
+      "password": {password},
+   }.Encode()
+   req, err := http.NewRequest(
+      "POST", "https://rango.id.peacocktv.com/signin/service/international",
+      strings.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("content-type": "application/x-www-form-urlencoded")
+   req.Header.Set("x-skyott-proposition": "NBCUOTT")
+   req.Header.Set("x-skyott-provider": "NBCU")
+   req.Header.Set("x-skyott-territory": Territory)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusCreated {
+      var data strings.Builder
+      err = resp.Write(&data)
+      if err != nil {
+         return nil, err
+      }
+      return errors.New(data.String())
+   }
+   for _, cookie := range resp.Cookies() {
+      if cookie.Name == "idsession" {
+         return cookie, nil
+      }
+   }
+   return nil, http.ErrNoCookie
+}
+
+func (s SignIn) Auth() (*AuthToken, error) {
+   var v struct {
+      Auth struct {
+         AuthScheme string `json:"authScheme"`
+         Proposition string `json:"proposition"`
+         Provider string `json:"provider"`
+         ProviderTerritory string `json:"providerTerritory"`
+      } `json:"auth"`
+      Device struct {
+         DrmDeviceId string `json:"drmDeviceId"`
+         ID string `json:"id"`
+         Platform string `json:"platform"`
+         Type string `json:"type"`
+      } `json:"device"`
+   }
+   v.Auth.AuthScheme = "MESSO"
+   v.Auth.Proposition = "NBCUOTT"
+   v.Auth.Provider = "NBCU"
+   v.Auth.ProviderTerritory = Territory
+   // if empty /drm/widevine/acquirelicense will fail with
+   // {
+   //    "errorCode": "OVP_00306",
+   //    "description": "Security failure"
+   // }
+   v.Device.DrmDeviceId = "UNKNOWN"
+   // if incorrect /video/playouts/vod will fail with
+   // {
+   //    "errorCode": "OVP_00311",
+   //    "description": "Unknown deviceId"
+   // }
+   // changing this too often will result in a four hour block
+   // {
+   //    "errorCode": "OVP_00014",
+   //    "description": "Maximum number of streaming devices exceeded"
+   // }
+   v.Device.ID = "PC"
+   v.Device.Platform = "ANDROIDTV"
+   v.Device.Type = "TV"
+   data, err := json.Marshal(v)
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://ovp.peacocktv.com/auth/tokens", bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.AddCookie(s.cookie)
+   req.Header.Set("content-type", "application/vnd.tokens.v1+json")
+   req.Header.Set("x-sky-signature", sign(req.Method, req.URL.Path, nil, data))
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      var b bytes.Buffer
+      resp.Write(&b)
+      return nil, errors.New(b.String())
+   }
+   auth := new(AuthToken)
+   if err := json.NewDecoder(resp.Body).Decode(auth); err != nil {
+      return nil, err
+   }
+   return auth, nil
 }
