3696fa98cbd41e6bdfb3484d346130ad4d97acad
27 <27@riseup.net>
2026-02-17 06:29:53 -0600

    +func Login(at, username, password string) (*http.Cookie, error) {

--- paramount/paramount_test.go
+++ paramount/paramount_test.go
@@ -2,24 +2,23 @@ package paramount
 
 import "testing"
 
-func TestLog(t *testing.T) {
-   t.Log(contents)
+func TestPath(t *testing.T) {
+   path, err := FetchPath(
+      "http://paramountplus.com/movies/video/wjQ4RChi6BHHu4MVTncppVuCwu44uq2Q",
+   )
+   if err != nil {
+      t.Fatal(err)
+   }
+   t.Log(path)
 }
 
-var contents = [][]string{
-   {
-      "https://paramountplus.com/ie/movies/video/3DcGhIoTusoQFB_YLGCtLvefraLxuZMJ",
-      "https://paramountplus.com/movies/video/3DcGhIoTusoQFB_YLGCtLvefraLxuZMJ",
-   },
-   {
-      "https://paramountplus.com/movies/video/Y8sKvb2bIoeX4XZbsfjadF4GhNPwcjTQ",
-      "https://paramountplus.com/ie/movies/video/Y8sKvb2bIoeX4XZbsfjadF4GhNPwcjTQ",
-   },
-   {
-      "https://paramountplus.com/movies/video/wjQ4RChi6BHHu4MVTncppVuCwu44uq2Q",
-      "https://paramountplus.com/movies/zodiac/wjQ4RChi6BHHu4MVTncppVuCwu44uq2Q",
-   },
-   {
-      "https://paramountplus.com/shows/video/esJvFlqdrcS_kFHnpxSuYp449E7tTexD",
-   },
+func TestResolve(t *testing.T) {
+   t.Log(resolved)
+}
+
+var resolved = []string{
+   "https://paramountplus.com/ie/movies/video/3DcGhIoTusoQFB_YLGCtLvefraLxuZMJ/",
+   "https://paramountplus.com/ie/movies/video/Y8sKvb2bIoeX4XZbsfjadF4GhNPwcjTQ/",
+   "https://paramountplus.com/movies/video/wjQ4RChi6BHHu4MVTncppVuCwu44uq2Q/",
+   "https://paramountplus.com/shows/video/esJvFlqdrcS_kFHnpxSuYp449E7tTexD/",
 }
