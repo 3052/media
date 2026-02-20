fbc3d739d9d1c131f2c7d02910ec051ed4fbad65
27 <27@riseup.net>
2026-02-19 20:28:47 -0600

    +func (c *command) do_dash() error {

--- research/paramount/paramount.go
+++ research/paramount/paramount.go
@@ -11,124 +11,16 @@ import (
    "path/filepath"
 )
 
-func (c *command) app_secret() string {
-   if c.intl {
-      return paramount.AppSecrets[0].ComCbsCa
-   }
-   return paramount.AppSecrets[0].ComCbsApp
-}
-
-func (c *command) run() error {
-   cache, err := os.UserCacheDir()
-   if err != nil {
-      return err
-   }
-   cache = filepath.ToSlash(cache)
-   c.name = cache + "/paramount/userCache.xml"
-   c.job.CertificateChain = cache + "/SL2000/CertificateChain"
-   c.job.EncryptSignKey = cache + "/SL2000/EncryptSignKey"
-   // 1
-   flag.StringVar(&c.username, "username", "", "username")
-   flag.StringVar(&c.password, "password", "", "password")
-   // 1, 2
-   flag.BoolVar(&c.intl, "i", false, "intl")
-   // 2
-   flag.StringVar(&c.paramount, "p", "", "paramount ID")
-   flag.StringVar(&c.proxy, "P", "", "proxy")
-   // 3
-   flag.StringVar(&c.dash, "d", "", "DASH ID")
-   flag.BoolVar(&c.cookie, "c", false, "cookie")
-   flag.StringVar(&c.job.CertificateChain, "C", c.job.CertificateChain, "certificate chain")
-   flag.StringVar(&c.job.EncryptSignKey, "E", c.job.EncryptSignKey, "encrypt sign key")
-   flag.Parse()
-   maya.SetProxy(func(req *http.Request) (string, bool) {
-      switch path.Ext(req.URL.Path) {
-      case ".m4s", ".mp4":
-         return "", false
-      }
-      switch path.Base(req.URL.Path) {
-      case "anonymous-session-token.json", "getlicense", "rightsmanager.asmx":
-         return "", true
-      }
-      return c.proxy, true
-   })
-   if c.username != "" {
-      if c.password != "" {
-         return c.do_username_password()
-      }
-   }
-   if c.paramount != "" {
-      return c.do_paramount()
-   }
-   if c.dash != "" {
-      return c.do_dash()
-   }
-   return maya.Usage([][]string{
-      {"U", "P", "i"},
-      {"p", "i"},
-      {"d", "c", "C", "E"},
-   })
-}
-
-func (c *command) do_username_password() error {
-   at, err := paramount.GetAt(c.app_secret())
-   if err != nil {
-      return err
-   }
-   var cache user_cache
-   cache.Cookie, err = paramount.Login(at, c.username, c.password)
-   if err != nil {
-      return err
-   }
-   return maya.Write(c.name, &cache)
-}
-
-type user_cache struct {
-   Dash      *paramount.Dash
-   Cookie    *http.Cookie
-   ContentId string
-   Intl      bool
-}
-
-func (c *command) do_paramount() error {
-   at, err := paramount.GetAt(c.app_secret())
-   if err != nil {
-      return err
-   }
-   cache, err := maya.Read[user_cache](c.name)
-   if err != nil {
-      cache = &user_cache{}
-   }
-   item, err := paramount.FetchItem(at, c.paramount)
-   if err != nil {
-      return err
-   }
-   cache.Dash, err = item.Dash()
-   if err != nil {
-      return err
-   }
-   cache.ContentId = c.paramount
-   cache.Intl = c.intl
-   err = maya.Write(c.name, cache)
-   if err != nil {
-      return err
-   }
-   return maya.ListDash(cache.Dash.Body, cache.Dash.Url)
-}
-
 func (c *command) do_dash() error {
-   at, err := paramount.GetAt(c.app_secret())
-   if err != nil {
-      return err
-   }
    cache, err := maya.Read[user_cache](c.name)
    if err != nil {
       return err
    }
-   if !c.cookie {
-      cache.Cookie = nil
+   at, err := paramount.GetAt(paramount.AppSecrets[0].ComCbsApp)
+   if err != nil {
+      return err
    }
-   token, err := paramount.PlayReady(at, cache.ContentId, cache.Cookie)
+   token, err := paramount.PlayReady(at, cache.ContentId, nil)
    if err != nil {
       return err
    }
@@ -146,15 +38,70 @@ func main() {
 type command struct {
    name string
    // 1
-   username string
-   password string
-   // 1, 2
-   intl bool
-   // 2
    paramount string
-   proxy     string
-   // 3
-   dash   string
-   cookie bool
-   job    maya.PlayReadyJob
+   // 2
+   dash string
+   job  maya.PlayReadyJob
+}
+
+func (c *command) run() error {
+   cache, err := os.UserCacheDir()
+   if err != nil {
+      return err
+   }
+   cache = filepath.ToSlash(cache)
+   c.name = cache + "/paramount/userCache.xml"
+   c.job.CertificateChain = cache + "/SL2000/CertificateChain"
+   c.job.EncryptSignKey = cache + "/SL2000/EncryptSignKey"
+   // 1
+   flag.StringVar(&c.paramount, "p", "", "paramount ID")
+   // 2
+   flag.StringVar(&c.dash, "d", "", "DASH ID")
+   flag.StringVar(&c.job.CertificateChain, "c", c.job.CertificateChain, "certificate chain")
+   flag.StringVar(&c.job.EncryptSignKey, "e", c.job.EncryptSignKey, "encrypt sign key")
+   flag.Parse()
+   maya.SetProxy(func(req *http.Request) (string, bool) {
+      switch path.Ext(req.URL.Path) {
+      case ".m4s", ".mp4":
+         return "", false
+      }
+      return "", true
+   })
+   if c.paramount != "" {
+      return c.do_paramount()
+   }
+   if c.dash != "" {
+      return c.do_dash()
+   }
+   return maya.Usage([][]string{
+      {"p"},
+      {"d", "c", "e"},
+   })
+}
+
+func (c *command) do_paramount() error {
+   at, err := paramount.GetAt(paramount.AppSecrets[0].ComCbsApp)
+   if err != nil {
+      return err
+   }
+   item, err := paramount.FetchItem(at, c.paramount)
+   if err != nil {
+      return err
+   }
+   var cache user_cache
+   cache.Dash, err = item.Dash()
+   if err != nil {
+      return err
+   }
+   cache.ContentId = c.paramount
+   err = maya.Write(c.name, cache)
+   if err != nil {
+      return err
+   }
+   return maya.ListDash(cache.Dash.Body, cache.Dash.Url)
+}
+
+type user_cache struct {
+   ContentId string
+   Dash      *paramount.Dash
 }
--- research/paramount/readme.md
+++ research/paramount/readme.md
@@ -1,12 +1,21 @@
 # paramount
 
-1. us video, us location
-2. us video, us location, cookie
-3. us video, ca location
-4. us video, ca location, cookie
-5. ca video, us location
-6. ca video, us location, cookie
-7. ca video, ca location
-8. ca video, ca location, cookie
-9. ca video, mx location
-10. ca video, mx location, cookie
+## us video, us location
+
+## us video, us location, cookie
+
+## us video, ca location
+
+## us video, ca location, cookie
+
+## ca video, us location
+
+## ca video, us location, cookie
+
+## ca video, ca location
+
+## ca video, ca location, cookie
+
+## ca video, mx location
+
+## ca video, mx location, cookie
