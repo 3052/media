3abd1db59506623d1ed36fb5d7f8c26b3acd0165
27 <27@riseup.net>
2026-03-07 10:32:41 -0600

    new file:   disney/AccountWithoutActiveProfile.go

--- disney/disney.go
+++ disney/disney.go
@@ -37,30 +37,6 @@ type AuthenticateWithOtp struct {
    ActionGrant string
 }
 
-type InactiveAccount struct {
-   Data struct {
-      Login *struct {
-         Account struct {
-            Profiles []Profile
-         }
-      }
-      LoginWithActionGrant *struct {
-         Account struct {
-            Profiles []Profile
-         }
-      }
-   }
-   Errors     []Error
-   Extensions struct {
-      Sdk struct {
-         Token struct {
-            AccessToken     string
-            AccessTokenType string // AccountWithoutActiveProfile
-         }
-      }
-   }
-}
-
 // passcode can start with 0
 func (d *Device) AuthenticateWithOtp(email, passcode string) (*AuthenticateWithOtp, error) {
    data, err := json.Marshal(map[string]any{
@@ -103,38 +79,6 @@ func (d *Device) AuthenticateWithOtp(email, passcode string) (*AuthenticateWithO
    return &result.Data.AuthenticateWithOtp, nil
 }
 
-func (d *Device) LoginWithActionGrant(actionGrant string) (*InactiveAccount, error) {
-   data, err := json.Marshal(map[string]any{
-      "query": mutation_login_with_action_grant,
-      "variables": map[string]any{
-         "input": map[string]string{
-            "actionGrant": actionGrant,
-         },
-      },
-   })
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://disney.api.edge.bamgrid.com/v1/public/graphql",
-      bytes.NewReader(data),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("authorization", "Bearer " + d.Token.AccessToken)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   result := &InactiveAccount{}
-   err = json.NewDecoder(resp.Body).Decode(result)
-   if err != nil {
-      return nil, err
-   }
-   return result, nil
-}
 func (d *Device) RequestOtp(email string) (*RequestOtp, error) {
    data, err := json.Marshal(map[string]any{
       "query": mutation_request_otp,
@@ -186,43 +130,6 @@ func (r RequestOtp) String() string {
 
 type RequestOtp struct {
    Accepted bool
-}
-
-func (d *Device) Login(email, password string) (*InactiveAccount, error) {
-   data, err := json.Marshal(map[string]any{
-      "query": mutation_login,
-      "variables": map[string]any{
-         "input": map[string]string{
-            "email":    email,
-            "password": password,
-         },
-      },
-   })
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://disney.api.edge.bamgrid.com/v1/public/graphql",
-      bytes.NewReader(data),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("authorization", "Bearer "+d.Token.AccessToken)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var result InactiveAccount
-   err = json.NewDecoder(resp.Body).Decode(&result)
-   if err != nil {
-      return nil, err
-   }
-   if len(result.Errors) >= 1 {
-      return nil, &result.Errors[0]
-   }
-   return &result, nil
 }
 
 func RegisterDevice() (*Device, error) {
@@ -314,39 +221,6 @@ type Error struct {
 type Hls struct {
    Body []byte
    Url  *url.URL
-}
-
-func (i *InactiveAccount) SwitchProfile(profileId string) (*Account, error) {
-   data, err := json.Marshal(map[string]any{
-      "query": mutation_switch_profile,
-      "variables": map[string]any{
-         "input": map[string]string{
-            "profileId": profileId,
-         },
-      },
-   })
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://disney.api.edge.bamgrid.com/v1/public/graphql",
-      bytes.NewReader(data),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("authorization", "Bearer "+i.Extensions.Sdk.Token.AccessToken)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   result := &Account{}
-   err = json.NewDecoder(resp.Body).Decode(result)
-   if err != nil {
-      return nil, err
-   }
-   return result, nil
 }
 
 func (p *Page) String() string {
