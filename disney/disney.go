9c3b22a1559f8f3e99b672dfd5e790e0738224a9
27 <27@riseup.net>
2026-03-07 10:07:40 -0600

    new file:   disney/login.gql

--- disney/disney.go
+++ disney/disney.go
@@ -9,20 +9,184 @@ import (
    "net/http"
    "net/url"
    "strings"
+   _ "embed"
 )
 
-const mutation_login = `
-mutation login($input: LoginInput!) {
-   login(login: $input) {
-      account {
-         profiles {
-            id
-            name
+//go:embed refreshToken.gql
+var mutation_refresh_token string
+
+//go:embed registerDevice.gql
+var mutation_register_device string
+
+//go:embed switchProfile.gql
+var mutation_switch_profile string
+
+//go:embed login.gql
+var mutation_login string
+
+//go:embed loginWithActionGrant.gql
+var mutation_login_with_action_grant string
+
+//go:embed requestOtp.gql
+var mutation_request_otp string
+
+//go:embed authenticateWithOtp.gql
+var mutation_authenticate_with_otp string
+
+type AuthenticateWithOtp struct {
+   ActionGrant string
+}
+
+type InactiveAccount struct {
+   Data struct {
+      Login *struct {
+         Account struct {
+            Profiles []Profile
+         }
+      }
+      LoginWithActionGrant *struct {
+         Account struct {
+            Profiles []Profile
+         }
+      }
+   }
+   Errors     []Error
+   Extensions struct {
+      Sdk struct {
+         Token struct {
+            AccessToken     string
+            AccessTokenType string // AccountWithoutActiveProfile
          }
       }
    }
 }
-`
+
+// passcode can start with 0
+func (d *Device) AuthenticateWithOtp(email, passcode string) (*AuthenticateWithOtp, error) {
+   data, err := json.Marshal(map[string]any{
+      "query": mutation_authenticate_with_otp,
+      "variables": map[string]any{
+         "input": map[string]string{
+            "email": email,
+            "passcode": passcode,
+         },
+      },
+   })
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://disney.api.edge.bamgrid.com/v1/public/graphql",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("authorization", "Bearer " + d.Token.AccessToken)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   var result struct {
+      Data struct {
+         AuthenticateWithOtp AuthenticateWithOtp
+      }
+      Errors []Error
+   }
+   err = json.NewDecoder(resp.Body).Decode(&result)
+   if err != nil {
+      return nil, err
+   }
+   if len(result.Errors) >= 1 {
+      return nil, &result.Errors[0]
+   }
+   return &result.Data.AuthenticateWithOtp, nil
+}
+
+func (d *Device) LoginWithActionGrant(actionGrant string) (*InactiveAccount, error) {
+   data, err := json.Marshal(map[string]any{
+      "query": mutation_login_with_action_grant,
+      "variables": map[string]any{
+         "input": map[string]string{
+            "actionGrant": actionGrant,
+         },
+      },
+   })
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://disney.api.edge.bamgrid.com/v1/public/graphql",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("authorization", "Bearer " + d.Token.AccessToken)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   result := &InactiveAccount{}
+   err = json.NewDecoder(resp.Body).Decode(result)
+   if err != nil {
+      return nil, err
+   }
+   return result, nil
+}
+func (d *Device) RequestOtp(email string) (*RequestOtp, error) {
+   data, err := json.Marshal(map[string]any{
+      "query": mutation_request_otp,
+      "variables": map[string]any{
+         "input": map[string]string{
+            "email": email,
+            "reason": "Login",
+         },
+      },
+   })
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://disney.api.edge.bamgrid.com/v1/public/graphql",
+      bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("authorization", "Bearer " + d.Token.AccessToken)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   var result struct {
+      Data struct {
+         RequestOtp RequestOtp
+      }
+      Errors []Error
+   }
+   err = json.NewDecoder(resp.Body).Decode(&result)
+   if err != nil {
+      return nil, err
+   }
+   if len(result.Errors) >= 1 {
+      return nil, &result.Errors[0]
+   }
+   return &result.Data.RequestOtp, nil
+}
+
+func (r RequestOtp) String() string {
+   if r.Accepted {
+      return "accepted = true"
+   }
+   return "accepted = false"
+}
+
+type RequestOtp struct {
+   Accepted bool
+}
 
 func (d *Device) Login(email, password string) (*InactiveAccount, error) {
    data, err := json.Marshal(map[string]any{
@@ -280,42 +444,10 @@ type Stream struct {
       }
    }
 }
-const mutation_register_device = `
-mutation registerDevice($input: RegisterDeviceInput!) {
-   registerDevice(registerDevice: $input) {
-      token {
-         accessToken
-         accessTokenType
-      }
-   }
-}
-`
-
-const mutation_switch_profile = `
-mutation switchProfile($input: SwitchProfileInput!) {
-   switchProfile(switchProfile: $input) {
-      account {
-         activeProfile {
-            name
-         }
-      }
-   }
-}
-`
 
 // ZGlzbmV5JmJyb3dzZXImMS4wLjA
 // disney&browser&1.0.0
 const client_api_key = "ZGlzbmV5JmJyb3dzZXImMS4wLjA.Cu56AgSfBTDag5NiRA81oLHkDZfu5L3CKadnefEAY84"
-
-const mutation_refresh_token = `
-mutation refreshToken($input: RefreshTokenInput!) {
-   refreshToken(refreshToken: $input) {
-      activeSession {
-         sessionId
-      }
-   }
-}
-`
 
 // https://disneyplus.com/browse/entity-7df81cf5-6be5-4e05-9ff6-da33baf0b94d
 // https://disneyplus.com/cs-cz/browse/entity-7df81cf5-6be5-4e05-9ff6-da33baf0b94d
