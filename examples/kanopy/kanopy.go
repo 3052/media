ca9f861795d643204ab1bc9f3e54250fb634d28b
27 <27@riseup.net>
2026-03-18 21:31:09 -0500

    +   Job maya.Job

--- examples/kanopy/kanopy.go
+++ examples/kanopy/kanopy.go
@@ -8,75 +8,12 @@ import (
    "log"
 )
 
-func (c *client) do_email_password() error {
-   c.Login = &kanopy.Login{}
-   err := c.Login.Fetch(c.email, c.password)
-   if err != nil {
-      return err
-   }
-   return cache.Write(c)
-}
-
-type client struct {
-   Dash         *kanopy.Dash
-   Login        *kanopy.Login
-   PlayManifest *kanopy.PlayManifest
-   // 1
-   email    string
-   password string
-   // 2
-   address string
-   // 3
-   dash_id string
-}
-
-func (c *client) do_address() error {
-   video := &kanopy.Video{}
-   err := video.Parse(c.address)
-   if err != nil {
-      return err
-   }
-   err = cache.Update(c, func() error {
-      if video.VideoId == 0 {
-         video, err = c.Login.Video(video.Alias)
-         if err != nil {
-            return err
-         }
-      }
-      member, err := c.Login.Membership()
-      if err != nil {
-         return err
-      }
-      plays, err := c.Login.Plays(member, video.VideoId)
-      if err != nil {
-         return err
-      }
-      for _, caption := range plays.Captions {
-         for _, file := range caption.Files {
-            fmt.Println(file.Url)
-         }
-      }
-      c.PlayManifest, err = plays.Dash()
-      if err != nil {
-         return err
-      }
-      c.Dash, err = c.PlayManifest.Dash()
-      return err
-   })
-   if err != nil {
-      return err
-   }
-   return maya.ListDash(c.Dash.Body, c.Dash.Url)
-}
 func (c *client) do_dash_id() error {
-   err := cache.Read(c)
-   if err != nil {
-      return err
-   }
-   job.Send = func(data []byte) ([]byte, error) {
-      return c.Login.Widevine(c.PlayManifest, data)
-   }
-   return job.DownloadDash(c.Dash.Body, c.Dash.Url, c.dash_id)
+   return c.Job.DownloadDash(c.Dash.Body, c.Dash.Url, c.dash_id,
+      func(data []byte) ([]byte, error) {
+         return c.Login.Widevine(c.PlayManifest, data)
+      },
+   )
 }
 
 func main() {
@@ -90,39 +27,101 @@ func main() {
 
 var cache maya.Cache
 
-var job maya.WidevineJob
+type client struct {
+   Dash         *kanopy.Dash
+   Login        *kanopy.Login
+   PlayManifest *kanopy.PlayManifest
+   // 1
+   Job maya.Job
+   // 2
+   email    string
+   password string
+   // 3
+   address string
+   // 4
+   dash_id string
+}
 
 func (c *client) do() error {
-   job.ClientId, _ = maya.ResolveCache("L3/client_id.bin")
-   job.PrivateKey, _ = maya.ResolveCache("L3/private_key.pem")
    err := cache.Setup("rosso/kanopy.xml")
    if err != nil {
       return err
    }
+   read_err := cache.Read(c)
    // 1
-   flag.StringVar(&c.email, "e", "", "email")
-   flag.StringVar(&c.password, "p", "", "password")
+   widevine := maya.StringVar(&c.Job.Widevine, "w", "Widevine")
    // 2
-   flag.StringVar(&c.address, "a", "", "address")
+   email := maya.StringVar(&c.email, "e", "email")
+   password := maya.StringVar(&c.password, "p", "password")
    // 3
-   flag.StringVar(&c.dash_id, "d", "", "DASH ID")
-   flag.StringVar(&job.ClientId, "C", job.ClientId, "client ID")
-   flag.StringVar(&job.PrivateKey, "P", job.PrivateKey, "private key")
-   flag.Parse()
-   if c.email != "" {
-      if c.password != "" {
-         return c.do_email_password()
-      }
-   }
-   if c.address != "" {
+   address := maya.StringVar(&c.address, "a", "address")
+   // 4
+   dash_id := maya.StringVar(&c.dash_id, "d", "DASH ID")
+   set := maya.Parse()
+   switch {
+   case len(set) == 0:
+      return maya.Usage([][]*flag.Flag{
+         {widevine},
+         {email, password},
+         {address},
+         {dash_id},
+      })
+   case set[email.Name] && set[password.Name]:
+      return c.do_email_password()
+   case read_err != nil:
+      return read_err
+   case set[address.Name]:
       return c.do_address()
-   }
-   if c.dash_id != "" {
+   case set[dash_id.Name]:
       return c.do_dash_id()
    }
-   return maya.Usage([][]string{
-      {"e", "p"},
-      {"a"},
-      {"d", "C", "P"},
-   })
+   return nil
+}
+
+func (c *client) do_email_password() error {
+   var err error
+   c.Login, err = kanopy.FetchLogin(c.email, c.password)
+   if err != nil {
+      return err
+   }
+   return cache.Write(c)
+}
+
+func (c *client) do_address() error {
+   video, err := kanopy.ParseVideo(c.address)
+   if err != nil {
+      return err
+   }
+   if video.VideoId == 0 {
+      video, err = c.Login.Video(video.Alias)
+      if err != nil {
+         return err
+      }
+   }
+   member, err := c.Login.Membership()
+   if err != nil {
+      return err
+   }
+   plays, err := c.Login.Plays(member, video.VideoId)
+   if err != nil {
+      return err
+   }
+   for _, caption := range plays.Captions {
+      for _, file := range caption.Files {
+         fmt.Println(file.Url)
+      }
+   }
+   c.PlayManifest, err = plays.Dash()
+   if err != nil {
+      return err
+   }
+   c.Dash, err = c.PlayManifest.Dash()
+   if err != nil {
+      return err
+   }
+   err = cache.Write(c)
+   if err != nil {
+      return err
+   }
+   return maya.ListDash(c.Dash.Body, c.Dash.Url)
 }
--- kanopy/kanopy.go
+++ kanopy/kanopy.go
@@ -12,6 +12,76 @@ import (
    "strings"
 )
 
+// Supports URLs such as:
+// - https://kanopy.com/video/6440418
+// - https://kanopy.com/video/genius-party
+// - https://kanopy.com/en/video/genius-party
+// - https://kanopy.com/en/product/genius-party
+func ParseVideo(urlData string) (*Video, error) {
+   urlParse, err := url.Parse(urlData)
+   if err != nil {
+      return nil, err
+   }
+   if !strings.Contains(urlParse.Host, "kanopy.com") {
+      return nil, errors.New("invalid domain")
+   }
+   // Get the directory of the path (removes the final identifier).
+   // e.g., "/en/product/genius-party" -> "/en/product"
+   dir := path.Dir(urlParse.Path)
+   // Check if the directory ends with "/video" OR "/product".
+   // This supports:
+   // - /video/{id}
+   // - /en/video/{id}
+   // - /en/product/{id}
+   if !strings.HasSuffix(dir, "/video") && !strings.HasSuffix(dir, "/product") {
+      return nil, errors.New("invalid path structure")
+   }
+   v := &Video{}
+   identifier := path.Base(urlParse.Path)
+   numericId, err := strconv.Atoi(identifier)
+   if err != nil {
+      v.Alias = identifier
+   } else {
+      v.VideoId = numericId
+   }
+   return v, nil
+}
+
+func FetchLogin(email, password string) (*Login, error) {
+   data, err := json.Marshal(map[string]any{
+      "credentialType": "email",
+      "emailUser": map[string]string{
+         "email":    email,
+         "password": password,
+      },
+   })
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest(
+      "POST", "https://www.kanopy.com/kapi/login", bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("content-type", "application/json")
+   req.Header.Set("user-agent", user_agent)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      return nil, errors.New(resp.Status)
+   }
+   result := &Login{}
+   err = json.NewDecoder(resp.Body).Decode(result)
+   if err != nil {
+      return nil, err
+   }
+   return result, nil
+}
+
 func (p *PlayManifest) Dash() (*Dash, error) {
    req, err := http.NewRequest("", p.Url, nil)
    if err != nil {
@@ -33,39 +103,6 @@ func (p *PlayManifest) Dash() (*Dash, error) {
 type Video struct {
    Alias   string
    VideoId int
-}
-
-// https://kanopy.com/video/6440418
-// https://kanopy.com/video/genius-party
-// https://kanopy.com/en/video/genius-party
-// https://kanopy.com/en/product/genius-party
-func (v *Video) Parse(urlData string) error {
-   urlParse, err := url.Parse(urlData)
-   if err != nil {
-      return err
-   }
-   if !strings.Contains(urlParse.Host, "kanopy.com") {
-      return errors.New("invalid domain")
-   }
-   // Get the directory of the path (removes the final identifier).
-   // e.g., "/en/product/genius-party" -> "/en/product"
-   dir := path.Dir(urlParse.Path)
-   // Update: Check if the directory ends with "/video" OR "/product".
-   // This supports:
-   // - /video/{id}
-   // - /en/video/{id}
-   // - /en/product/{id}
-   if !strings.HasSuffix(dir, "/video") && !strings.HasSuffix(dir, "/product") {
-      return errors.New("invalid path structure")
-   }
-   identifier := path.Base(urlParse.Path)
-   numericId, err := strconv.Atoi(identifier)
-   if err != nil {
-      v.Alias = identifier
-   } else {
-      v.VideoId = numericId
-   }
-   return nil
 }
 
 const x_version = "!/!/!/!"
@@ -140,36 +177,6 @@ const user_agent = "!"
 
 type Membership struct {
    DomainId int
-}
-
-func (l *Login) Fetch(email, password string) error {
-   data, err := json.Marshal(map[string]any{
-      "credentialType": "email",
-      "emailUser": map[string]string{
-         "email":    email,
-         "password": password,
-      },
-   })
-   if err != nil {
-      return err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://www.kanopy.com/kapi/login", bytes.NewReader(data),
-   )
-   if err != nil {
-      return err
-   }
-   req.Header.Set("content-type", "application/json")
-   req.Header.Set("user-agent", user_agent)
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return err
-   }
-   defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
-      return errors.New(resp.Status)
-   }
-   return json.NewDecoder(resp.Body).Decode(l)
 }
 
 func (l *Login) Widevine(manifest *PlayManifest, data []byte) ([]byte, error) {
