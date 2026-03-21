9876b25992e87f74127a71f135fc3e30d639d764
27 <27@riseup.net>
2026-03-20 22:15:19 -0500

    new file:   paramount/dash.go

--- paramount/paramount.go
+++ paramount/paramount.go
@@ -13,38 +13,41 @@ import (
    "net/http"
    "net/url"
    "slices"
-   "strconv"
    "strings"
 )
 
-const cms_account_id = "dJ5BDC"
-
-func FetchDash(contentId string) (*Dash, error) {
+// 1080p SL2000
+// 1440p SL2000 + cookie
+func PlayReady(at, contentId string, cbsCom *http.Cookie) (*SessionToken, error) {
    var req http.Request
-   req.URL = &url.URL{
-      Scheme: "https",
-      Host:   "link.theplatform.com",
-      Path: join(
-         "/s/", cms_account_id,
-         "/media/guid/", strconv.Itoa(cms_account(cms_account_id)),
-         "/", contentId,
-      ),
-      RawQuery: "formats=MPEG-DASH",
-   }
    req.Header = http.Header{}
+   req.URL = &url.URL{}
+   req.URL.Scheme = "https"
+   req.URL.Host = "www.paramountplus.com"
+   req.URL.RawQuery = url.Values{
+      "at":        {at},
+      "contentId": {contentId},
+   }.Encode()
+   if cbsCom != nil {
+      req.AddCookie(cbsCom)
+      req.URL.Path = "/apps-api/v3.1/xboxone/irdeto-control/session-token.json"
+   } else {
+      req.URL.Path = "/apps-api/v3.1/xboxone/irdeto-control/anonymous-session-token.json"
+   }
    resp, err := http.DefaultClient.Do(&req)
    if err != nil {
       return nil, err
    }
    defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
-      return nil, errors.New(resp.Status)
-   }
-   body, err := io.ReadAll(resp.Body)
+   var result SessionToken
+   err = json.NewDecoder(resp.Body).Decode(&result)
    if err != nil {
       return nil, err
    }
-   return &Dash{Body: body, Url: resp.Request.URL}, nil
+   if result.Errors != "" {
+      return nil, errors.New(result.Errors)
+   }
+   return &result, nil
 }
 
 // WARNING IF YOU RUN THIS TOO MANY TIMES YOU WILL GET AN IP BAN. HOWEVER THE BAN
@@ -177,22 +180,7 @@ func join(items ...string) string {
    return strings.Join(items, "")
 }
 
-const (
-   encoding   = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-   secret_key = "302a6a0d70a7e9b967f91d39fef3e387816e3095925ae4537bce96063311f9c5"
-)
-
-func cms_account(id string) int {
-   var (
-      result     = 0
-      multiplier = 1
-   )
-   for _, digit := range id {
-      result += strings.IndexRune(encoding, digit) * multiplier
-      multiplier *= len(encoding)
-   }
-   return result
-}
+const secret_key = "302a6a0d70a7e9b967f91d39fef3e387816e3095925ae4537bce96063311f9c5"
 
 func pkcs7_pad(data []byte, blockSize int) []byte {
    // Calculate the number of padding bytes needed.
@@ -206,40 +194,6 @@ func pkcs7_pad(data []byte, blockSize int) []byte {
       data = append(data, padByte)
    }
    return data
-}
-
-// 1080p SL2000
-// 1440p SL2000 + cookie
-func PlayReady(at, contentId string, cookie *http.Cookie) (*SessionToken, error) {
-   var req http.Request
-   req.Header = http.Header{}
-   req.URL = &url.URL{}
-   req.URL.Scheme = "https"
-   req.URL.Host = "www.paramountplus.com"
-   req.URL.RawQuery = url.Values{
-      "at":        {at},
-      "contentId": {contentId},
-   }.Encode()
-   if cookie != nil {
-      req.AddCookie(cookie)
-      req.URL.Path = "/apps-api/v3.1/xboxone/irdeto-control/session-token.json"
-   } else {
-      req.URL.Path = "/apps-api/v3.1/xboxone/irdeto-control/anonymous-session-token.json"
-   }
-   resp, err := http.DefaultClient.Do(&req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var result SessionToken
-   err = json.NewDecoder(resp.Body).Decode(&result)
-   if err != nil {
-      return nil, err
-   }
-   if result.Errors != "" {
-      return nil, errors.New(result.Errors)
-   }
-   return &result, nil
 }
 
 type SessionToken struct {
@@ -278,9 +232,4 @@ var AppSecrets = []struct {
       Us:            "9fc14cb03691c342",
       International: "6c68178445de8138",
    },
-}
-
-type Dash struct {
-   Body []byte
-   Url  *url.URL
 }
--- paramount/paramount_test.go
+++ paramount/paramount_test.go
@@ -4,22 +4,22 @@ import "testing"
 
 var videos = []struct {
    resolution string
-   paramount        string
-   justWatch string
+   paramount  string
+   justWatch  string
 }{
    {
-      justWatch: "https://justwatch.com/us/movie/zodiac",
-      paramount:        "https://paramountplus.com/movies/video/wjQ4RChi6BHHu4MVTncppVuCwu44uq2Q",
+      justWatch:  "https://justwatch.com/us/movie/zodiac",
+      paramount:  "https://paramountplus.com/movies/video/wjQ4RChi6BHHu4MVTncppVuCwu44uq2Q",
       resolution: "2160p",
    },
    {
-      justWatch: "https://justwatch.com/us/tv-show/criminal-minds",
-      paramount:        "https://paramountplus.com/shows/video/esJvFlqdrcS_kFHnpxSuYp449E7tTexD",
+      justWatch:  "https://justwatch.com/us/tv-show/criminal-minds",
+      paramount:  "https://paramountplus.com/shows/video/esJvFlqdrcS_kFHnpxSuYp449E7tTexD",
       resolution: "2160p",
    },
    {
-      justWatch: "https://justwatch.com/us/movie/scream-1",
-      paramount:        "https://paramountplus.com/gb/movies/video/yh8qG9949D8fvJa0dFmY1C_SilOYt2hS",
+      justWatch:  "https://justwatch.com/us/movie/scream-1",
+      paramount:  "https://paramountplus.com/gb/movies/video/yh8qG9949D8fvJa0dFmY1C_SilOYt2hS",
       resolution: "2160p",
    },
    {
